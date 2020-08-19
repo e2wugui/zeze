ae53f72f77017ac2c9b0639866982745153c9b94
lichenghua <e2wugui@163.com>
2020-08-19 22:10:29 +0800

    Table more test

--- UnitTest/Zeze/Trans/TestTable.cs
+++ UnitTest/Zeze/Trans/TestTable.cs
@@ -17,25 +17,114 @@ namespace UnitTest.Zeze.Trans
         Table1 table2 = new Table1("table2");
 
         [TestMethod]
-        public void Test3()
+        public void TestUpdate()
         {
             Assert.IsTrue(new Procedure(ProcGetOrAdd).Call());
+            Assert.IsTrue(new Procedure(ProcGetUpdate).Call());
+            Assert.IsTrue(new Procedure(ProcGetUpdateCheckRemove).Call());
         }
 
-        bool ProcGetOrAdd()
+        bool ProcGetUpdate()
         {
+            demo.Module1.Value v = table1.Get(1);
+
+            v.Int1 = 11;
+            v.Long2 = 22;
+            v.String3 = "33";
+            v.Bool4 = true;
+            v.Short5 = 55;
+            v.Float6 = 66;
+            v.Double7 = 77;
+            v.List9.Add(new demo.Bean1());
+            v.Set10.Add(1010);
+            v.Map11.Add(2, new demo.Module2.Value());
+            v.Bean12.Int1 = 1212;
+            v.Byte13 = 131;
+            return true;
+        }
+
+        bool ProcGetUpdateCheckRemove()
+        {
+            demo.Module1.Value v = table1.Get(1);
+
+            Assert.IsTrue(v.Int1 == 11);
+            Assert.IsTrue(v.Long2 == 22);
+            Assert.IsTrue(v.String3.Equals("33"));
+            Assert.IsTrue(v.Bool4);
+            Assert.IsTrue(v.Short5 == 55);
+            Assert.IsTrue(v.Float6 == 66);
+            Assert.IsTrue(v.Double7 == 77);
+            Assert.IsTrue(v.List9.Count == 2);
+            Assert.IsTrue(v.Set10.Contains(10));
+            Assert.IsTrue(v.Set10.Contains(1010));
+            Assert.IsTrue(v.Set10.Count == 2);
+            Assert.IsTrue(v.Map11.Count == 2);
+            Assert.IsTrue(v.Bean12.Int1 == 1212);
+            Assert.IsTrue(v.Byte13 == 131);
+
             return true;
         }
 
         [TestMethod]
-        public void Test1()
+        public void TestGetOrAdd()
+        {
+            Assert.IsTrue(new Procedure(ProcGetOrAdd).Call());
+            Assert.IsTrue(new Procedure(ProcGetOrAddCheckAndRemove).Call());
+        }
+
+        bool ProcGetOrAdd()
+        {
+            demo.Module1.Value v = table1.GetOrAdd(1);
+
+            v.Int1 = 1;
+            v.Long2 = 2;
+            v.String3 = "3";
+            v.Bool4 = true;
+            v.Short5 = 5;
+            v.Float6 = 6;
+            v.Double7 = 7;
+            v.List9.Add(new demo.Bean1());
+            v.Set10.Add(10);
+            v.Map11.Add(1, new demo.Module2.Value());
+            v.Bean12.Int1 = 12;
+            v.Byte13 = 13;
+
+            return true;
+        }
+
+        bool ProcGetOrAddCheckAndRemove()
+        {
+            var v = table1.Get(1);
+            Assert.IsNotNull(v);
+
+            Assert.IsTrue(v.Int1 == 1);
+            Assert.IsTrue(v.Long2 == 2);
+            Assert.IsTrue(v.String3.Equals("3"));
+            Assert.IsTrue(v.Bool4);
+            Assert.IsTrue(v.Short5 == 5);
+            Assert.IsTrue(v.Float6 == 6);
+            Assert.IsTrue(v.Double7 == 7);
+            Assert.IsTrue(v.List9.Count == 1);
+            Assert.IsTrue(v.Set10.Contains(10));
+            Assert.IsTrue(v.Set10.Count == 1);
+            Assert.IsTrue(v.Map11.Count == 1);
+            Assert.IsTrue(v.Bean12.Int1 == 12);
+            Assert.IsTrue(v.Byte13 == 13);
+
+            table1.Remove(1);
+            Assert.IsNull(table1.Get(1));
+            return true;
+        }
+
+        [TestMethod]
+        public void Test1TableGetPut()
         {
             Assert.IsTrue(new Procedure(ProcGet11).Call());
             Assert.IsTrue(new Procedure(ProcGet12).Call());
         }
 
         [TestMethod]
-        public void Test2()
+        public void Test2TableGetPut()
         {
             Assert.IsTrue(new Procedure(ProcGet21).Call());
             Assert.IsTrue(new Procedure(ProcGet22).Call());
