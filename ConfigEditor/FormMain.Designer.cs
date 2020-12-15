b8ee13a29218609b1bba1b76ff4554a42738f32e
lichenghua <e2wugui@163.com>
2020-12-15 14:00:56 +0800

    ConfigEditor 搭了一些架子。

--- ConfigEditor/FormMain.Designer.cs
+++ ConfigEditor/FormMain.Designer.cs
@@ -29,25 +29,32 @@ namespace ConfigEditor
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
             this.tabFile = new System.Windows.Forms.TabPage();
             this.Grid = new System.Windows.Forms.DataGridView();
             this.tabs = new System.Windows.Forms.TabControl();
             this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.newButton = new System.Windows.Forms.ToolStripButton();
+            this.saveButton = new System.Windows.Forms.ToolStripButton();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openButton = new System.Windows.Forms.ToolStripButton();
             this.tabFile.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.Grid)).BeginInit();
             this.tabs.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabFile
             // 
             this.tabFile.Controls.Add(this.Grid);
-            this.tabFile.Location = new System.Drawing.Point(4, 22);
-            this.tabFile.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabFile.Location = new System.Drawing.Point(8, 39);
+            this.tabFile.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabFile.Name = "tabFile";
-            this.tabFile.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.tabFile.Size = new System.Drawing.Size(1223, 576);
+            this.tabFile.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabFile.Size = new System.Drawing.Size(1696, 961);
             this.tabFile.TabIndex = 1;
-            this.tabFile.Text = "NoFile";
+            this.tabFile.Text = "NewFile";
             this.tabFile.UseVisualStyleBackColor = true;
             // 
             // Grid
@@ -58,13 +65,13 @@ namespace ConfigEditor
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.Grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.Grid.Location = new System.Drawing.Point(0, 2);
-            this.Grid.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Grid.Location = new System.Drawing.Point(0, 0);
+            this.Grid.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Grid.MultiSelect = false;
             this.Grid.Name = "Grid";
             this.Grid.RowHeadersWidth = 82;
             this.Grid.RowTemplate.Height = 37;
-            this.Grid.Size = new System.Drawing.Size(1222, 577);
+            this.Grid.Size = new System.Drawing.Size(1696, 952);
             this.Grid.TabIndex = 0;
             // 
             // tabs
@@ -73,27 +80,79 @@ namespace ConfigEditor
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tabs.Controls.Add(this.tabFile);
-            this.tabs.Location = new System.Drawing.Point(0, 2);
-            this.tabs.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabs.Location = new System.Drawing.Point(0, 54);
+            this.tabs.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabs.Name = "tabs";
             this.tabs.SelectedIndex = 0;
-            this.tabs.Size = new System.Drawing.Size(1231, 602);
+            this.tabs.Size = new System.Drawing.Size(1712, 1008);
             this.tabs.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newButton,
+            this.openButton,
+            this.saveButton});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Padding = new System.Windows.Forms.Padding(0, 0, 4, 0);
+            this.toolStrip1.Size = new System.Drawing.Size(1712, 50);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // newButton
+            // 
+            this.newButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.newButton.Image = ((System.Drawing.Image)(resources.GetObject("newButton.Image")));
+            this.newButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.newButton.Name = "newButton";
+            this.newButton.Size = new System.Drawing.Size(46, 44);
+            this.newButton.Text = "New(ctrl + N)";
+            this.newButton.Click += new System.EventHandler(this.newButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.saveButton.Image = ((System.Drawing.Image)(resources.GetObject("saveButton.Image")));
+            this.saveButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(46, 44);
+            this.saveButton.Text = "Save(ctrl + S)";
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openButton
+            // 
+            this.openButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.openButton.Image = ((System.Drawing.Image)(resources.GetObject("openButton.Image")));
+            this.openButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(46, 44);
+            this.openButton.Text = "openButton";
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
             // 
             // FormMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(812, 530);
+            this.ClientSize = new System.Drawing.Size(1712, 1056);
+            this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.tabs);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormMain";
             this.Text = "Main";
             this.Load += new System.EventHandler(this.FormMain_Load);
             this.tabFile.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.Grid)).EndInit();
             this.tabs.ResumeLayout(false);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -103,6 +162,11 @@ namespace ConfigEditor
         private System.Windows.Forms.DataGridView Grid;
         private System.Windows.Forms.TabControl tabs;
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton newButton;
+        private System.Windows.Forms.ToolStripButton saveButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripButton openButton;
     }
 }
 
--- ConfigEditor/FormMain.cs
+++ ConfigEditor/FormMain.cs
@@ -5,10 +5,9 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Text.Json;
-using System.Text.Json.Serialization;
+using System.Xml;
 
 namespace ConfigEditor
 {
@@ -73,6 +72,96 @@ namespace ConfigEditor
             this.folderBrowserDialog.ShowDialog();
             Config.SetRecentHome(this.folderBrowserDialog.SelectedPath);
             SaveConfig();
+        }
+
+        private DataGridView newGrid()
+        {
+            DataGridView grid = new DataGridView();
+            grid.AllowUserToAddRows = false;
+            grid.AllowUserToDeleteRows = false;
+            grid.Anchor = ((AnchorStyles)((((AnchorStyles.Top | AnchorStyles.Bottom) | AnchorStyles.Left) | AnchorStyles.Right)));
+            grid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            grid.Location = new Point(0, 0);
+            grid.Margin = new Padding(2);
+            grid.MultiSelect = false;
+            grid.Name = "Grid";
+            grid.RowHeadersWidth = 82;
+            grid.RowTemplate.Height = 37;
+            //gird.Size = new Size(848, 476);
+            grid.TabIndex = 0;
+
+            TabPage tab = new TabPage();
+            tab.Text = "NewFile";
+            tab.Controls.Add(grid);
+            tabs.Controls.Add(tab);
+            return grid;
+        }
+
+        private void newButton_Click(object sender, EventArgs e)
+        {
+            newGrid();
+        }
+
+        class Document
+        {
+            public string FileName { get; set; }
+            public XmlDocument Xml { get; set; }
+
+            public void BuildNew(DataGridView grid)
+            {
+                Xml = new XmlDocument();
+
+            }
+
+            public void Open(DataGridView grid)
+            {
+                Xml = new XmlDocument();
+                Xml.Load(FileName);
+
+            }
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if (tabs.SelectedTab == null)
+                return;
+
+            if (!tabs.SelectedTab.Controls.GetEnumerator().MoveNext())
+                return;
+
+            DataGridView grid = (DataGridView)tabs.SelectedTab.Controls.GetEnumerator().Current;
+            Document doc = (Document)grid.Tag;
+            if (null == doc)
+            {
+                this.openFileDialog1.InitialDirectory = Config.RecentHomes[0];
+                this.openFileDialog1.FileName = "";
+                this.openFileDialog1.Filter = "(*.xml)|*.xml";
+                if (DialogResult.OK != this.openFileDialog1.ShowDialog())
+                    return;
+                string file = this.openFileDialog1.FileName;
+                if (!file.EndsWith(".xml"))
+                    file = file + ".xml";
+
+                doc = new Document() { FileName = file };
+                doc.BuildNew(grid);
+                doc.Xml.Save(file);
+                grid.Tag = doc;
+                return;
+            }
+            doc.Xml.Save(doc.FileName);
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            this.openFileDialog1.InitialDirectory = Config.RecentHomes[0];
+            this.openFileDialog1.FileName = "";
+            this.openFileDialog1.Filter = "(*.xml)|*.xml";
+            if (DialogResult.OK != this.openFileDialog1.ShowDialog())
+                return;
+            Document doc = new Document() { FileName = this.openFileDialog1.FileName };
+            DataGridView grid = newGrid();
+            doc.Open(grid);
+            grid.Tag = doc;
         }
     }
 }
