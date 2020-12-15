a02fcf25783eff41707759822c6484cfebed942c
lichenghua <e2wugui@163.com>
2020-12-15 12:20:34 +0800

    start ConfigEditor.csproj

---
+++ ConfigEditor/FormMain.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace ConfigEditor
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabFile = new System.Windows.Forms.TabPage();
+            this.Grid = new System.Windows.Forms.DataGridView();
+            this.tabs = new System.Windows.Forms.TabControl();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.tabFile.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Grid)).BeginInit();
+            this.tabs.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabFile
+            // 
+            this.tabFile.Controls.Add(this.Grid);
+            this.tabFile.Location = new System.Drawing.Point(4, 22);
+            this.tabFile.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabFile.Name = "tabFile";
+            this.tabFile.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabFile.Size = new System.Drawing.Size(1223, 576);
+            this.tabFile.TabIndex = 1;
+            this.tabFile.Text = "NoFile";
+            this.tabFile.UseVisualStyleBackColor = true;
+            // 
+            // Grid
+            // 
+            this.Grid.AllowUserToAddRows = false;
+            this.Grid.AllowUserToDeleteRows = false;
+            this.Grid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Grid.Location = new System.Drawing.Point(0, 2);
+            this.Grid.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Grid.MultiSelect = false;
+            this.Grid.Name = "Grid";
+            this.Grid.RowHeadersWidth = 82;
+            this.Grid.RowTemplate.Height = 37;
+            this.Grid.Size = new System.Drawing.Size(1222, 577);
+            this.Grid.TabIndex = 0;
+            // 
+            // tabs
+            // 
+            this.tabs.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabs.Controls.Add(this.tabFile);
+            this.tabs.Location = new System.Drawing.Point(0, 2);
+            this.tabs.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabs.Name = "tabs";
+            this.tabs.SelectedIndex = 0;
+            this.tabs.Size = new System.Drawing.Size(1231, 602);
+            this.tabs.TabIndex = 0;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 530);
+            this.Controls.Add(this.tabs);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "FormMain";
+            this.Text = "Main";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.tabFile.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Grid)).EndInit();
+            this.tabs.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabPage tabFile;
+        private System.Windows.Forms.DataGridView Grid;
+        private System.Windows.Forms.TabControl tabs;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+    }
+}
+
---
+++ ConfigEditor/FormMain.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ConfigEditor
+{
+    public partial class FormMain : Form
+    {
+        public class EditorConfig
+        {
+            public IList<string> RecentHomes { get; set; }
+
+            public void SetRecentHome(string home)
+            {
+                RecentHomes.Insert(0, home);
+                IList<string> distinct = new List<string>();
+                foreach (var r in RecentHomes.Distinct())
+                    distinct.Add(r);
+                RecentHomes = distinct;
+                if (RecentHomes.Count > 10)
+                    RecentHomes.RemoveAt(RecentHomes.Count - 1);
+            }
+        }
+
+        public EditorConfig Config { get; private set; }
+        public FormMain()
+        {
+            InitializeComponent();
+            LoadConfig();
+        }
+
+        private string GetConfigFileFullName()
+        {
+            string localappdata = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            string confighome = System.IO.Path.Combine(localappdata, "zeze");
+            System.IO.Directory.CreateDirectory(confighome);
+            return System.IO.Path.Combine(confighome, "ConfigEditor.json");
+        }
+
+        private void LoadConfig()
+        {
+            try
+            {
+                string json = Encoding.UTF8.GetString(System.IO.File.ReadAllBytes(GetConfigFileFullName()));
+                Config = JsonSerializer.Deserialize<EditorConfig>(json);
+            }
+            catch (Exception)
+            {
+                // skip error
+            }
+            if (null == Config)
+                Config = new EditorConfig() { RecentHomes = new List<string>() };
+        }
+
+        private void SaveConfig()
+        {
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            System.IO.File.WriteAllBytes(GetConfigFileFullName(), JsonSerializer.SerializeToUtf8Bytes(Config, options));
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            if (Config.RecentHomes.Count > 0)
+                this.folderBrowserDialog.SelectedPath = Config.RecentHomes.First();
+            this.folderBrowserDialog.ShowDialog();
+            Config.SetRecentHome(this.folderBrowserDialog.SelectedPath);
+            SaveConfig();
+        }
+    }
+}
