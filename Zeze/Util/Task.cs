059296719b5def8b836b7b33fb5c64c85a43142d
lichenghua <e2wugui@163.com>
2021-02-03 07:00:04 +0800

    add

---
+++ Zeze/Util/Task.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+
+namespace Zeze.Util
+{
+    public class Task
+    {
+        private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+        public static System.Threading.Tasks.Task Run(Action action, string actionName)
+        {
+            return System.Threading.Tasks.Task.Run(() =>
+            {
+                try
+                {
+                    action();
+                }
+                catch (Exception ex)
+                {
+                    logger.Error(ex, actionName);
+                }
+            });
+        }
+
+        public static System.Threading.Tasks.Task Run(Func<int> func, string actionName)
+        {
+            return System.Threading.Tasks.Task.Run(() =>
+            {
+                try
+                {
+                    func();
+                }
+                catch (Exception ex)
+                {
+                    logger.Error(ex, actionName);
+                }
+            });
+        }
+
+        public static System.Threading.Tasks.Task Run(Zeze.Transaction.Procedure procdure)
+        {
+            return System.Threading.Tasks.Task.Run(() =>
+            {
+                try
+                {
+                    procdure.Call();
+                }
+                catch (Exception ex)
+                {
+                    logger.Error(ex, procdure.ActionName);
+                }
+            });
+        }
+    }
+}
