48a2cdc03c3348fa893e545320b7688ee144cdf9
lichenghua <e2wugui@163.com>
2021-05-12 16:03:23 +0800

    Raft.Rpcs

--- Zeze/Raft/Rpcs.cs
+++ Zeze/Raft/Rpcs.cs
@@ -3,21 +3,220 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Zeze.Serialize;
+using Zeze.Transaction;
 
 namespace Zeze.Raft
 {
-    public class RequestVote
+    public sealed class RequestVoteArgument : Zeze.Transaction.Bean
+    {
+        public long Term { get; set; }
+        public long CandidateId { get; set; }
+        public long LastLogIndex { get; set; }
+        public long LastLogTerm { get; set; }
+
+        public override void Decode(ByteBuffer bb)
+        {
+            Term = bb.ReadLong();
+            CandidateId = bb.ReadLong();
+            LastLogIndex = bb.ReadLong();
+            LastLogTerm = bb.ReadLong();
+        }
+
+        public override void Encode(ByteBuffer bb)
+        {
+            bb.WriteLong(Term);
+            bb.WriteLong(CandidateId);
+            bb.WriteLong(LastLogIndex);
+            bb.WriteLong(LastLogTerm);
+        }
+
+        protected override void InitChildrenRootInfo(Record.RootInfo root)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public sealed class RequestVoteResult : Zeze.Transaction.Bean
     { 
+        public long Term { get; set; }
+        public bool VoteGranted { get; set; }
+
+        public override void Decode(ByteBuffer bb)
+        {
+            Term = bb.ReadLong();
+            VoteGranted = bb.ReadBool();
+        }
+
+        public override void Encode(ByteBuffer bb)
+        {
+            bb.WriteLong(Term);
+            bb.WriteBool(VoteGranted);
+        }
+
+        protected override void InitChildrenRootInfo(Record.RootInfo root)
+        {
+            throw new NotImplementedException();
+        }
     }
 
-    public class AppendEntries
+    public sealed class RequestVote : Zeze.Net.Rpc<RequestVoteArgument, RequestVoteResult>
     {
+        public readonly static int ProtocolId_ = Bean.Hash16(typeof(RequestVote).FullName);
 
+        public override int ModuleId => 0;
+        public override int ProtocolId => ProtocolId_;
     }
 
-    public class InstallSnapshot
+    public sealed class AppendEntriesArgument : Zeze.Transaction.Bean
     {
+        public long Term { get; set; }
+        public string LeaderId { get; set; } // Ip:Port
+        public long PrevLogIndex { get; set; }
+        public long PrevLogTerm { get; set; }
+        public List<Zeze.Net.Binary> Entries { get; } = new List<Net.Binary>();
+        public long LeaderCommit { get; set; }
 
+        public override void Decode(ByteBuffer bb)
+        {
+            Term = bb.ReadLong();
+            LeaderId = bb.ReadString();
+            PrevLogIndex = bb.ReadLong();
+            PrevLogTerm = bb.ReadLong();
+
+            Entries.Clear();
+            for (int c = bb.ReadInt(); c > 0; --c)
+            {
+                Entries.Add(bb.ReadBinary());
+            }
+
+            LeaderCommit = bb.ReadLong();
+        }
+
+        public override void Encode(ByteBuffer bb)
+        {
+            bb.WriteLong(Term);
+            bb.WriteString(LeaderId);
+            bb.WriteLong(PrevLogIndex);
+            bb.WriteLong(PrevLogTerm);
+
+            bb.WriteInt(Entries.Count);
+            foreach (var e in Entries)
+            {
+                bb.WriteBinary(e);
+            }
+
+            bb.WriteLong(LeaderCommit);
+        }
+
+        protected override void InitChildrenRootInfo(Record.RootInfo root)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public sealed class AppendEntriesResult : Zeze.Transaction.Bean
+    {
+        public long Term { get; set; }
+        public bool Success { get; set; }
+
+        public override void Decode(ByteBuffer bb)
+        {
+            Term = bb.ReadLong();
+            Success = bb.ReadBool();
+        }
+
+        public override void Encode(ByteBuffer bb)
+        {
+            bb.WriteLong(Term);
+            bb.WriteBool(Success);
+        }
+
+        protected override void InitChildrenRootInfo(Record.RootInfo root)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public sealed class AppendEntries : Zeze.Net.Rpc<AppendEntriesArgument, AppendEntriesResult>
+    {
+        public readonly static int ProtocolId_ = Bean.Hash16(typeof(AppendEntries).FullName);
+
+        public override int ModuleId => 0;
+        public override int ProtocolId => ProtocolId_;
+    }
+
+    public sealed class InstallSnapshotArgument : Zeze.Transaction.Bean
+    {
+        public long Term { get; set; }
+        public string LeaderId { get; set; } // Ip:Port
+        public long LastIncludedIndex { get; set; }
+        public long LastIncludedTerm { get; set; }
+
+        /// <summary>
+        /// Raft 文档的Snapshot描述是基于文件流传输的。
+        /// 这里使用Binary，也是基于流传输，但内容为自定义格式。
+        /// 需要自己解析和处理，不仅限于文件传输。
+        /// 【注意】跟Raft文档相比，这里去掉Offset。
+        /// 当Leader发送最后一个Trunk时设置Done为true。
+        /// </summary>
+        public Zeze.Net.Binary Trunk { get; set; }
+        public bool Done { get; set; }
+
+        public override void Decode(ByteBuffer bb)
+        {
+            Term = bb.ReadLong();
+            LeaderId = bb.ReadString();
+            LastIncludedIndex = bb.ReadLong();
+            LastIncludedTerm = bb.ReadLong();
+
+            Trunk = bb.ReadBinary();
+            Done = bb.ReadBool();
+        }
+
+        public override void Encode(ByteBuffer bb)
+        {
+            bb.WriteLong(Term);
+            bb.WriteString(LeaderId);
+            bb.WriteLong(LastIncludedIndex);
+            bb.WriteLong(LastIncludedTerm);
+
+            bb.WriteBinary(Trunk);
+            bb.WriteBool(Done);
+        }
+
+        protected override void InitChildrenRootInfo(Record.RootInfo root)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public sealed class InstallSnapshotResult : Zeze.Transaction.Bean
+    {
+        public long Term { get; set; }
+
+        public override void Decode(ByteBuffer bb)
+        {
+            Term = bb.ReadLong();
+        }
+
+        public override void Encode(ByteBuffer bb)
+        {
+            bb.WriteLong(Term);
+        }
+
+        protected override void InitChildrenRootInfo(Record.RootInfo root)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public sealed class InstallSnapshot : Zeze.Net.Rpc<InstallSnapshotArgument, InstallSnapshotResult>
+    {
+        public readonly static int ProtocolId_ = Bean.Hash16(typeof(InstallSnapshot).FullName);
+
+        public override int ModuleId => 0;
+        public override int ProtocolId => ProtocolId_;
     }
 
 
