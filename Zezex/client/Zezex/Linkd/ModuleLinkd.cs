9546bd10d914d39642262e8cb0a558a5794a2a49
lichenghua <e2wugui@163.com>
2021-07-21 17:15:41 +0800

    Zezex

---
+++ Zezex/client/Zezex/Linkd/ModuleLinkd.cs
@@ -0,0 +1,24 @@
+﻿
+namespace gnet.Linkd
+{
+    public sealed partial class ModuleLinkd : AbstractModule
+    {
+        public void Start(Game.App app)
+        {
+        }
+
+        public void Stop(Game.App app)
+        {
+        }
+
+        public override int ProcessKeepAlive(KeepAlive protocol)
+        {
+            return Zeze.Transaction.Procedure.NotImplement;
+        }
+
+        public override int ProcessReportError(ReportError protocol)
+        {
+            return Zeze.Transaction.Procedure.NotImplement;
+        }
+    }
+}
