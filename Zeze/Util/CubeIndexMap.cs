2fbf9c4747c5da8573cbb115bc236b093e255781
lichenghua <e2wugui@163.com>
2021-08-16 16:15:01 +0800

    CubeIndexMap

--- Zeze/Util/CubeIndexMap.cs
+++ Zeze/Util/CubeIndexMap.cs
@@ -3,127 +3,87 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Text;
 using System.Collections.Concurrent;
+using System.Collections.Immutable;
 
 namespace Zeze.Util
 {
+    public class CubeIndex : IComparable<CubeIndex>
+    {
+        public long X { get; set; }
+        public long Y { get; set; }
+        public long Z { get; set; }
+
+        public override int GetHashCode()
+        {
+            const int prime = 31;
+            int result = 17;
+            result = prime * result + X.GetHashCode();
+            result = prime * result + Y.GetHashCode();
+            result = prime * result + Z.GetHashCode();
+            return result;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == this)
+                return true;
+
+            if (obj is CubeIndex other)
+            {
+                return X == other.X && Y == other.Y && Z == other.Z;
+            }
+            return false;
+        }
+
+        public int CompareTo(CubeIndex other)
+        {
+            int c = X.CompareTo(other.X);
+            if (c != 0)
+                return c;
+            c = Y.CompareTo(other.Y);
+            if (c != 0)
+                return c;
+            return Z.CompareTo(other.Z);
+        }
+    }
+
+    public abstract class Cube<TObject>
+    {
+        public const int StateRemoved = -1;
+
+        /// <summary>
+        /// 子类实现可以利用这个状态，自定义状态必须大于等于0，负数保留给内部使用。
+        /// </summary>
+        public int State { get; set; }
+
+        public abstract void Add(CubeIndex index, TObject obj);
+
+        /// <summary>
+        /// 返回 True 表示 Cube 可以删除。这是为了回收内存。
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public abstract bool Remove(CubeIndex index, TObject obj);
+    }
+
     /// <summary>
     /// 把三维空间划分成一个个相邻的Cube。
     /// 地图中的玩家或者物品Id记录在所在的Cube中。
     /// 用来快速找到某个坐标周围的玩家或物体。
     /// </summary>
-    public class CubeIndexMap
+    public class CubeIndexMap<TCube, TObject> where TCube : Cube<TObject>, new()
     {
-        public class Index : IComparable<Index>
-        {
-            public long X { get; set; }
-            public long Y { get; set; }
-            public long Z { get; set; }
-
-            public override int GetHashCode()
-            {
-                const int prime = 31;
-                int result = 17;
-                result = prime * result + X.GetHashCode();
-                result = prime * result + Y.GetHashCode();
-                result = prime * result + Z.GetHashCode();
-                return result;
-            }
-
-            public override bool Equals(object obj)
-            {
-                if (obj == this)
-                    return true;
-
-                if (obj is Index other)
-                {
-                    return X == other.X && Y == other.Y && Z == other.Z;
-                }
-                return false;
-            }
-
-            public int CompareTo(Index other)
-            {
-                int c = X.CompareTo(other.X);
-                if (c != 0)
-                    return c;
-                c = Y.CompareTo(other.Y);
-                if (c != 0)
-                    return c;
-                return Z.CompareTo(other.Z);
-            }
-        }
-
-        public class ObjectId : IComparable<ObjectId>
-        {
-            public int Type { get; set; }
-            public int ConfigId { get; set; }
-            public long InstanceId { get; set; }
-
-            public override int GetHashCode()
-            {
-                const int prime = 31;
-                int result = 17;
-                result = prime * result + Type.GetHashCode();
-                result = prime * result + ConfigId.GetHashCode();
-                result = prime * result + InstanceId.GetHashCode();
-                return result;
-            }
-
-            public override bool Equals(object obj)
-            {
-                if (obj == null)
-                    return false;
-
-                if (obj == this)
-                    return true;
-
-                if (obj is ObjectId o)
-                {
-                    return Type == o.Type && ConfigId == o.ConfigId && InstanceId == o.InstanceId;
-                }
-                return false;
-            }
-
-            public int CompareTo(ObjectId x)
-            {
-                int c = Type.CompareTo(x.Type);
-                if (c != 0)
-                    return c;
-                c = ConfigId.CompareTo(x.ConfigId);
-                if (c != 0)
-                    return c;
-                return InstanceId.CompareTo(x.InstanceId);
-            }
-        }
-
-        public class Cube
-        {
-            public const int StateRemoved = -1;
-
-            public int State { get; set; }
-
-            public HashSet<ObjectId> ObjectIds { get; } = new HashSet<ObjectId>();
-
-            public virtual void Add(Index index, ObjectId obj)
-            {
-                ObjectIds.Add(obj);
-            }
-
-            public virtual void Remove(Index index, ObjectId obj)
-            {
-                ObjectIds.Remove(obj);
-            }
-        }
-
-        private ConcurrentDictionary<Index, Cube> Cubes = new ConcurrentDictionary<Index, Cube>();
+        private ConcurrentDictionary<CubeIndex, TCube> Cubes
+            = new ConcurrentDictionary<CubeIndex, TCube>();
 
         public int CubeSizeX { get; }
         public int CubeSizeY { get; }
         public int CubeSizeZ { get; }
 
-        public Index ToIndex(double x, double y, double z)
+        public CubeIndex ToIndex(double x, double y, double z)
         {
-            return new Index()
+            return new CubeIndex()
             {
                 X = (long)(x / CubeSizeX),
                 Y = (long)(y / CubeSizeY),
@@ -131,14 +91,33 @@ namespace Zeze.Util
             };
         }
 
+        public CubeIndex ToIndex(float x, float y, float z)
+        {
+            return new CubeIndex()
+            {
+                X = (long)(x / CubeSizeX),
+                Y = (long)(y / CubeSizeY),
+                Z = (long)(z / CubeSizeZ),
+            };
+        }
+
+        public CubeIndex ToIndex(long x, long y, long z)
+        {
+            return new CubeIndex()
+            {
+                X = (long)(x / CubeSizeX),
+                Y = (long)(y / CubeSizeY),
+                Z = (long)(z / CubeSizeZ),
+            };
+        }
         public CubeIndexMap(int cubeSizeX = 256, int cubeSizeY = 256, int cubeSizeZ = 256)
         {
             if (cubeSizeX <= 0)
-                throw new ArgumentException();
+                throw new ArgumentException("cubeSizeX <= 0");
             if (cubeSizeY <= 0)
-                throw new ArgumentException();
+                throw new ArgumentException("cubeSizeY <= 0");
             if (cubeSizeZ <= 0)
-                throw new ArgumentException();
+                throw new ArgumentException("cubeSizeZ <= 0");
 
             this.CubeSizeX = cubeSizeX;
             this.CubeSizeY = cubeSizeY;
@@ -146,26 +125,16 @@ namespace Zeze.Util
         }
 
         /// <summary>
-        /// 可以重载，提供Cube的子类进行扩展。
-        /// </summary>
-        /// <param name="index"></param>
-        /// <returns></returns>
-        public virtual Cube CreateCubeFactory(Index index)
-        {
-            return new Cube();
-        }
-
-        /// <summary>
         /// perfrom action if cube exist.
         /// under lock (cube)
         /// </summary>
-        public void TryPerfrom(Index index, Action<Index, Cube> action)
+        public void TryPerfrom(CubeIndex index, Action<CubeIndex, TCube> action)
         {
             if (Cubes.TryGetValue(index, out var cube))
             {
                 lock (cube)
                 {
-                    if (cube.State != Cube.StateRemoved)
+                    if (cube.State != Cube<TObject>.StateRemoved)
                     {
                         action(index, cube);
                     }
@@ -177,14 +146,14 @@ namespace Zeze.Util
         /// perfrom action for Cubes.GetOrAdd.
         /// under lock (cube)
         /// </summary>
-        public void Perform(Index index, Action<Index, Cube> action)
+        public void Perform(CubeIndex index, Action<CubeIndex, TCube> action)
         {
             while (true)
             {
-                var cube = Cubes.GetOrAdd(index, CreateCubeFactory);
+                var cube = Cubes.GetOrAdd(index, (_) => new TCube());
                 lock (cube)
                 {
-                    if (cube.State == Cube.StateRemoved)
+                    if (cube.State == Cube<TObject>.StateRemoved)
                         continue;
                     action(index, cube);
                 }
@@ -194,84 +163,91 @@ namespace Zeze.Util
         /// <summary>
         /// 角色进入地图时
         /// </summary>
-        /// <param name="objId"></param>
-        /// <param name="x"></param>
-        /// <param name="y"></param>
-        /// <param name="z"></param>
-        public void OnEnter(ObjectId objId, double x, double y, double z)
+        public void OnEnter(TObject obj, double x, double y, double z)
         {
-            Perform(ToIndex(x, y, z), (index, cube) => cube.Add(index, objId));
+            Perform(ToIndex(x, y, z), (index, cube) => cube.Add(index, obj));
+        }
+
+        public void OnEnter(TObject obj, float x, float y, float z)
+        {
+            Perform(ToIndex(x, y, z), (index, cube) => cube.Add(index, obj));
+        }
+
+        public void OnEnter(TObject obj, long x, long y, long z)
+        {
+            Perform(ToIndex(x, y, z), (index, cube) => cube.Add(index, obj));
+        }
+
+        private void RemoveObject(CubeIndex index, TCube cube, TObject obj)
+        {
+            if (cube.Remove(index, obj))
+            {
+                cube.State = Cube<TObject>.StateRemoved;
+                Cubes.TryRemove(index, out var _);
+            }
+        }
+
+        private void OnMove(CubeIndex oIndex, CubeIndex nIndex, TObject obj)
+        {
+            if (oIndex.Equals(nIndex))
+                return;
+
+            TryPerfrom(oIndex, (index, cube) => RemoveObject(index, cube, obj));
+            Perform(nIndex, (index, cube) => cube.Add(index, obj));
         }
 
         /// <summary>
         /// 角色位置变化时
         /// </summary>
-        /// <param name="objId"></param>
-        /// <param name="oldx"></param>
-        /// <param name="oldy"></param>
-        /// <param name="oldz"></param>
-        /// <param name="newx"></param>
-        /// <param name="newy"></param>
-        /// <param name="newz"></param>
-        public void OnMove(ObjectId objId,
+        public void OnMove(TObject obj,
             double oldx, double oldy, double oldz,
             double newx, double newy, double newz)
         {
-            var oIndex = ToIndex(oldx, oldy, oldz);
-            var nIndex = ToIndex(newx, newy, newz);
-
-            if (oIndex.Equals(nIndex))
-                return;
-
-            TryPerfrom(oIndex, (index, cube) => RemoveObject(index, cube, objId));
-            Perform(nIndex, (index, cube) => cube.Add(index, objId));
+            OnMove(ToIndex(oldx, oldy, oldz), ToIndex(newx, newy, newz), obj);
         }
 
-        private void RemoveObject(Index index, Cube cube, ObjectId obj)
+        public void OnMove(TObject obj,
+            float oldx, float oldy, float oldz,
+            float newx, float newy, float newz)
         {
-            cube.Remove(index, obj);
-            if (cube.ObjectIds.Count == 0)
-            {
-                cube.State = Cube.StateRemoved;
-                Cubes.TryRemove(index, out var _);
-            }
+            OnMove(ToIndex(oldx, oldy, oldz), ToIndex(newx, newy, newz), obj);
+        }
+
+        public void OnMove(TObject obj,
+            long oldx, long oldy, long oldz,
+            long newx, long newy, long newz)
+        {
+            OnMove(ToIndex(oldx, oldy, oldz), ToIndex(newx, newy, newz), obj);
         }
 
         /// <summary>
         /// 角色离开地图时
         /// </summary>
-        /// <param name="objId"></param>
-        /// <param name="x"></param>
-        /// <param name="y"></param>
-        /// <param name="z"></param>
-        public void OnLeave(ObjectId objId, double x, double y, double z)
+        public void OnLeave(TObject obj, double x, double y, double z)
         {
-            TryPerfrom(ToIndex(x, y, z), (index, cube) => RemoveObject(index, cube, objId));
+            TryPerfrom(ToIndex(x, y, z), (index, cube) => RemoveObject(index, cube, obj));
         }
 
-        /// <summary>
-        /// 返回 center 坐标所在的 cube 周围的所有 cube。
-        /// 可以遍历返回的Cube的所有角色，进一步进行精确的距离判断。
-        /// </summary>
-        /// <param name="centerX"></param>
-        /// <param name="centerY"></param>
-        /// <param name="centerZ"></param>
-        /// <param name="range">周围cube数</param>
-        /// <returns></returns>
-        public List<Cube> GetCubes(
-            double centerX, double centerY, double centerZ,
-            int rangeX = 4, int rangeY = 4, int rangeZ = 4)
+        public void OnLeave(TObject obj, float x, float y, float z)
         {
-            var center = ToIndex(centerX, centerY, centerZ);
+            TryPerfrom(ToIndex(x, y, z), (index, cube) => RemoveObject(index, cube, obj));
+        }
 
-            List<Cube> result = new List<Cube>();
+        public void OnLeave(TObject obj, long x, long y, long z)
+        {
+            TryPerfrom(ToIndex(x, y, z), (index, cube) => RemoveObject(index, cube, obj));
+        }
+
+        public List<TCube> GetCubes(CubeIndex center, int rangeX, int rangeY, int rangeZ)
+        {
+            List<TCube> result = new List<TCube>();
             for (long i = center.X - rangeX; i <= center.X + rangeX; ++i)
             {
                 for (long j = center.Y - rangeY; j <= center.Y + rangeY; ++j)
                 {
                     for (long k = center.Z - rangeZ; k <= center.Z + rangeZ; ++k)
                     {
-                        var index = new Index() { X = i, Y = j, Z = k };
+                        var index = new CubeIndex() { X = i, Y = j, Z = k };
                         if (Cubes.TryGetValue(index, out var cube))
                             result.Add(cube);
                     }
@@ -279,5 +255,104 @@ namespace Zeze.Util
             }
             return result;
         }
+
+        /// <summary>
+        /// 返回 center 坐标所在的 cube 周围的所有 cube。
+        /// 可以遍历返回的Cube的所有角色，进一步进行精确的距离判断。
+        /// </summary>
+        public List<TCube> GetCubes(
+            double centerX, double centerY, double centerZ,
+            int rangeX = 4, int rangeY = 4, int rangeZ = 4)
+        {
+            return GetCubes(ToIndex(centerX, centerY, centerZ), rangeX, rangeY, rangeZ);
+        }
+
+        public List<TCube> GetCubes(
+            float centerX, float centerY, float centerZ,
+            int rangeX = 4, int rangeY = 4, int rangeZ = 4)
+        {
+            return GetCubes(ToIndex(centerX, centerY, centerZ), rangeX, rangeY, rangeZ);
+        }
+
+        public List<TCube> GetCubes(
+            long centerX, long centerY, long centerZ,
+            int rangeX = 4, int rangeY = 4, int rangeZ = 4)
+        {
+            return GetCubes(ToIndex(centerX, centerY, centerZ), rangeX, rangeY, rangeZ);
+        }
+    }
+
+    /// <summary>
+    /// Game Helper
+    /// </summary>
+    public class GameObjectId : IComparable<GameObjectId>
+    {
+        public int Type { get; set; }
+        public int ConfigId { get; set; }
+        public long InstanceId { get; set; }
+
+        public override int GetHashCode()
+        {
+            const int prime = 31;
+            int result = 17;
+            result = prime * result + Type.GetHashCode();
+            result = prime * result + ConfigId.GetHashCode();
+            result = prime * result + InstanceId.GetHashCode();
+            return result;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (obj == this)
+                return true;
+
+            if (obj is GameObjectId o)
+            {
+                return Type == o.Type && ConfigId == o.ConfigId && InstanceId == o.InstanceId;
+            }
+            return false;
+        }
+
+        public int CompareTo(GameObjectId x)
+        {
+            int c = Type.CompareTo(x.Type);
+            if (c != 0)
+                return c;
+            c = ConfigId.CompareTo(x.ConfigId);
+            if (c != 0)
+                return c;
+            return InstanceId.CompareTo(x.InstanceId);
+        }
+    }
+
+    public class GameCube : Cube<GameObjectId>
+    {
+        public ImmutableHashSet<GameObjectId> ObjectIds { get; private set; }
+            = ImmutableHashSet<GameObjectId>.Empty;
+
+        public override void Add(CubeIndex index, GameObjectId obj)
+        {
+            ObjectIds = ObjectIds.Add(obj);
+        }
+
+        /// <summary>
+        /// 返回 True 表示 Cube 可以删除。这是为了回收内存。
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Remove(CubeIndex index, GameObjectId obj)
+        {
+            ObjectIds = ObjectIds.Remove(obj);
+            return ObjectIds.Count == 0;
+        }
+    }
+
+
+    public class GameMap : CubeIndexMap<GameCube, GameObjectId>
+    {
     }
 }
