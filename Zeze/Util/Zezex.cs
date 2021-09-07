01d4f8397a6ac9a68a11559a3d99b3fa17e511de
lichenghua <e2wugui@163.com>
2021-09-07 16:35:18 +0800

    Zezex export . TODO copy module source

--- Zeze/Util/Zezex.cs
+++ Zeze/Util/Zezex.cs
@@ -4,15 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Xml;
 
 namespace Zeze.Util
 {
     public class Zezex
     {
-        private string modules = "login";
+        private string modules = "Login";
         private bool linkd = true;
+
         private string SolutionName = null;
         private string ServerProjectName = null;
+        private string ClientProjectName = null;
         private string ClientLang = null;
         private string ExportDirectory = "../../";
         private string ZezexDirectory = "./";
@@ -27,6 +30,7 @@ namespace Zeze.Util
                     case "-ExportDirectory": ExportDirectory = args[++i]; break;
                     case "-ZezexDirectory": ZezexDirectory = args[++i]; break;
                     case "-ServerProjectName": ServerProjectName = args[++i]; break;
+                    case "-ClientProjectName": ClientProjectName = args[++i]; break;
                     case "-ClientLang": ClientLang = args[++i]; break;
                     case "-nolinkd": linkd = false; break;
                     case "-modules": modules = args[++i]; break;
@@ -66,7 +70,11 @@ namespace Zeze.Util
         private bool VerifyParams()
         {
             if (string.IsNullOrEmpty(SolutionName))
+            {
+                Console.WriteLine($"SolutionName Need.");
                 return false;
+            }
+
             if (false == Directory.Exists(ExportDirectory))
             {
                 Console.WriteLine($"ExportDirectory Not Exist: Path={ExportDirectory}");
@@ -93,23 +101,167 @@ namespace Zeze.Util
             if (linkd)
                 ExportLinkd();
 
-            var moduleset = new HashSet<string>();
-            foreach (var m in modules.Split(","))
-                moduleset.Add(m);
-
             ExportModules();
+        }
+
+        private string ModuleExportType = "";
+
+        private string GetClientHandleByClientLang()
+        {
+            if (string.IsNullOrEmpty(ClientLang))
+                return null;
+
+            switch (ClientLang)
+            {
+                case "cs":
+                    return "client";
+                default:
+                    return "clientscript";
+            }
+        }
+
+        private void UpdateProtocolClientHandle(XmlElement self, string clientHandle)
+        {
+            foreach (XmlNode n in self.ChildNodes)
+            {
+                if (XmlNodeType.Element != n.NodeType)
+                    continue;
+
+                XmlElement e = (XmlElement)n;
+                switch (e.Name)
+                {
+                    case "protocol":
+                    case "rpc":
+                        var newHandle = e.GetAttribute("handle").Replace("client", clientHandle);
+                        e.SetAttribute("handle", newHandle);
+                        break;
+                }
+            }
+        }
+
+        private void UpdateClientServiceHandleName(XmlElement self, string clientHandle)
+        {
+            foreach (XmlNode n in self.ChildNodes)
+            {
+                if (XmlNodeType.Element != n.NodeType)
+                    continue;
+
+                XmlElement e = (XmlElement)n;
+                switch (e.Name)
+                {
+                    case "service":
+                        if (e.GetAttribute("name").Equals("Client"))
+                        {
+                            e.SetAttribute("handle", clientHandle);
+                        }
+                        break;
+                }
+            }
+        }
+
+        private void UpdateProject(XmlElement e, string clientHandle)
+        {
+            switch (e.GetAttribute("name"))
+            {
+                case "server":
+                    if (false == string.IsNullOrEmpty(ServerProjectName))
+                        e.SetAttribute("name", ServerProjectName);
+                    break;
+
+                case "client":
+                    // 需要更多参数，client采用脚本的话，目录组织可能很不一样。
+                    // 以后实际使用的时候再支持。
+                    if (false == string.IsNullOrEmpty(ClientProjectName))
+                        e.SetAttribute("name", ClientProjectName);
+                    if (false == string.IsNullOrEmpty(clientHandle))
+                        UpdateClientServiceHandleName(e, clientHandle);
+                    break;
+
+                default:
+                    e.ParentNode.RemoveChild(e);
+                    break;
+            }
         }
 
         private void ExportModules()
         {
+            var exportModules = new HashSet<string>();
 
+            foreach (var m in modules.Split(","))
+            {
+                if (m.Equals("all") || m.Equals("none"))
+                {
+                    if (ModuleExportType != null)
+                        throw new Exception($"ModuleExportType has setup with '{ModuleExportType}'");
+                    ModuleExportType = m;
+                }
+                else if (false == string.IsNullOrEmpty(m))
+                {
+                    exportModules.Add(m);
+                }
+            }
+
+            if (ModuleExportType.Equals("none"))
+                return;
+
+            var solutionXmlFile = "solution.xml";
+            XmlDocument doc = new XmlDocument();
+            doc.PreserveWhitespace = true;
+            doc.Load(Path.Combine(ZezexDirectory, solutionXmlFile));
+
+            XmlElement self = doc.DocumentElement;
+            self.SetAttribute("name", SolutionName);
+
+            if (ModuleExportType.Equals("all"))
+            {
+                if (exportModules.Count > 0)
+                    throw new Exception("-modules all must present along.");
+            }
+
+            // update document.
+            var clientHandle = GetClientHandleByClientLang();
+            for (int i = self.ChildNodes.Count - 1; i >= 0; --i)
+            {
+                XmlNode child = self.ChildNodes[i];
+
+                if (XmlNodeType.Element != child.NodeType)
+                    continue;
+
+                XmlElement e = (XmlElement)child;
+                switch (e.Name)
+                {
+                    case "module":
+                        if (false == ModuleExportType.Equals("all")
+                            && false == exportModules.Contains(e.GetAttribute("name")))
+                        {
+                            self.RemoveChild(child);
+                        }
+                        else if (false == string.IsNullOrEmpty(clientHandle))
+                        {
+                            UpdateProtocolClientHandle(e, clientHandle);
+                        }
+                        break;
+
+                    case "project":
+                        UpdateProject(e, clientHandle);
+                        break;
+                }
+            }
+
+            using (TextWriter sw = new StreamWriter(
+                Path.Combine(ExportDirectory, solutionXmlFile),
+                false, Encoding.UTF8))
+            {
+                doc.Save(sw);
+            }
         }
 
         private void ExportLinkd()
         {
+            CopyTo("solution.linkd.xml", ExportDirectory);
+
             var linkdDir = Path.Combine(ExportDirectory, "linkd");
             Directory.CreateDirectory(linkdDir);
-
             CopyTo("linkd/Zezex", linkdDir);
 
             CopyTo("linkd/linkd.csproj", linkdDir);
--- Zezex/export.bat
+++ Zezex/export.bat
@@ -1,4 +1,4 @@
 
-..\Gen\bin\Debug\net5.0\Gen.exe -c zezex -SolutionName ZezeUser
+..\Gen\bin\Debug\net5.0\Gen.exe -c zezex -SolutionName ZezeUser -ClientLang ts
 
 pause
