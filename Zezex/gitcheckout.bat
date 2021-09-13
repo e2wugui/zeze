0ea3914d363f9823cf57ae5f88935eac0182d5c9
lichenghua <e2wugui@163.com>
2021-09-13 11:53:48 +0800

    ，，，

--- Zezex/gitcheckout.bat
+++ Zezex/gitcheckout.bat
@@ -2,12 +2,21 @@
 
 cd "%1"
 
-set tag = %2
+set tag=%2
 
 if "%tag%" == "" (
 	for /f %%x in ('"git describe --tags --abbrev=0"') do set tag=%%x
-	echo %tag%
 )
 
-git checkout "%tag%"
+if "%tag%" == "" goto ERROREXIT
 
+git checkout "%tag%"
+if NOT errorlevel 0 goto ERROREXIT
+
+exit /B 0
+
+:ERROREXIT
+
+echo ERROR
+
+exit /B 1
