4d5e727bef39b617ccf73645de81faad68b9a7c5
wuhaifeng <wuhaifeng4@163.com>
2021-10-14 10:55:18 +0800

    byte[].toHexString()

--- ZezeJava/src/Zeze/Util/BitConverter.java
+++ ZezeJava/src/Zeze/Util/BitConverter.java
@@ -1,7 +1,25 @@
 package Zeze.Util;
 
 public class BitConverter {
+	
+	private static final char[] hexDigit = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
 	public static String toString(byte[] bytes) {
-		return ""; // TODO whf
+		var len = bytes.length;
+		var r = new char[len*2];
+		for(int i = 0; i < len; ++i) {
+			byte b = bytes[i];
+			r[2*i] = hexDigit[(b>>8)&0xf];
+			r[2*i+1] = hexDigit[b&0xf];
+		}
+		return new String(r);
+	}
+	
+	public static void main(String[] args) {
+		var bytes = new byte[256];
+		for(int i = 0; i < bytes.length; ++i)
+			bytes[i] = (byte)i;
+		System.out.println(BitConverter.toString(bytes));
 	}
 }
+
+
