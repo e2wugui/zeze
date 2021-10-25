dc51559bd20b3cf7746e47596c2981996639b702
lichenghua <e2wugui@163.com>
2021-10-25 15:50:08 +0800

    RedoException.cs

---
+++ Zeze/Transaction/RedoException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zeze.Transaction
+{
+    public sealed class RedoException : Exception
+    {
+        internal RedoException()
+        { 
+        }
+
+        internal RedoException(string msg) : base(msg)
+        {
+
+        }
+    }
+}
