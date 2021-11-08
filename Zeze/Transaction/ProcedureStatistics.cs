a47658bfb57f27a104ac8cbca0b9036d7d71d76a
lichenghua <e2wugui@163.com>
2021-11-08 15:32:04 +0800

    ProcedureStatistics.Watcher

--- Zeze/Transaction/ProcedureStatistics.cs
+++ Zeze/Transaction/ProcedureStatistics.cs
@@ -12,6 +12,8 @@ namespace Zeze.Transaction
     /// </summary>
     public class ProcedureStatistics
     {
+        static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
         public static ProcedureStatistics Instance { get; } = new ProcedureStatistics();
 
         public ConcurrentDictionary<string, Statistics> Procedures { get; } = new ConcurrentDictionary<string, Statistics>();
@@ -29,6 +31,56 @@ namespace Zeze.Transaction
             public Zeze.Util.AtomicLong GetOrAdd(long result)
             {
                 return Results.GetOrAdd(result, (key) => new Util.AtomicLong());
+            }
+
+            public long GetTotalCount()
+            {
+                long total = 0;
+                foreach (var e in Results)
+                {
+                    total += e.Value.Get();
+                }
+                return total;
+            }
+
+            public void Watch(long reachPerSecond, Action handle)
+            {
+                var watcher = new Watcher(this, reachPerSecond, handle);
+                Util.Scheduler.Instance.Schedule((thisTask) => watcher.Check(this),
+                    Watcher.CheckPeriod * 1000, Watcher.CheckPeriod * 1000);
+            }
+
+            public class Watcher
+            {
+                public const int CheckPeriod = 30;
+
+                public long Last { get; private set; }
+                public long Reach { get; }
+                public Action ReachHandle { get; }
+
+                public Watcher(Statistics stats, long reachPerSecond, Action handle)
+                {
+                    Last = stats.GetTotalCount();
+                    Reach = reachPerSecond;
+                    ReachHandle = handle;
+                }
+
+                public void Check(Statistics stats)
+                {
+                    var total = stats.GetTotalCount();
+                    if ((total - Last) / CheckPeriod > Reach)
+                    {
+                        try
+                        {
+                            ReachHandle();
+                        }
+                        catch (Exception e)
+                        {
+                            logger.Error(e, "ProcedureStatistics.Watcher");
+                        }
+                    }
+                    Last = total;
+                }
             }
         }
     }
--- ZezeJava/src/Zeze/Transaction/ProcedureStatistics.java
+++ ZezeJava/src/Zeze/Transaction/ProcedureStatistics.java
@@ -1,5 +1,8 @@
 package Zeze.Transaction;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -13,6 +16,7 @@ public class ProcedureStatistics {
 	public static ProcedureStatistics getInstance() {
 		return Instance;
 	}
+	static final Logger logger = LogManager.getLogger(ProcedureStatistics.class);
 
 	private ConcurrentHashMap<String, Statistics> Procedures = new ConcurrentHashMap<> ();
 	public final ConcurrentHashMap<String, Statistics> getProcedures() {
@@ -31,6 +35,48 @@ public class ProcedureStatistics {
 
 		public final AtomicLong GetOrAdd(long result) {
 			return getResults().computeIfAbsent(result, (key) -> new AtomicLong());
+		}
+
+		public long GetTotalCount() {
+			long total = 0;
+			for (var e : Results.entrySet())
+			{
+				total += e.getValue().get();
+			}
+			return total;
+		}
+
+		public void Watch(long reachPerSecond, Runnable handle) {
+			var watcher = new Watcher(this, reachPerSecond, handle);
+			Zeze.Util.Task.schedule((thisTask) -> watcher.Check(this),
+					Watcher.CheckPeriod * 1000, Watcher.CheckPeriod * 1000);
+		}
+
+		public class Watcher {
+			public final static int CheckPeriod = 30;
+
+			public long Last;
+			public long Reach;
+			public Runnable ReachHandle;
+
+			public Watcher(Statistics stats, long reachPerSecond, Runnable handle) {
+				Last = stats.GetTotalCount();
+				Reach = reachPerSecond;
+				ReachHandle = handle;
+			}
+
+			public void Check(Statistics stats) {
+				var total = stats.GetTotalCount();
+				if ((total - Last) / CheckPeriod > Reach)
+				{
+					try {
+						ReachHandle.run();
+					} catch (Throwable e) {
+						logger.error("ProcedureStatistics.Watcher", e);
+					}
+				}
+				Last = total;
+			}
 		}
 	}
 }
