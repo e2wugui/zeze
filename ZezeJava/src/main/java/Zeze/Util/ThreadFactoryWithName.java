0067a51b591b0ffa602e03145aace581e4cdb480
zhenyu.yin <zhidong44061@163.com>
2021-12-31 10:30:25 +0800

    ThreadFactoryWithName newThread改为守护线程

--- ZezeJava/src/main/java/Zeze/Util/ThreadFactoryWithName.java
+++ ZezeJava/src/main/java/Zeze/Util/ThreadFactoryWithName.java
@@ -17,8 +17,7 @@ public class ThreadFactoryWithName implements ThreadFactory {
 
     public Thread newThread(Runnable r) {
         Thread t = new Thread(group, r, namePrefix + threadNumber.getAndIncrement(), 0);
-        if (t.isDaemon())
-            t.setDaemon(false);
+        t.setDaemon(true);
         if (t.getPriority() != Thread.NORM_PRIORITY)
             t.setPriority(Thread.NORM_PRIORITY);
         return t;
