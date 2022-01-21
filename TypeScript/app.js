59a03b37e373db75a253d4140bf40715433b00a5
dwing <dwing4g@gmail.com>
2022-01-21 12:26:02 +0800

    不提交ts编译的结果(*.js,*.js.map)

--- TypeScript/app.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const test_1 = require("test");
-console.log(new test_1.Test('').test());
-//# sourceMappingURL=app.js.map
