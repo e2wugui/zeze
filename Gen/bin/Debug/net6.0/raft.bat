098250ab83b8fe658eed065ea787bfcf803bb605
dwing <dwing4g@gmail.com>
2022-02-17 11:20:19 +0800

    测试脚本给每个控制台窗口设置标题

--- Gen/bin/Debug/net6.0/raft.bat
+++ Gen/bin/Debug/net6.0/raft.bat
@@ -1,16 +1,19 @@
+@echo off
+setlocal
+pushd %~dp0
 
 del *.log
 
-start gen -c RaftTest -Config raft.xml
+start "raft.xml"   gen -c RaftTest -Config raft.xml
 
-start gen -c RaftTest -Config raft3.xml
-start gen -c RaftTest -Config raft6.xml
-start gen -c RaftTest -Config raft9.xml
+start "raft3.xml"  gen -c RaftTest -Config raft3.xml
+start "raft6.xml"  gen -c RaftTest -Config raft6.xml
+start "raft9.xml"  gen -c RaftTest -Config raft9.xml
 
-start gen -c RaftTest -Config raft12.xml
-start gen -c RaftTest -Config raft15.xml
-start gen -c RaftTest -Config raft18.xml
+start "raft12.xml" gen -c RaftTest -Config raft12.xml
+start "raft15.xml" gen -c RaftTest -Config raft15.xml
+start "raft18.xml" gen -c RaftTest -Config raft18.xml
 
-start gen -c RaftTest -Config raft21.xml
-start gen -c RaftTest -Config raft24.xml
-start gen -c RaftTest -Config raft27.xml
+start "raft21.xml" gen -c RaftTest -Config raft21.xml
+start "raft24.xml" gen -c RaftTest -Config raft24.xml
+start "raft27.xml" gen -c RaftTest -Config raft27.xml
