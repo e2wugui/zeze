2fc86ac78167b797f04186c85194e18afadc2ccd
lichenghua <e2wugui@163.com>
2022-02-24 16:26:52 +0800

    raft add 5 nodes test config for debug and release

---
+++ Gen/bin/Debug/net6.0/raft.5x6node.bat
@@ -0,0 +1,19 @@
+@echo off
+setlocal
+pushd %~dp0
+
+del raft.debug.5x6node.6500_6504.xml.log
+start "raft.debug.5x6node.6500_6504.xml"   gen -c RaftTest -Config raft.debug.5x6node.6500_6504.xml
+
+del raft.debug.5x6node.6505_6509.xml.log
+start "raft.debug.5x6node.6505_6509.xml"  gen -c RaftTest -Config raft.debug.5x6node.6505_6509.xml
+
+del raft.debug.5x6node.6510_6514.xml.log
+start "raft.debug.5x6node.6510_6514.xml"  gen -c RaftTest -Config raft.debug.5x6node.6510_6514.xml
+
+del raft.debug.5x6node.6515_6519.xml.log
+start "raft.debug.5x6node.6515_6519.xml"  gen -c RaftTest -Config raft.debug.5x6node.6515_6519.xml
+
+del raft.debug.5x6node.6520_6524.xml.log
+start "raft.debug.5x6node.6520_6524.xml"  gen -c RaftTest -Config raft.debug.5x6node.6520_6524.xml
+
