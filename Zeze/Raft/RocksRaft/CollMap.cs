c6498eabb0b3a877b9c6234e734450240caa21fc
lichenghua <e2wugui@163.com>
2022-02-28 14:34:30 +0800

    RocksRaft 2

--- Zeze/Raft/RocksRaft/CollMap.cs
+++ Zeze/Raft/RocksRaft/CollMap.cs
@@ -9,12 +9,12 @@ namespace Zeze.Raft.RocksRaft
 {
 	public abstract class CollMap<K, V> : Collection
 	{
-		protected ImmutableDictionary<K, V> map;
+		internal ImmutableDictionary<K, V> map;
 
 		public abstract V Get(K key);
 		public abstract void Put(K key, V value);
 		public abstract void Remove(K key);
-		public abstract void Apply(LogMap1<K, V> log);
+		public abstract void Apply(LogMap log);
 
 		internal V _Get(K key)
 		{
--- Zeze/Raft/RocksRaft/Log.cs
+++ Zeze/Raft/RocksRaft/Log.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,16 +10,38 @@ namespace Zeze.Raft.RocksRaft
 {
 	public abstract class Log : Serializable
 	{
-		// 事务运行时属性，不会被系列化。当 Decode，Bean为null。
+		// 事务运行时属性，不会被系列化。
+		// 当 Decode，Bean为null。
+		// Apply通过参数得到日志应用需要的Bean。
 		public Bean Bean { get; set; }
 		public long LogKey => Bean.ObjectId + VariableId;
+		public virtual void Collect(Changes changes, Log vlog)
+		{
+			// LogBean LogCollection 需要实现这个方法收集日志.
+		}
 
 		// 会被系列化，实际上由LogBean管理。
+		private readonly int _TypeId;
+		public virtual int TypeId => _TypeId;
 		public int VariableId { get; set; }
+
+		public Log()
+        {
+			_TypeId = Zeze.Transaction.Bean.Hash32(GetType().FullName);
+        }
 
 		public abstract void Apply(Bean holder);
 		public abstract void Encode(ByteBuffer bb);
 		public abstract void Decode(ByteBuffer bb);
+
+		public static ConcurrentDictionary<int, Func<Log>> Factorys { get; } = new ConcurrentDictionary<int, Func<Log>>();
+
+		public static Log Create(int typeId)
+        {
+			if (Factorys.TryGetValue(typeId, out var factory))
+				return factory();
+			throw new Exception($"unkown log typeid={typeId}");
+		}
 	}
 
 	// 简单类型日志辅助。
