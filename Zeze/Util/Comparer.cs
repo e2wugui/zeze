41fbdfcd4e6b21832a8dac5ecf198408c3040629
lichenghua <e2wugui@163.com>
2022-03-03 19:27:27 +0800

    add Comparer.cs

---
+++ Zeze/Util/Comparer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zeze.Util
+{
+	public sealed class ComparerInt : IComparer<int>
+	{
+		public readonly static IComparer<int> Instance = new ComparerInt();
+
+		public int Compare(int x, int y)
+		{
+			return x.CompareTo(y);
+		}
+	}
+
+	public sealed class ComparerLong : IComparer<long>
+	{
+		public readonly static IComparer<long> Instance = new ComparerLong();
+
+		public int Compare(long x, long y)
+		{
+			return x.CompareTo(y);
+		}
+	}
+
+	public sealed class ComparerString : IComparer<string>
+	{
+		public readonly static IComparer<string> Instance = new ComparerString();
+
+		public int Compare(string x, string y)
+		{
+			return x.CompareTo(y);
+		}
+	}
+}
