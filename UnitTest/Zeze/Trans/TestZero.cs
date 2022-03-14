9877e479ab480be368faa40d11c50f03014468c9
e2wugui <e2wugui@163.com>
2022-03-14 16:29:38 +0800

    TestZero

---
+++ UnitTest/Zeze/Trans/TestZero.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.Zeze.Trans
+{
+    [TestClass]
+    public class TestZero
+    {
+        [TestMethod]
+        public void Go()
+        {
+            demo.App.Instance.Start();
+            try
+            {
+                demo.App.Instance.Zeze.NewProcedure(() =>
+                {
+                    demo.App.Instance.demo_Module1.Table1.Remove(1);
+                    return 0;
+                }, "go").Call();
+            }
+            finally
+            {
+                demo.App.Instance.Stop();
+            }
+        }
+    }
+}
