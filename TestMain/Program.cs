ba66fff15b438bd9ef30d31de98ba7dc09a2cb84
e2wugui <e2wugui@163.com>
2022-03-17 13:43:51 +0800

    Program.cs 写法恢复旧模式。

--- TestMain/Program.cs
+++ TestMain/Program.cs
@@ -1,48 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 
-string command = "";
-for (int i = 0; i < args.Length; ++i)
+public class Program
 {
-    if (args[i].Equals("-c"))
-        command = args[++i];
-}
+    public static void Main(string[] args)
+    {
 
-switch (command)
-{
-    case "go":
-        new UnitTest.Zeze.Trans.TestZero().Go();
-        break;
+        string command = "";
+        for (int i = 0; i < args.Length; ++i)
+        {
+            if (args[i].Equals("-c"))
+                command = args[++i];
+        }
 
-    case "BenchOneThread":
-        new Benchmark.ABasicSimpleAddOneThread().testBenchmark();
-        break;
+        switch (command)
+        {
+            case "go":
+                new UnitTest.Zeze.Trans.TestZero().Go();
+                break;
 
-    case "BenchConflict":
-        new Benchmark.BBasicSimpleAddConcurrentWithConflict().testBenchmark();
-        break;
+            case "BenchOneThread":
+                new Benchmark.ABasicSimpleAddOneThread().testBenchmark();
+                break;
 
-    case "BenchConcurrent":
-        new Benchmark.CBasicSimpleAddConcurrent().testBenchmark();
-        break;
+            case "BenchConflict":
+                new Benchmark.BBasicSimpleAddConcurrentWithConflict().testBenchmark();
+                break;
 
-    case "TestGlobal":
-        new UnitTest.Zeze.Trans.TestGlobal().Test2App();
-        break;
+            case "BenchConcurrent":
+                new Benchmark.CBasicSimpleAddConcurrent().testBenchmark();
+                break;
 
-    case "TestSocketBeginXXX":
-        new UnitTest.Zeze.Net.TestSocketBeginXXX().Test();
-        break;
+            case "TestGlobal":
+                new UnitTest.Zeze.Trans.TestGlobal().Test2App();
+                break;
 
-    case "TikvTest":
-        Zeze.Tikv.Test.Run(args[0]);
-        break;
+            case "TestSocketBeginXXX":
+                new UnitTest.Zeze.Net.TestSocketBeginXXX().Test();
+                break;
 
-    case "RaftTest":
-    case "RaftDump":
-        new Zeze.Raft.Test().Run(command, args);
-        break;
+            case "TikvTest":
+                Zeze.Tikv.Test.Run(args[0]);
+                break;
 
-    case "RocksRaft":
-        new Zeze.Raft.RocksRaft.Test().Test_1();
-        break;
+            case "RaftTest":
+            case "RaftDump":
+                new Zeze.Raft.Test().Run(command, args);
+                break;
+
+            case "RocksRaft":
+                new Zeze.Raft.RocksRaft.Test().Test_1();
+                break;
+        }
+    }
 }
