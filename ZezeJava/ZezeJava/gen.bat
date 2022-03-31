7438a49e1f913770d00858c91d07e9248c303c8e
dwing <dwing4g@gmail.com>
2022-03-31 17:48:19 +0800

    ZezeJava: gen.bat切换成publish版本

--- ZezeJava/ZezeJava/gen.bat
+++ ZezeJava/ZezeJava/gen.bat
@@ -2,8 +2,8 @@
 setlocal
 pushd %~dp0
 
-rem ..\..\publish\Gen.exe solution.zeze.xml
+..\..\publish\Gen.exe solution.zeze.xml
 
-..\..\Gen\bin\Debug\net6.0\Gen.exe solution.zeze.xml
+rem ..\..\Gen\bin\Debug\net6.0\Gen.exe solution.zeze.xml
 
 pause
