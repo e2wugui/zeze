a553fb4faa0ba24b374d51d089e887bcf81f9a74
e2wugui <e2wugui@163.com>
2022-04-01 13:32:40 +0800

    手动初始化模块。 Provider

---
+++ ZezeJava/ZezexJava/server/src/Game/Provider.java
@@ -0,0 +1,67 @@
+package Game;
+
+import Zeze.Beans.Provider.*;
+import Zeze.Transaction.Procedure;
+
+
+public final class Provider extends Zeze.Arch.ProviderImplement {
+
+	public Game.App App;
+	public Provider(Game.App app) {
+		App = app;
+	}
+
+	public void Start(Game.App app) {
+	}
+
+	public void Stop(Game.App app) {
+	}
+
+	@Override
+	protected long ProcessLinkBroken(LinkBroken protocol) throws Throwable {
+		// 目前仅需设置online状态。
+		if (false == protocol.Argument.getStates().isEmpty()) {
+			var roleId = protocol.Argument.getStates().get(0);
+			Game.App.getInstance().Game_Login.getOnlines().OnLinkBroken(roleId);
+		}
+		return Procedure.Success;
+	}
+
+	@Override
+	protected long ProcessTransmit(Transmit p) throws Throwable {
+		Zeze.Serialize.Serializable parameter = null;
+		if (false == p.Argument.getParameterBeanName().isEmpty())
+		{
+			var factory = App.Game_Login.getOnlines().getTransmitParameterFactorys().get(p.Argument.getParameterBeanName());
+			if (null == factory)
+				return ErrorCode(ErrorTransmitParameterFactoryNotFound);
+
+			parameter = factory.call(p.Argument.getParameterBeanName());
+		}
+		App.Game_Login.getOnlines().ProcessTransmit(
+				p.Argument.getSender(),
+				p.Argument.getActionName(),
+				p.Argument.getRoles().keySet(),
+				parameter);
+		return Procedure.Success;
+	}
+
+	@Override
+	protected long ProcessAnnounceLinkInfo(AnnounceLinkInfo protocol) throws Throwable {
+		var linkSession = (Game.Server.LinkSession)protocol.getSender().getUserState();
+		linkSession.Setup(protocol.Argument.getLinkId(), protocol.Argument.getProviderSessionId());
+		return Procedure.Success;
+	}
+
+	@Override
+	protected long ProcessSendConfirm(SendConfirm protocol) throws Throwable {
+		var linkSession = (Game.Server.LinkSession)protocol.getSender().getUserState();
+		var ctx = App.Server.<Game.Login.Onlines.ConfirmContext>TryGetManualContext(
+				protocol.Argument.getConfirmSerialId());
+		if (ctx != null) {
+			ctx.ProcessLinkConfirm(linkSession.getName());
+		}
+		// linkName 也可以从 protocol.Sender.Connector.Name 获取。
+		return Procedure.Success;
+	}
+}
