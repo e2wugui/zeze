b47f79941fd4adff86b0db699192f5990e0dc68d
e2wugui <e2wugui@163.com>
2022-04-17 07:17:05 +0800

    RedoQueue

---
+++ Zeze/Component/RedoQueue.cs
@@ -0,0 +1,217 @@
+using RocksDbSharp;
+using System;
+using System.Collections.Concurrent;
+using System.Text;
+using System.Threading.Tasks;
+using Zeze.Beans.RedoQueue;
+using Zeze.Net;
+using Zeze.Serialize;
+using Zeze.Transaction;
+using Zeze.Util;
+
+namespace Zeze.Component
+{
+
+	/*
+	 * 连接：
+	 * 1.每个队列一个连接服务。
+	 * 2.可以从可用的zeze-server中选择部分，配置到zeze.xml中。
+	 * 3.【可选】使用ServiceManager动态发现zeze-server。感觉没有必要。
+	 */
+	public class RedoQueue : Zeze.Services.HandshakeClient
+	{
+		private Nito.AsyncEx.AsyncLock Mutex = new();
+
+		public RedoQueue(string name, Zeze.Config config)
+			: base(name, config)
+		{
+		}
+
+		static AsyncExecutor AsyncExecutor = new(() => 100);
+
+		public override async void Start()
+		{
+			using (Mutex.Lock())
+			{
+				if (null != Db)
+					return;
+
+				var dbOptions = new DbOptions().SetCreateIfMissing(true);
+				var dbHome = Name;
+				var columnFamilies = new ColumnFamilies();
+				foreach (var cf in RocksDb.ListColumnFamilies(dbOptions, dbHome))
+				{
+					columnFamilies.Add(cf, CfOptions);
+				}
+				// DirectOperates 依赖 Db，所以只能在这里打开。要不然，放在Open里面更加合理。
+				Db = await AsyncRocksDb.OpenAsync(dbOptions, dbHome, columnFamilies, AsyncExecutor);
+				foreach (var f in columnFamilies)
+				{
+					Families[f.Name] = new(Db.RocksDb.GetColumnFamily(f.Name));
+				}
+				FamilyLastDoneTaskId = await GetOrAddFamily("FamilyLastDoneTaskId");
+				FamilyTaskQueue = await GetOrAddFamily("FamilyTaskQueue");
+				using (var qit = Db.RocksDb.NewIterator(FamilyTaskQueue))
+				{
+					qit.SeekToLast();
+					if (qit.Valid())
+					{
+						var last = ByteBuffer.Wrap(qit.Key());
+						LastTaskId = last.ReadLong();
+					}
+				}
+				var done = await Db.GetAsync(LastDoneTaskIdKey, LastDoneTaskIdKey.Length, FamilyLastDoneTaskId);
+				if (done != null)
+				{
+					LastDoneTaskId = ByteBuffer.Wrap(done).ReadLong();
+				}
+				base.Start();
+
+			}
+		}
+
+		public override void Stop()
+		{
+			base.Stop();
+			using (Mutex.Lock())
+			{
+
+				Db.RocksDb.Dispose();
+				Db = null;
+			}
+		}
+
+		public async Task AddAsync(int taskType, Zeze.Serialize.Serializable taskParam)
+		{
+			using (await Mutex.LockAsync())
+			{
+				var key = ByteBuffer.Allocate(16);
+				++LastTaskId;
+				key.WriteLong(LastTaskId);
+
+				var task = new BQueueTask();
+				task.QueueName = Name;
+				task.PrevTaskId = LastTaskId - 1;
+				task.TaskId = LastTaskId;
+				task.TaskType = taskType;
+				var value = ByteBuffer.Allocate(1024 + 16);
+				task.Encode(value);
+
+				// 保存完整的rpc请求，重新发送的时候不用再次打包。
+				await Db.PutAsync(key.Bytes, key.Size, value.Bytes, value.Size, FamilyTaskQueue, WriteOptions);
+				await TryStartSendNextTask(task, null);
+			}
+		}
+
+		private RunTask Pending;
+		private AsyncSocket Socket;
+
+		private async Task TryStartSendNextTask(BQueueTask add, AsyncSocket socket)
+		{
+			if (null != Pending)
+				return;
+
+			if (LastDoneTaskId < LastTaskId) {
+				var taskId = LastDoneTaskId + 1;
+				var rpc = new RunTask();
+				if (add != null && taskId == add.TaskId) {
+					rpc.Argument = add; // 最近加入的就是要发送的。优化！
+				} else {
+					// 最近加入的不是要发送的，从Db中读取。
+					var key = ByteBuffer.Allocate(16);
+					key.WriteLong(taskId);
+					var value = await Db.GetAsync(key.Bytes, key.Size, FamilyTaskQueue, ReadOptions);
+					if (null == value)
+						return; // error
+					rpc.Argument.Decode(ByteBuffer.Wrap(value));
+				}
+				if (null == Socket) {
+					Socket = socket;
+					if (null == Socket) {
+						Socket = GetSocket();
+						if (null == Socket)
+							return;
+					}
+				}
+				if (rpc.Send(Socket, ProcessRunTaskResult))
+					Pending = rpc;
+			}
+		}
+
+		private async Task<long> ProcessRunTaskResult(Protocol p)
+		{
+			var rpc = p as RunTask;
+			if (Pending != rpc)
+				return Procedure.LogicError;
+
+			Pending = null;
+			if (rpc.ResultCode == 0L || rpc.ResultCode == Procedure.ErrorRequestId)
+			{
+				LastDoneTaskId = rpc.Result.TaskId;
+				var value = ByteBuffer.Allocate(16);
+				value.WriteLong(LastDoneTaskId);
+				await Db.PutAsync(LastDoneTaskIdKey, LastDoneTaskIdKey.Length, value.Bytes, value.Size, FamilyLastDoneTaskId, WriteOptions);
+				await TryStartSendNextTask(null, rpc.Sender);
+				return 0L;
+			}
+
+			return rpc.ResultCode;
+		}
+
+		public override async void OnHandshakeDone(AsyncSocket sender)
+		{
+			base.OnHandshakeDone(sender);
+			using (await Mutex.LockAsync())
+			{
+				await TryStartSendNextTask(null, sender);
+			}
+		}
+
+		public override async void OnSocketClose(AsyncSocket socket, Exception ex)
+		{
+			base.OnSocketClose(socket, ex);
+			using (await Mutex.LockAsync())
+			{ 
+				if (Socket == socket) {
+					Socket = null;
+				}
+			}
+		}
+
+		public class ColumnFamilyAsync
+        {
+			public async Task TryCreateAsync(RedoQueue rq, string name)
+            {
+				using (await rq.Mutex.LockAsync())
+                {
+					if (Handle == null)
+					Handle = await rq.Db.CreateColumnFamily(rq.CfOptions, name);
+				}
+			}
+			public ColumnFamilyHandle Handle { get; set; }
+
+			public ColumnFamilyAsync(ColumnFamilyHandle handle = null)
+            {
+				Handle = handle;
+            }
+		}
+
+		private AsyncRocksDb Db;
+		private ColumnFamilyOptions CfOptions = new ColumnFamilyOptions();
+		public WriteOptions WriteOptions = new WriteOptions();
+		public ReadOptions ReadOptions = new ReadOptions();
+		private ConcurrentDictionary<string, ColumnFamilyAsync> Families = new();
+		private ColumnFamilyHandle FamilyLastDoneTaskId;
+		private ColumnFamilyHandle FamilyTaskQueue;
+		private long LastTaskId;
+		private long LastDoneTaskId;
+		private byte[] LastDoneTaskIdKey = Encoding.UTF8.GetBytes("LastDoneTaskId");
+
+		async Task<ColumnFamilyHandle> GetOrAddFamily(string name)
+		{
+			var cf = Families.GetOrAdd(name, (key) => new ColumnFamilyAsync());
+			await cf.TryCreateAsync(this, name);
+			return cf.Handle;
+		}
+	}
+}
--- Zeze/Component/RedoQueueServer.cs
+++ Zeze/Component/RedoQueueServer.cs
@@ -1,13 +1,77 @@
 
+using System;
+using System.Collections.Concurrent;
+using Zeze.Beans.RedoQueue;
+using Zeze.Net;
+using Zeze.Transaction;
+using Zeze.Util;
+
 namespace Zeze.Component
 {
-    public class RedoQueueServer : AbstractRedoQueueServer
-    {
-        protected override async System.Threading.Tasks.Task<long> ProcessRunTaskRequest(Zeze.Net.Protocol _p)
-        {
-            var p = _p as Zeze.Beans.RedoQueue.RunTask;
-            return Zeze.Transaction.Procedure.NotImplement;
-        }
+	public class RedoQueueServer : AbstractRedoQueueServer
+	{
+		protected override async System.Threading.Tasks.Task<long> ProcessRunTaskRequest(Zeze.Net.Protocol _p)
+		{
+			var r = _p as RunTask;
+			Zeze.Transaction.Transaction.Current.RunWhileCommit(() => r.SendResult());
 
-    }
+			var last = await _tQueueLastTaskId.GetOrAddAsync(r.Argument.QueueName);
+			r.Result.TaskId = last.TaskId;
+			if (r.Argument.PrevTaskId != last.TaskId)
+				return Procedure.ErrorRequestId;
+			if (!handles.TryGetValue(r.Argument.QueueName, out var queue))
+				return Procedure.NotImplement;
+			if (!queue.TryGetValue(r.Argument.TaskType, out var handle))
+				return Procedure.NotImplement;
+			if (!handle(r.Argument.TaskParam))
+				return Procedure.LogicError;
+			last.TaskId = r.Argument.TaskId;
+			r.Result.TaskId = last.TaskId;
+			return Procedure.Success;
+		}
+
+		private readonly ConcurrentDictionary<string, ConcurrentDictionary<int, Func<Binary, bool>>> handles = new();
+		private Server server;
+
+		public RedoQueueServer(Zeze.Application zeze)
+		{
+			server = new Server(zeze);
+			RegisterProtocols(server);
+			RegisterZezeTables(zeze);
+		}
+
+		public void Start()
+		{
+			server.Start();
+		}
+
+		public void Stop()
+		{
+			server.Stop();
+		}
+
+		/**
+		 * ×¢²áÈÎÎñ£¬
+		 */
+		public void Register(String queue, int type, Func<Binary, bool> task)
+		{
+			if (!handles.GetOrAdd(queue, (key) => new()).TryAdd(type, task))
+				throw new Exception("duplicate task type. " + type);
+		}
+
+		public class Server : Zeze.Services.HandshakeServer
+		{
+			public Server(Zeze.Application zeze)
+				: base("RedoQueueServer", zeze)
+			{
+			}
+
+			public override void DispatchProtocol(Protocol p, ProtocolFactoryHandle factoryHandle)
+			{
+				var r = p as RunTask;
+				var proc = Zeze.NewProcedure(async () => await factoryHandle.Handle(p), $"RedoQueue={r.Argument.QueueName} RunTask={r.Argument.TaskType}");
+				_ = Mission.CallAsync(proc, p, (p, code) => p.SendResultCode(code)); // error result
+			}
+		}
+	}
 }
