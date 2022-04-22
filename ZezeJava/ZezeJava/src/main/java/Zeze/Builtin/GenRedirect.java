41ccdf936b002f66dbea69ce0c6c720788e3aca1
e2wugui <e2wugui@163.com>
2022-04-22 11:00:13 +0800

    GenRedirect

---
+++ ZezeJava/ZezeJava/src/main/java/Zeze/Builtin/GenRedirect.java
@@ -0,0 +1,13 @@
+package Zeze.Builtin;
+
+/**
+ * 1. 所有需要Redirect支持的内建模块在这里生成代码。
+ * 2. 生成的代码提交到代码库中。
+ * 3. 该模块创建的时候提供方法，直接创建生成的重载类。不需要用户生成代码。
+ * 4. 这个类创建原始内建模块实例，并执行ReplaceModuleInstance生成代码。
+ */
+public class GenRedirect {
+	public static void main(String args[]) {
+
+	}
+}
