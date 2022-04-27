12b9e5511ed73b8e1c78f581ab63e2bcf117ab75
e2wugui <e2wugui@163.com>
2022-04-27 12:37:40 +0800

    TestOnline Client.App Init。

--- ZezeJava/ZezeJavaTest/src/Zezex/TestOnline.java
+++ ZezeJava/ZezeJavaTest/src/Zezex/TestOnline.java
@@ -4,12 +4,14 @@ import Zeze.Net.AsyncSocket;
 import junit.framework.TestCase;
 
 public class TestOnline extends TestCase {
+	Client.App client1;
 	Zezex.App link1;
 	Game.App  server1;
 	Game.App  server2;
 
 	@Override
 	protected void setUp() {
+		client1 = Client.App.Instance;
 		link1 = App.Instance;
 		server1 = Game.App.Instance;
 		server2 = new Game.App();
@@ -20,6 +22,7 @@ public class TestOnline extends TestCase {
 			server2.Start(new String[]{"-ServerId", "1", "-ProviderDirectPort", "20002"});
 
 			Thread.sleep(2000); // wait server register ready
+			client1.Start();
 		} catch (Throwable ex) {
 			throw new RuntimeException(ex);
 		}
@@ -29,6 +32,7 @@ public class TestOnline extends TestCase {
 	protected void tearDown() {
 		System.out.println("Begin Stop");
 		try {
+			client1.Stop();
 			server1.Stop();
 			server2.Stop();
 			link1.Stop();
