30c3423b94d7a541c3cdce306e84c4b559adf074
e2wugui <e2wugui@163.com>
2022-05-06 08:38:51 +0800

    Arch.ProviderDirectWithTransmit.cs ProviderImplementWithOnline.cs

---
+++ Zeze/Arch/ProviderDirectWithTransmit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zeze.Arch;
+using Zeze.Builtin.ProviderDirect;
+using Zeze.Net;
+using Zeze.Transaction;
+
+namespace Zeze.Arch
+{
+    public class ProviderDirectWithTransmit : ProviderDirect
+    {
+        protected override async Task<long> ProcessTransmitAccount(Protocol _p)
+        {
+			var p = _p as TransmitAccount;
+			var provider = ProviderApp.ProviderImplement as ProviderImplementWithOnline;
+			provider.Online.ProcessTransmit(p.Argument.SenderAccount, p.Argument.SenderClientId,
+				p.Argument.ActionName, p.Argument.TargetAccounts, p.Argument.Parameter);
+			return Procedure.Success;
+		}
+	}
+}
---
+++ Zeze/Arch/ProviderImplementWithOnline.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zeze.Arch;
+using Zeze.Builtin.Provider;
+using Zeze.Net;
+using Zeze.Transaction;
+
+namespace Zeze.Arch
+{
+    public class ProviderImplementWithOnline : Zeze.Arch.ProviderImplement
+    {
+        public Online Online { get; set; }
+
+        protected override async Task<long> ProcessLinkBroken(Protocol _p)
+        {
+            var p = _p as LinkBroken;
+            // 目前仅需设置online状态。
+            if (false == string.IsNullOrEmpty(p.Argument.Context))
+            {
+                await Online.OnLinkBroken(p.Argument.Account, p.Argument.Context, p.Argument);
+            }
+            return Procedure.Success;
+        }
+    }
+}
