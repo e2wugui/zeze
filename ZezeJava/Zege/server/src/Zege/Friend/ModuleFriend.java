4cbe569d69bd42f74525d2b7afcf44c6719185ac
e2wugui <e2wugui@163.com>
2022-05-23 15:19:13 +0800

    ModuleFriend add some protocol

--- ZezeJava/Zege/server/src/Zege/Friend/ModuleFriend.java
+++ ZezeJava/Zege/server/src/Zege/Friend/ModuleFriend.java
@@ -7,6 +7,36 @@ public class ModuleFriend extends AbstractModule {
     public void Stop(Zege.App app) throws Throwable {
     }
 
+    @Override
+    protected long ProcessAddFriendRequest(Zege.Friend.AddFriend r) {
+        return Zeze.Transaction.Procedure.NotImplement;
+    }
+
+    @Override
+    protected long ProcessCreateDepartmentRequest(Zege.Friend.CreateDepartment r) {
+        return Zeze.Transaction.Procedure.NotImplement;
+    }
+
+    @Override
+    protected long ProcessDeleteDepartmentRequest(Zege.Friend.DeleteDepartment r) {
+        return Zeze.Transaction.Procedure.NotImplement;
+    }
+
+    @Override
+    protected long ProcessGetDepartmentNodeRequest(Zege.Friend.GetDepartmentNode r) {
+        return Zeze.Transaction.Procedure.NotImplement;
+    }
+
+    @Override
+    protected long ProcessGetFriendNodeRequest(Zege.Friend.GetFriendNode r) {
+        return Zeze.Transaction.Procedure.NotImplement;
+    }
+
+    @Override
+    protected long ProcessMoveDepartmentRequest(Zege.Friend.MoveDepartment r) {
+        return Zeze.Transaction.Procedure.NotImplement;
+    }
+
     // ZEZE_FILE_CHUNK {{{ GEN MODULE @formatter:off
     public ModuleFriend(Zege.App app) {
         super(app);
