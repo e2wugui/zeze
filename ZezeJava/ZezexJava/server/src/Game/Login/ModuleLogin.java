ad8f287c8dc4b538b143db990d3dcba91c8e7221
dwing <dwing4g@gmail.com>
2022-05-24 17:33:29 +0800

    ZezeJava: ProcessCreateRoleRequest的回复时机改成提交后

--- ZezeJava/ZezexJava/server/src/Game/Login/ModuleLogin.java
+++ ZezeJava/ZezexJava/server/src/Game/Login/ModuleLogin.java
@@ -42,7 +42,7 @@ public final class ModuleLogin extends AbstractModule {
 		// ...
 
 		rpc.Result = role;
-		session.SendResponse(rpc);
+		session.SendResponseWhileCommit(rpc);
 		return Procedure.Success;
 	}
 
