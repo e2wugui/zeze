fc553ef12a899e0ba8c94cddd5779472b94e05a2
dwing <dwing4g@gmail.com>
2022-06-27 14:45:07 +0800

    ZezeJava: 简化 EventDispatcher 的实现

--- ZezeJava/ZezeJava/src/main/java/Zeze/Util/EventDispatcher.java
+++ ZezeJava/ZezeJava/src/main/java/Zeze/Util/EventDispatcher.java
@@ -1,8 +1,6 @@
 package Zeze.Util;
 
-import java.util.ArrayList;
 import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.locks.ReentrantLock;
 import Zeze.Application;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -28,40 +26,10 @@ public class EventDispatcher {
 		RunThread,
 	}
 
-	public static final class Events extends ArrayList<EventHandle> {
-		private final ReentrantLock lock = new ReentrantLock();
-		private final ConcurrentLinkedQueue<EventHandle> delayRemoves = new ConcurrentLinkedQueue<>();
-
-		public void remove(EventHandle handle) {
-			delayRemoves.offer(handle);
-			do {
-				if (lock.isLocked() || !lock.tryLock()) // 任何线程持有锁(包括当前线程),或者尝试获取锁失败(有小概率恰好被抢走)
-					return;
-				try {
-					for (EventHandle h; (h = delayRemoves.poll()) != null; )
-						super.remove(h);
-				} finally {
-					lock.unlock();
-				}
-			} while (!delayRemoves.isEmpty());
-		}
-
-		public void tryRemoveDelay() {
-			if (!delayRemoves.isEmpty() && !lock.isLocked() && lock.tryLock()) {
-				try {
-					for (EventHandle h; (h = delayRemoves.poll()) != null; )
-						super.remove(h);
-				} finally {
-					lock.unlock();
-				}
-			}
-		}
-	}
-
 	private final String name;
-	private final Events runEmbedEvents = new Events();
-	private final Events runProcedureEvents = new Events();
-	private final Events runThreadEvents = new Events();
+	private final ConcurrentLinkedQueue<EventHandle> runEmbedEvents = new ConcurrentLinkedQueue<>();
+	private final ConcurrentLinkedQueue<EventHandle> runProcedureEvents = new ConcurrentLinkedQueue<>();
+	private final ConcurrentLinkedQueue<EventHandle> runThreadEvents = new ConcurrentLinkedQueue<>();
 
 	public EventDispatcher(String name) {
 		this.name = name;
@@ -71,15 +39,15 @@ public class EventDispatcher {
 		return name;
 	}
 
-	public Events getRunEmbedEvents() {
+	public ConcurrentLinkedQueue<EventHandle> getRunEmbedEvents() {
 		return runEmbedEvents;
 	}
 
-	public Events getRunProcedureEvents() {
+	public ConcurrentLinkedQueue<EventHandle> getRunProcedureEvents() {
 		return runProcedureEvents;
 	}
 
-	public Events getRunThreadEvents() {
+	public ConcurrentLinkedQueue<EventHandle> getRunThreadEvents() {
 		return runThreadEvents;
 	}
 
@@ -89,7 +57,7 @@ public class EventDispatcher {
 	 * @return 如果需要取消注册，请保存返回值，并调用其cancel。
 	 */
 	public Canceler add(Mode mode, EventHandle handle) {
-		Events events;
+		ConcurrentLinkedQueue<EventHandle> events;
 		switch (mode) {
 		case RunEmbed:
 			events = runEmbedEvents;
@@ -103,12 +71,7 @@ public class EventDispatcher {
 		default:
 			throw new IllegalArgumentException("Unknown mode=" + mode);
 		}
-		events.lock.lock();
-		try {
-			events.add(handle);
-		} finally {
-			events.lock.unlock();
-		}
+		events.offer(handle);
 		return () -> events.remove(handle);
 	}
 
@@ -116,72 +79,40 @@ public class EventDispatcher {
 
 	// 启动新的线程执行。
 	public void triggerThread(Object sender, EventArgument arg) {
-		runThreadEvents.lock.lock();
-		try {
-			//noinspection ForLoopReplaceableByForEach
-			for (int i = 0, n = runThreadEvents.size(); i < n; i++) {
-				var handle = runThreadEvents.get(i);
-				Task.run(() -> handle.invoke(sender, arg), "EventDispatch." + name + ".runAsync");
-			}
-		} finally {
-			runThreadEvents.lock.unlock();
-			runThreadEvents.tryRemoveDelay();
-		}
+		for (EventHandle handle : runThreadEvents)
+			Task.run(() -> handle.invoke(sender, arg), "EventDispatch." + name + ".runAsync");
 	}
 
 	// 嵌入当前线程执行，所有错误都报告出去，如果需要对错误进行特别处理，需要自己遍历Handles手动触发。
 	public void triggerEmbed(Object sender, EventArgument arg) throws Throwable {
-		runEmbedEvents.lock.lock();
-		try {
-			//noinspection ForLoopReplaceableByForEach
-			for (int i = 0, n = runEmbedEvents.size(); i < n; i++)
-				runEmbedEvents.get(i).invoke(sender, arg);
-		} finally {
-			runEmbedEvents.lock.unlock();
-			runEmbedEvents.tryRemoveDelay();
-		}
+		for (EventHandle handle : runEmbedEvents)
+			handle.invoke(sender, arg);
 	}
 
 	// 在当前线程中，创建新的存储过程并执行，忽略所有错误。
 	public void triggerProcedureIgnoreError(Application app, Object sender, EventArgument arg) {
-		runProcedureEvents.lock.lock();
-		try {
-			//noinspection ForLoopReplaceableByForEach
-			for (int i = 0, n = runProcedureEvents.size(); i < n; i++) {
-				var handle = runProcedureEvents.get(i);
-				try {
-					app.NewProcedure(() -> {
-						handle.invoke(sender, arg);
-						return 0L;
-					}, "EventDispatcher.triggerProcedureIgnoreError").Call();
-					// 返回错误码时是逻辑错误，这里不需要记录日志。内部已经记录了。
-				} catch (Throwable ex) {
-					logger.error("EventDispatcher.triggerProcedureIgnoreError", ex); // 除了框架错误，一般情况下，错误不会到达这里。
-				}
+		for (EventHandle handle : runProcedureEvents) {
+			try {
+				app.NewProcedure(() -> {
+					handle.invoke(sender, arg);
+					return 0L;
+				}, "EventDispatcher.triggerProcedureIgnoreError").Call();
+				// 返回错误码时是逻辑错误，这里不需要记录日志。内部已经记录了。
+			} catch (Throwable ex) {
+				logger.error("EventDispatcher.triggerProcedureIgnoreError", ex); // 除了框架错误，一般情况下，错误不会到达这里。
 			}
-		} finally {
-			runProcedureEvents.lock.unlock();
-			runProcedureEvents.tryRemoveDelay();
 		}
 	}
 
 	// 在当前线程中，创建新的存储过程并执行，所有错误都报告出去，如果需要对错误进行特别处理，需要自己遍历Handles手动触发。
 	public void triggerProcedure(Application app, Object sender, EventArgument arg) throws Throwable {
-		runProcedureEvents.lock.lock();
-		try {
-			//noinspection ForLoopReplaceableByForEach
-			for (int i = 0, n = runProcedureEvents.size(); i < n; i++) {
-				var handle = runProcedureEvents.get(i);
-				var result = app.NewProcedure(() -> {
-					handle.invoke(sender, arg);
-					return 0L;
-				}, "EventDispatcher.triggerProcedure").Call();
-				if (result != 0)
-					throw new RuntimeException("Nest Call Fail: " + result);
-			}
-		} finally {
-			runProcedureEvents.lock.unlock();
-			runProcedureEvents.tryRemoveDelay();
+		for (EventHandle handle : runProcedureEvents) {
+			var result = app.NewProcedure(() -> {
+				handle.invoke(sender, arg);
+				return 0L;
+			}, "EventDispatcher.triggerProcedure").Call();
+			if (result != 0)
+				throw new RuntimeException("Nest Call Fail: " + result);
 		}
 	}
 }
