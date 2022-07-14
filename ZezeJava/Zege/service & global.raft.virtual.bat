eb7ab0ae22e93e432c6973085d9bdf6b60d739fe
e2wugui <e2wugui@163.com>
2022-07-14 16:03:16 +0800

    [doc] & update global raft virtual thread to new version

--- ZezeJava/Zege/service & global.raft.virtual.bat
+++ ZezeJava/Zege/service & global.raft.virtual.bat
@@ -6,4 +6,4 @@ set classes=../ZezeJava/build/classes/java/main
 start "ServiceManagerServer" java -Dlogname=ServiceManagerServer -cp %classes%;../ZezeJava/lib/*;. Zeze.Services.ServiceManagerServer
 
 set classes=../ZezeJava/build/classes/java/main
-start "GlobalCacheManagerRaft" "C:\Program Files\Eclipse Adoptium\jdk-19.0.0.27-hotspot\bin\java" -Dlogname=GlobalCacheManagerRaft --enable-preview -cp %classes%;../ZezeJava/lib/*;. Zeze.Services.GlobalCacheManagerServer -raft RunAllNodes
+start "GlobalCacheManagerRaft" "C:\Program Files\Eclipse Adoptium\jdk-19.0.0.30-hotspot\bin\java" -Dlogname=GlobalCacheManagerRaft --enable-preview -cp %classes%;../ZezeJava/lib/*;. Zeze.Services.GlobalCacheManagerServer -raft RunAllNodes
