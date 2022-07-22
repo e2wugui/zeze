d9d224f2c2d0499ffb28bbe9b6a859b3fe1a5216
e2wugui <e2wugui@163.com>
2022-07-22 09:28:07 +0800

    [debug] 联合运行两个测试BUG。增加不间断运行main。

---
+++ ZezeJava/ZezeJavaTest/src/Infinite/TwoTestBug.java
@@ -0,0 +1,28 @@
+package Infinite;
+
+public class TwoTestBug {
+	public static void main(String args[]) throws Throwable {
+		var i = 0;
+		while (true) {
+			Simulate.logger.fatal("----------- CBasicSimpleAddConcurrent " + i + " -----------");
+			var test1 = new CBasicSimpleAddConcurrent();
+			try {
+				test1.testBenchmark();
+			} catch (Throwable ex) {
+				Simulate.logger.fatal("CBasicSimpleAddConcurrent", ex);
+			}
+			Simulate.logger.fatal("----------- Simulate " + i + " -----------");
+			var simulate = new Simulate();
+			simulate.Infinite = false;
+			simulate.Before();
+			try {
+				simulate.testMain();
+			} catch (Throwable e) {
+				Simulate.logger.fatal("main exception:", e);
+				throw e;
+			} finally {
+				simulate.After();
+			}
+		}
+	}
+}
