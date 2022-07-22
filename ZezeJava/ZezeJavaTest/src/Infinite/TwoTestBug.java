f53413deda829523b8678a27de3d5405ad17bfca
e2wugui <e2wugui@163.com>
2022-07-22 10:05:11 +0800

    [debug] TwoTestBug: add end log

--- ZezeJava/ZezeJavaTest/src/Infinite/TwoTestBug.java
+++ ZezeJava/ZezeJavaTest/src/Infinite/TwoTestBug.java
@@ -3,26 +3,30 @@ package Infinite;
 public class TwoTestBug {
 	public static void main(String args[]) throws Throwable {
 		var i = 0;
-		while (true) {
-			Simulate.logger.fatal("----------- CBasicSimpleAddConcurrent " + i + " -----------");
-			var test1 = new CBasicSimpleAddConcurrent();
-			try {
-				test1.testBenchmark();
-			} catch (Throwable ex) {
-				Simulate.logger.fatal("CBasicSimpleAddConcurrent", ex);
+		try {
+			while (true) {
+				Simulate.logger.fatal("----------- CBasicSimpleAddConcurrent " + i + " -----------");
+				var test1 = new CBasicSimpleAddConcurrent();
+				try {
+					test1.testBenchmark();
+				} catch (Throwable ex) {
+					Simulate.logger.fatal("CBasicSimpleAddConcurrent", ex);
+				}
+				Simulate.logger.fatal("----------- Simulate " + i + " -----------");
+				var simulate = new Simulate();
+				simulate.Infinite = false;
+				simulate.Before();
+				try {
+					simulate.testMain();
+				} catch (Throwable e) {
+					Simulate.logger.fatal("main exception:", e);
+					throw e;
+				} finally {
+					simulate.After();
+				}
 			}
-			Simulate.logger.fatal("----------- Simulate " + i + " -----------");
-			var simulate = new Simulate();
-			simulate.Infinite = false;
-			simulate.Before();
-			try {
-				simulate.testMain();
-			} catch (Throwable e) {
-				Simulate.logger.fatal("main exception:", e);
-				throw e;
-			} finally {
-				simulate.After();
-			}
+		} finally {
+			Simulate.logger.fatal("----------- End " + i + " -----------");
 		}
 	}
 }
