457950355bc72f88d2f0d0582a2b7c28fb20deaf
e2wugui <e2wugui@163.com>
2022-07-28 16:37:39 +0800

    【DispatchMode注释】

--- ZezeJava/ZezeJava/src/main/java/Zeze/Transaction/DispatchMode.java
+++ ZezeJava/ZezeJava/src/main/java/Zeze/Transaction/DispatchMode.java
@@ -3,5 +3,5 @@ package Zeze.Transaction;
 public enum DispatchMode {
 	Normal, // Task.run: Task.getThreadPool().execute
 	Critical, // Task.getCriticalThreadPool().execute
-	Direct, // Direct Call In Io Thread
+	Direct, // Direct Run In Caller Thread
 }
