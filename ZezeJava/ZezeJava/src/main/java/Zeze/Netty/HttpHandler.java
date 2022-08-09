983abd7d40c03177eff72abbc205d6ab67ad5e17
e2wugui <e2wugui@163.com>
2022-08-09 13:58:48 +0800

    doc/zeze.docx

--- ZezeJava/ZezeJava/src/main/java/Zeze/Netty/HttpHandler.java
+++ ZezeJava/ZezeJava/src/main/java/Zeze/Netty/HttpHandler.java
@@ -7,9 +7,9 @@ import io.netty.handler.codec.http.HttpContent;
 
 public class HttpHandler {
 	public int MaxContentLength = 8192; // -1 表示不限制，按流处理。
-	public TransactionLevel Level = TransactionLevel.Serializable;
-	public DispatchMode Mode = DispatchMode.Normal;
-	// 普通请求处理函数，如果需要内部会自动把流合并到一个请求里面。
+	public TransactionLevel Level = TransactionLevel.Serializable; // 事务级别
+	public DispatchMode Mode = DispatchMode.Normal; // 线程派发模式
+	// 普通请求处理函数，不是流处理方式时，如果需要内部会自动把流合并到一个请求里面。
 	public HttpFullRequestHandle FullRequestHandle;
 	// 上行流处理函数。
 	public HttpBeginStreamHandle BeginStreamHandle;
