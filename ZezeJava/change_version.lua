f3a6e1f7e217f9026f26575cacd0b5b748d2302f
dwing <dwing4g@gmail.com>
2022-09-26 11:07:08 +0800

    ZezeJava: 更新版本号 1.0.9-SNAPSHOT; 更新依赖

--- ZezeJava/change_version.lua
+++ ZezeJava/change_version.lua
@@ -1,5 +1,5 @@
-local old_version = "1.0.7-SNAPSHOT"
-local new_version = "1.0.8-SNAPSHOT"
+local old_version = "1.0.8-SNAPSHOT"
+local new_version = "1.0.9-SNAPSHOT"
 
 local files = {
 	{ 1, "build.gradle" },
