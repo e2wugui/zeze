39a16c6fac1421a7c06e452320de5e607847d5c4
e2wugui <e2wugui@163.com>
2022-10-19 10:53:27 +0800

    Zege.Message/MessageDrawable.cs 重构

--- Zege/Zege/Message/MessageDrawable.cs
+++ Zege/Zege/Message/MessageDrawable.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Drawing;
-using Microsoft.Maui.Graphics;
 
 namespace Zege.Message
 {
@@ -12,113 +10,110 @@ namespace Zege.Message
     {
         public string Message { get; set; } = "啊一啊一啊一啊一啊一啊一啊一啊一啊一啊一啊一啊一啊一啊一啊一啊一啊一啊一";
 
+        private const double Margin = 5;
+        private bool Init = false;
+        private Microsoft.Maui.Graphics.Font Font;
+        private int FontSize;
+        private SizeF CharSize;
+
         private bool IsLatin(char c)
         {
             return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z';
         }
 
+        private void DrawLine(ICanvas canvas, string line, double x, ref double y)
+        {
+            canvas.DrawString(line, (float)x, (float)y, HorizontalAlignment.Left);
+            if (line.Length < Message.Length)
+            {
+                y += CharSize.Height + Margin;
+                Message = Message.Substring(line.Length);
+                return;
+            }
+            Message = string.Empty;
+        }
+
+        private void DrawWordWrapLine(ICanvas canvas, string line, double x, ref double y)
+        {
+            if (line.Length > 0 && IsLatin(line[line.Length - 1]))
+            {
+                // 最后一个是单词。
+                var nextCharIndex = line.Length;
+                if (nextCharIndex < Message.Length && false == IsLatin(Message[nextCharIndex]))
+                {
+                    // 下一个不是字母。可以马上画出。
+                    DrawLine(canvas, line, x, ref y);
+                    return;
+                }
+
+                // 往前找单词分割。
+                var i = line.Length - 1;
+                for (; i >= 0 && IsLatin(line[i]); i--)
+                {
+                    // search...
+                }
+                if (i < 0)
+                {
+                    // 很长的单词，一行都放不下。强制画出。
+                    DrawLine(canvas, line, x, ref y);
+                    return;
+                }
+                line = line.Substring(0, i + 1);
+                // 继续后面的正常画出。
+            }
+            // 正常画出。
+            DrawLine(canvas, line, x, ref y);
+        }
+
         public void Draw(ICanvas canvas, RectF dirtyRect)
         {
-            var font = Microsoft.Maui.Graphics.Font.Default;
-            var fontSize = 18;
-            canvas.Font = font;
-            canvas.FontSize = fontSize;
+            if (false == Init)
+            {
+                Init = true;
+                Font = Microsoft.Maui.Graphics.Font.Default;
+                FontSize = 16;
+
+                canvas.Font = Font;
+                canvas.FontSize = FontSize;
+
+                CharSize = canvas.GetStringSize("啊", Font, FontSize);
+            }
             canvas.FontColor = Colors.Blue;
             canvas.DrawRectangle(dirtyRect);
 
-            var charSize = canvas.GetStringSize("啊", font, fontSize);
-            var rectf = new Rect(dirtyRect.X, dirtyRect.Y, dirtyRect.Height, dirtyRect.Width / 2);
-            var charsLine = (int)(rectf.Width / charSize.Width);
+            var rect = new Rect(dirtyRect.X, dirtyRect.Y, dirtyRect.Width / 2, dirtyRect.Height);
+            var rectf = new Rect(rect.X + Margin, rect.Y + Margin, rect.Width - 2 * Margin, rect.Height);
+            var charsLine = (int)(rectf.Width / CharSize.Width);
             var x = rectf.X;
-            var y = rectf.Y + charSize.Height;
-            var done = false;
-            while (!done)
+            var y = rectf.Y + CharSize.Height;
+            while (false == string.IsNullOrEmpty(Message))
             {
                 var lenLine = charsLine;
                 var line = lenLine < Message.Length ? Message.Substring(0, lenLine) : Message;
-                var lineSize = canvas.GetStringSize(line, font, fontSize);
+                var lineSize = canvas.GetStringSize(line, Font, FontSize);
                 if (lineSize.Width < rectf.Width)
                 {
                     string lastLine;
-                    int lastLenLine;
                     while (true)
                     {
                         // 记住最后行，往后找，直到画不下，然后画出最后一行。
                         lastLine = line;
-                        lastLenLine = lenLine;
                         if (lenLine < Message.Length)
                         {
                             ++lenLine;
                             line = lenLine < Message.Length ? Message.Substring(0, lenLine) : Message;
-                            lineSize = canvas.GetStringSize(line, font, fontSize);
+                            lineSize = canvas.GetStringSize(line, Font, FontSize);
                             if (lineSize.Width > rectf.Width)
                             {
-                                if (lastLine.Length > 0 && IsLatin(lastLine[lastLine.Length - 1]))
-                                {
-                                    // 最后一个是单词。
-                                    var nextCharIndex = lenLine;
-                                    if (nextCharIndex < Message.Length && false == IsLatin(Message[nextCharIndex]))
-                                    {
-                                        // 下一个不是字母。可以马上画出。
-                                        canvas.DrawString(lastLine, (float)x, (float)y, HorizontalAlignment.Left);
-                                        if (lastLenLine < Message.Length)
-                                        {
-                                            Message = Message.Substring(lastLenLine);
-                                            y += charSize.Height;
-                                            break;
-                                        }
-                                        done = true;
-                                        break; // all done
-                                    }
-                                    else
-                                    {
-                                        // 往前找单词分割。
-                                        var i = lastLine.Length - 1;
-                                        for (; i >= 0 && IsLatin(lastLine[i]); i--)
-                                        { 
-                                        }
-                                        if (i < 0)
-                                        {
-                                            // 很长的单词，一行都放不下。强制画出。
-                                            canvas.DrawString(lastLine, (float)x, (float)y, HorizontalAlignment.Left);
-                                            if (lastLenLine < Message.Length)
-                                            {
-                                                Message = Message.Substring(lastLenLine);
-                                                y += charSize.Height;
-                                                break;
-                                            }
-                                            done = true;
-                                            break; // all done
-                                        }
-                                        lastLenLine = i + 1;
-                                        lastLine = lastLine.Substring(0, lastLenLine);
-                                        // 继续后面的正常画出。
-                                    }
-                                }
-                                // 正常画出。
-                                canvas.DrawString(lastLine, (float)x, (float)y, HorizontalAlignment.Left);
-                                if (lastLenLine < Message.Length)
-                                {
-                                    Message = Message.Substring(lastLenLine);
-                                    y += charSize.Height;
-                                    break;
-                                }
-                                done = true;
-                                break; // all done
+                                DrawWordWrapLine(canvas, lastLine, x, ref y);
+                                break;
                             }
                         }
                         else
                         {
-                            // 消息已经不够了。
-                            canvas.DrawString(lastLine, (float)x, (float)y, HorizontalAlignment.Left);
-                            if (lastLenLine < Message.Length)
-                            {
-                                Message = Message.Substring(lastLenLine);
-                                y += charSize.Height;
-                                break;
-                            }
-                            done = true;
-                            break; // all done
+                            // 消息已经不够长了。
+                            DrawLine(canvas, lastLine, x, ref y);
+                            break;
                         }
                     }
                 }
@@ -129,66 +124,17 @@ namespace Zege.Message
                         // 往前找，直到能画得下，然后画出当前行。
                         --lenLine;
                         line = lenLine < Message.Length ? Message.Substring(0, lenLine) : Message;
-                        lineSize = canvas.GetStringSize(line, font, fontSize);
+                        lineSize = canvas.GetStringSize(line, Font, FontSize);
                         if (lineSize.Width < rectf.Width)
                         {
-                            if (line.Length > 0 && IsLatin(line[line.Length - 1]))
-                            {
-                                // 最后一个字母是单词。
-                                var nextCharIndex = lenLine + 1;
-                                if (nextCharIndex < Message.Length && false == IsLatin(Message[nextCharIndex]))
-                                {
-                                    // 下一个不是字母。可以马上画出。
-                                    canvas.DrawString(line, (float)x, (float)y, HorizontalAlignment.Left);
-                                    if (lenLine < Message.Length)
-                                    {
-                                        Message = Message.Substring(lenLine);
-                                        y += charSize.Height;
-                                        break;
-                                    }
-                                    done = true;
-                                    break; // all done
-                                }
-                                else
-                                {
-                                    // 往前找单词分割。
-                                    var i = line.Length - 1;
-                                    for (; i >= 0 && IsLatin(line[i]); i--)
-                                    {
-                                    }
-                                    if (i < 0)
-                                    {
-                                        // 很长的单词，一行都放不下。强制画出。
-                                        canvas.DrawString(line, (float)x, (float)y, HorizontalAlignment.Left);
-                                        if (lenLine < Message.Length)
-                                        {
-                                            Message = Message.Substring(lenLine);
-                                            y += charSize.Height;
-                                            break;
-                                        }
-                                        done = true;
-                                        break; // all done
-                                    }
-                                    lenLine = i + 1;
-                                    line = line.Substring(0, lenLine);
-                                    // 继续后面的正常画出。
-                                }
-                            }
-                            canvas.DrawString(line, (float)x, (float)y, HorizontalAlignment.Left);
-                            if (lenLine < Message.Length)
-                            {
-                                Message = Message.Substring(lenLine);
-                                y += charSize.Height;
-                                break; // 外层while将会继续。
-                            }
-                            done = true;
-                            break; // all done
+                            DrawWordWrapLine(canvas, line, x, ref y);
+                            break;
                         }
                     }
                 }
             }
-            rectf.Height = y - rectf.Y;
-            canvas.DrawRectangle(rectf);
+            rect.Height = y - rect.Y + Margin * 2;
+            canvas.DrawRectangle(rect);
 
             // 1. 消息框宽度 80%
             // 2. 消息框 Align
