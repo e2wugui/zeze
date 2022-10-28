40e03bc737a81a43a0fcf98b92cab1587f8aa878
e2wugui <e2wugui@163.com>
2022-10-28 10:23:13 +0800

    FamilyClass.cs

---
+++ Zeze/Net/FamilyClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zeze.Net
+{
+    public class FamilyClass
+    {
+        public const int Request = 0;
+        public const int Response = 1;
+        public const int Protocol = 2;
+    }
+}
