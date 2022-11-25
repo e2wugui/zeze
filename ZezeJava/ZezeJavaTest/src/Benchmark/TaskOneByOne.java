2d8d53b73f823eb3d9e2072940f6a516a4d04220
e2wugui <e2wugui@163.com>
2022-11-25 16:00:29 +0800

    TaskOneByOne Benchmark/TaskOneByOne.java 做点计数。

--- ZezeJava/ZezeJavaTest/src/Benchmark/TaskOneByOne.java
+++ ZezeJava/ZezeJavaTest/src/Benchmark/TaskOneByOne.java
@@ -1,22 +1,26 @@
 package Benchmark;
 
+import java.util.concurrent.atomic.AtomicLong;
 import Zeze.Util.Task;
 import Zeze.Util.TaskOneByOneByKey;
 import org.junit.Test;
 
 public class TaskOneByOne {
 	public final static int TaskCount = 100_0000;
+
+	private final AtomicLong counter = new AtomicLong();
 	@Test
 	public void testBenchmark() throws InterruptedException {
 		Task.tryInitThreadPool(null,null, null);
 		var oo = new TaskOneByOneByKey();
 		var b = new Zeze.Util.Benchmark();
 		for (int i = 0; i < TaskCount; ++i)
-			oo.Execute(1, () -> {});
-		oo.Execute(1, () -> { synchronized (this) { this.notify(); }});
+			oo.Execute(1, () -> { counter.incrementAndGet(); });
+		oo.Execute(1, () -> { synchronized (this) { counter.incrementAndGet(); this.notify(); }});
 		synchronized (this) {
 			this.wait();
 		}
 		b.report(this.getClass().getName(), TaskCount);
+		System.out.println(counter.get());
 	}
 }
