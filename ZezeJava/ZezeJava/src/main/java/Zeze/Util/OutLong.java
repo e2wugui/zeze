134274ba1b230c3dc833d7876b31c33a994f5d25
e2wugui <e2wugui@163.com>
2022-12-08 19:52:07 +0800

    TestConsistentHash 增加统计。打完收工。

--- ZezeJava/ZezeJava/src/main/java/Zeze/Util/OutLong.java
+++ ZezeJava/ZezeJava/src/main/java/Zeze/Util/OutLong.java
@@ -9,4 +9,9 @@ public class OutLong {
 	public OutLong(long value) {
 		this.value = value;
 	}
+
+	@Override
+	public String toString() {
+		return String.valueOf(value);
+	}
 }
--- ZezeJava/ZezeJavaTest/src/UnitTest/Zeze/Util/TestConsistentHash.java
+++ ZezeJava/ZezeJavaTest/src/UnitTest/Zeze/Util/TestConsistentHash.java
@@ -1,13 +1,18 @@
 package UnitTest.Zeze.Util;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
 import Zeze.Transaction.Bean;
 import Zeze.Util.ConsistentHash;
+import Zeze.Util.OutLong;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class TestConsistentHash {
 	@Test
-	public void testConsistentHash() {
+	public void testConsistentHash() throws IOException {
 		var consistentHash = new ConsistentHash<Integer>();
 
 		Assert.assertEquals(consistentHash.get(Integer.hashCode(1)), null);
@@ -17,12 +22,45 @@ public class TestConsistentHash {
 		consistentHash.add("3", 3);
 		consistentHash.add("4", 4);
 
+		{
+			var sum = new HashMap<Integer, OutLong>();
+			for (int i = 0; i < 1000_0000; ++i)
+				sum.computeIfAbsent(consistentHash.get(i), k -> new OutLong()).value += 1;
+			System.out.println(sum);
+		}
+
+		var nameFile = Path.of(".", "Chinese_Names_Corpus（120W）.txt");
+		if (Files.exists(nameFile)) {
+			var sum = new HashMap<Integer, OutLong>();
+			for (var line : Files.readAllLines(nameFile)) {
+				if (line.isEmpty())
+					continue;
+				sum.computeIfAbsent(consistentHash.get(line.hashCode()), k -> new OutLong()).value += 1;
+			}
+			System.out.println("name hash name.hashCode()");
+			System.out.println(sum);
+			sum.clear();
+			for (var line : Files.readAllLines(nameFile)) {
+				if (line.isEmpty())
+					continue;
+				sum.computeIfAbsent(consistentHash.get(Bean.hash32(line)), k -> new OutLong()).value += 1;
+			}
+			System.out.println("name hash Bean.hash32(name)");
+			System.out.println(sum);
+		}
+		//*
+		System.out.print(consistentHash.get(1) + ",");
+		System.out.print(consistentHash.get(2) + ",");
+		System.out.print(consistentHash.get(3) + ",");
+		System.out.print(consistentHash.get(4) + ",");
+		System.out.println();
+
 		System.out.print(consistentHash.get("1".hashCode()) + ",");
 		System.out.print(consistentHash.get("2".hashCode()) + ",");
 		System.out.print(consistentHash.get("3".hashCode()) + ",");
 		System.out.print(consistentHash.get("4".hashCode()) + ",");
 		System.out.println();
-		//*
+
 		System.out.print(consistentHash.get(Bean.hash32("1")) + ",");
 		System.out.print(consistentHash.get(Bean.hash32("2")) + ",");
 		System.out.print(consistentHash.get(Bean.hash32("3")) + ",");
