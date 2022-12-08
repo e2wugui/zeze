48c664a87c053abf69f04c15cb4fdc92114d4bfe
e2wugui <e2wugui@163.com>
2022-12-08 10:04:21 +0800

    SortedMap 粘贴c++ std::lower_bound std::upper_bound 算法。

--- ZezeJava/ZezeJava/src/main/java/Zeze/Util/SortedMap.java
+++ ZezeJava/ZezeJava/src/main/java/Zeze/Util/SortedMap.java
@@ -61,10 +61,62 @@ public class SortedMap<K extends Comparable<K>, V> {
 	}
 
 	public int lowerBoundIndex(K key) {
+		/*
+template<class ForwardIt, class T>
+ForwardIt lower_bound(ForwardIt first, ForwardIt last, const T& value)
+{
+    ForwardIt it;
+    typename std::iterator_traits<ForwardIt>::difference_type count, step;
+    count = std::distance(first, last);
+
+    while (count > 0)
+    {
+        it = first;
+        step = count / 2;
+        std::advance(it, step);
+
+        if (*it < value)
+        {
+            first = ++it;
+            count -= step + 1;
+        }
+        else
+            count = step;
+    }
+
+    return first;
+}
+		*/
 		return 0;
 	}
 
 	public int upperBoundIndex(K key) {
+		/*
+template<class ForwardIt, class T>
+ForwardIt upper_bound(ForwardIt first, ForwardIt last, const T& value)
+{
+    ForwardIt it;
+    typename std::iterator_traits<ForwardIt>::difference_type count, step;
+    count = std::distance(first, last);
+
+    while (count > 0)
+    {
+        it = first;
+        step = count / 2;
+        std::advance(it, step);
+
+        if (!(value < *it))
+        {
+            first = ++it;
+            count -= step + 1;
+        }
+        else
+            count = step;
+    }
+
+    return first;
+}
+		*/
 		return 0;
 	}
 
