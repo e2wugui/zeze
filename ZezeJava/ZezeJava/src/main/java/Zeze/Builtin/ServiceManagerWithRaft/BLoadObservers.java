a53004062abd1a6ea521ec6a4abe3a6c3b8e2ed9
e2wugui <e2wugui@163.com>
2022-12-15 20:03:06 +0800

    sm-raft ： load observers

---
+++ ZezeJava/ZezeJava/src/main/java/Zeze/Builtin/ServiceManagerWithRaft/BLoadObservers.java
@@ -0,0 +1,154 @@
+// auto-generated rocks @formatter:off
+package Zeze.Builtin.ServiceManagerWithRaft;
+
+import Zeze.Serialize.ByteBuffer;
+
+// load
+@SuppressWarnings({"UnusedAssignment", "RedundantIfStatement", "SwitchStatementWithTooFewBranches", "RedundantSuppression"})
+public final class BLoadObservers extends Zeze.Raft.RocksRaft.Bean {
+    public static final long TYPEID = -2506288796041147342L;
+
+    private final Zeze.Raft.RocksRaft.CollSet1<String> _Observers; // 负载订阅者，subscribeAndSend中订阅。
+
+    public Zeze.Raft.RocksRaft.CollSet1<String> getObservers() {
+        return _Observers;
+    }
+
+    public BLoadObservers() {
+        _Observers = new Zeze.Raft.RocksRaft.CollSet1<>(String.class);
+        _Observers.variableId(1);
+    }
+
+    public void assign(BLoadObservers other) {
+        _Observers.clear();
+        for (var e : other._Observers)
+            _Observers.add(e);
+    }
+
+    @Deprecated
+    public void Assign(BLoadObservers other) {
+        assign(other);
+    }
+
+    public BLoadObservers copyIfManaged() {
+        return isManaged() ? copy() : this;
+    }
+
+    @Override
+    public BLoadObservers copy() {
+        var copy = new BLoadObservers();
+        copy.assign(this);
+        return copy;
+    }
+
+    @Deprecated
+    public BLoadObservers Copy() {
+        return copy();
+    }
+
+    public static void swap(BLoadObservers a, BLoadObservers b) {
+        BLoadObservers save = a.copy();
+        a.assign(b);
+        b.assign(save);
+    }
+
+    @Override
+    public long typeId() {
+        return TYPEID;
+    }
+
+    @Override
+    public String toString() {
+        var sb = new StringBuilder();
+        buildString(sb, 0);
+        return sb.append(System.lineSeparator()).toString();
+    }
+
+    @Override
+    public void buildString(StringBuilder sb, int level) {
+        sb.append(Zeze.Util.Str.indent(level)).append("Zeze.Builtin.ServiceManagerWithRaft.BLoadObservers: {").append(System.lineSeparator());
+        level += 4;
+        sb.append(Zeze.Util.Str.indent(level)).append("Observers").append("=[").append(System.lineSeparator());
+        level += 4;
+        for (var _item_ : getObservers()) {
+            sb.append(Zeze.Util.Str.indent(level)).append("Item").append('=').append(_item_).append(',').append(System.lineSeparator());
+        }
+        level -= 4;
+        sb.append(Zeze.Util.Str.indent(level)).append(']').append(System.lineSeparator());
+        level -= 4;
+        sb.append(Zeze.Util.Str.indent(level)).append('}');
+    }
+
+    private static int _PRE_ALLOC_SIZE_ = 16;
+
+    @Override
+    public int preAllocSize() {
+        return _PRE_ALLOC_SIZE_;
+    }
+
+    @Override
+    public void preAllocSize(int size) {
+        _PRE_ALLOC_SIZE_ = size;
+    }
+
+    @Override
+    public void encode(ByteBuffer _o_) {
+        int _i_ = 0;
+        {
+            var _x_ = getObservers();
+            int _n_ = _x_.size();
+            if (_n_ != 0) {
+                _i_ = _o_.WriteTag(_i_, 1, ByteBuffer.LIST);
+                _o_.WriteListType(_n_, ByteBuffer.BYTES);
+                for (var _v_ : _x_)
+                    _o_.WriteString(_v_);
+            }
+        }
+        _o_.WriteByte(0);
+    }
+
+    @Override
+    public void decode(ByteBuffer _o_) {
+        int _t_ = _o_.ReadByte();
+        int _i_ = _o_.ReadTagSize(_t_);
+        if (_i_ == 1) {
+            var _x_ = getObservers();
+            _x_.clear();
+            if ((_t_ & ByteBuffer.TAG_MASK) == ByteBuffer.LIST) {
+                for (int _n_ = _o_.ReadTagSize(_t_ = _o_.ReadByte()); _n_ > 0; _n_--)
+                    _x_.add(_o_.ReadString(_t_));
+            } else
+                _o_.SkipUnknownFieldOrThrow(_t_, "Collection");
+            _i_ += _o_.ReadTagSize(_t_ = _o_.ReadByte());
+        }
+        while (_t_ != 0) {
+            _o_.SkipUnknownField(_t_);
+            _o_.ReadTagSize(_t_ = _o_.ReadByte());
+        }
+    }
+
+    @Override
+    protected void initChildrenRootInfo(Zeze.Raft.RocksRaft.Record.RootInfo root) {
+        _Observers.initRootInfo(root, this);
+    }
+
+    @Override
+    public void leaderApplyNoRecursive(Zeze.Raft.RocksRaft.Log vlog) {
+        switch (vlog.getVariableId()) {
+            case 1: _Observers.leaderApplyNoRecursive(vlog); break;
+        }
+    }
+
+    @Override
+    public void followerApply(Zeze.Raft.RocksRaft.Log log) {
+        var vars = ((Zeze.Raft.RocksRaft.LogBean)log).getVariables();
+        if (vars == null)
+            return;
+        for (var it = vars.iterator(); it.moveToNext(); ) {
+            var vlog = it.value();
+            switch (vlog.getVariableId()) {
+                case 1: _Observers.followerApply(vlog); break;
+            }
+        }
+    }
+}
