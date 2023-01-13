bbd2a1a27c8fb26e42330941932512452d472ec0
e2wugui <e2wugui@163.com>
2023-01-13 10:21:51 +0800

    MQListener 隐藏内部实现。

--- ZezeJava/ZezeJava/src/main/java/Zeze/Services/RocketMQ/Producer.java
+++ ZezeJava/ZezeJava/src/main/java/Zeze/Services/RocketMQ/Producer.java
@@ -11,7 +11,7 @@ import org.apache.rocketmq.client.producer.TransactionMQProducer;
 import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.common.message.MessageExt;
 
-public class Producer extends AbstractProducer implements org.apache.rocketmq.client.producer.TransactionListener {
+public class Producer extends AbstractProducer {
 
 	private static final Logger logger = LogManager.getLogger(Producer.class);
 	public Zeze.Application Zeze;
@@ -24,7 +24,7 @@ public class Producer extends AbstractProducer implements org.apache.rocketmq.cl
 
 	public void start() {
 		// todo 这里创建 producer，并建立连接。
-		producer.setTransactionListener(this);
+		producer.setTransactionListener(new MQListener());
 	}
 
 	public void stop() {
@@ -43,31 +43,33 @@ public class Producer extends AbstractProducer implements org.apache.rocketmq.cl
 		producer.sendMessageInTransaction(msg, future);
 	}
 
-	@Override
-	public LocalTransactionState executeLocalTransaction(Message msg, Object futureObj) {
-		try {
-			@SuppressWarnings("unchecked")
-			var future = (TaskCompletionSource<Boolean>)futureObj;
-			return future.get() ? LocalTransactionState.COMMIT_MESSAGE : LocalTransactionState.ROLLBACK_MESSAGE;
-		} catch (Throwable ex) {
-			logger.error("", ex);
-			return LocalTransactionState.ROLLBACK_MESSAGE;
+	class MQListener implements org.apache.rocketmq.client.producer.TransactionListener {
+		@Override
+		public LocalTransactionState executeLocalTransaction(Message msg, Object futureObj) {
+			try {
+				@SuppressWarnings("unchecked")
+				var future = (TaskCompletionSource<Boolean>)futureObj;
+				return future.get() ? LocalTransactionState.COMMIT_MESSAGE : LocalTransactionState.ROLLBACK_MESSAGE;
+			} catch (Throwable ex) {
+				logger.error("", ex);
+				return LocalTransactionState.ROLLBACK_MESSAGE;
+			}
 		}
-	}
 
-	@Override
-	public LocalTransactionState checkLocalTransaction(MessageExt msg) {
-		var checkResult = new OutInt(0);
-		try {
-			Zeze.newProcedure(() -> {
-				String transactionId = msg.getTransactionId();
-				var result = _tSent.get(transactionId);
-				if (null != result && result.isResult())
-					checkResult.value = 1;
-				return 0;
-			}, "checkLocalTransaction").call();
-		} catch (Throwable ignored) {
+		@Override
+		public LocalTransactionState checkLocalTransaction(MessageExt msg) {
+			var checkResult = new OutInt(0);
+			try {
+				Zeze.newProcedure(() -> {
+					String transactionId = msg.getTransactionId();
+					var result = _tSent.get(transactionId);
+					if (null != result && result.isResult())
+						checkResult.value = 1;
+					return 0;
+				}, "checkLocalTransaction").call();
+			} catch (Throwable ignored) {
+			}
+			return checkResult.value == 1 ? LocalTransactionState.COMMIT_MESSAGE : LocalTransactionState.ROLLBACK_MESSAGE;
 		}
-		return checkResult.value == 1 ? LocalTransactionState.COMMIT_MESSAGE : LocalTransactionState.ROLLBACK_MESSAGE;
 	}
 }
