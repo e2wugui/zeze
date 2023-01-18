46f0183fa8a3b6218395e6ba72b99f85e822b874
dwing <dwing4g@gmail.com>
2023-01-18 18:20:24 +0800

    ZezeJava: 更新版本号 1.1.2-SNAPSHOT

--- ZezeJava/test/InfiniteSimulate/simulate - Bug.bat
+++ ZezeJava/test/InfiniteSimulate/simulate - Bug.bat
@@ -3,6 +3,6 @@ setlocal
 pushd %~dp0
 
 rem -ea -XX:NativeMemoryTracking=detail
-java -Dlogname=TwoTestBug -cp .;..\..\ZezeJavaTest\lib\*;..\..\ZezeJava\build\libs\ZezeJava-1.1.1-SNAPSHOT.jar;..\..\ZezeJavaTest\build\libs\ZezeJavaTest-1.1.1-SNAPSHOT.jar Infinite.TwoTestBug
+java -Dlogname=TwoTestBug -cp .;..\..\ZezeJavaTest\lib\*;..\..\ZezeJava\build\libs\ZezeJava-1.1.2-SNAPSHOT.jar;..\..\ZezeJavaTest\build\libs\ZezeJavaTest-1.1.2-SNAPSHOT.jar Infinite.TwoTestBug
 
 pause
