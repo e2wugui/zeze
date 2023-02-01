3a6f888ce5746da8f97969ada6543ce9a381624b
e2wugui <e2wugui@163.com>
2023-02-01 14:59:04 +0800

    CheckpointFlush Bench

--- ZezeJava/ZezeJavaTest/src/Benchmark/CheckpointFlush.java
+++ ZezeJava/ZezeJavaTest/src/Benchmark/CheckpointFlush.java
@@ -8,63 +8,33 @@ import org.junit.Test;
 public class CheckpointFlush {
 	@Test
 	public void benchFlush() throws Exception {
-		{
-			var cfg = Config.load("zeze.xml");
-			cfg.setCheckpointPeriod(Integer.MAX_VALUE); // disable auto flush
-			App.Instance.Start(cfg);
-		}
-		try {
-			flush(CheckpointFlushMode.SingleThread);
-		} finally {
-			App.Instance.Stop();
-		}
-
-		{
-			var cfg = Config.load("zeze.xml");
-			cfg.setCheckpointPeriod(Integer.MAX_VALUE); // disable auto flush
-			App.Instance.Start(cfg);
-		}
-		try {
-			flush(CheckpointFlushMode.MultiThread);
-		} finally {
-			App.Instance.Stop();
-		}
-
-		{
-			var cfg = Config.load("zeze.xml");
-			cfg.setCheckpointPeriod(Integer.MAX_VALUE); // disable auto flush
-			App.Instance.Start(cfg);
-		}
-		try {
-			flush(CheckpointFlushMode.SingleThreadMerge);
-		} finally {
-			App.Instance.Stop();
-		}
-
-		{
-			var cfg = Config.load("zeze.xml");
-			cfg.setCheckpointPeriod(Integer.MAX_VALUE); // disable auto flush
-			App.Instance.Start(cfg);
-		}
-		try {
-			flush(CheckpointFlushMode.MultiThreadMerge);
-		} finally {
-			App.Instance.Stop();
-		}
+		bench(CheckpointFlushMode.SingleThread);
+		bench(CheckpointFlushMode.MultiThread);
+		bench(CheckpointFlushMode.SingleThreadMerge);
+		bench(CheckpointFlushMode.MultiThreadMerge);
 	}
 
-	private void flush(CheckpointFlushMode mode) {
-		App.Instance.Zeze.getConfig().setCheckpointFlushMode(mode);
-		var count = 10_0000;
-		for (int i = 0; i < count; ++i) {
-			long key = i;
-			App.Instance.Zeze.newProcedure(() -> {
-				App.Instance.demo_Module1.getTable1().getOrAdd(key).setLong2(key);
-				return 0;
-			}, "modify").call();
+	private void bench(CheckpointFlushMode mode) throws Exception {
+		var cfg = Config.load("zeze.xml");
+		cfg.setCheckpointPeriod(Integer.MAX_VALUE); // disable auto flush
+		cfg.setCheckpointFlushMode(mode);
+		App.Instance.Start(cfg);
+		try {
+			var count = 10_0000;
+			for (int i = 0; i < count; ++i) {
+				long key = i;
+				App.Instance.Zeze.newProcedure(() -> {
+					App.Instance.demo_Module1.getTable1().getOrAdd(key).setLong2(key);
+					return 0;
+				}, "modify").call();
+			}
+			var b = new Zeze.Util.Benchmark();
+			App.Instance.Zeze.checkpointRun();
+			var name = "flush " + mode + " threads=" + cfg.getCheckpointModeTableFlushConcurrent();
+			b.report(name, count);
+		} finally {
+			App.Instance.Stop();
 		}
-		var b = new Zeze.Util.Benchmark();
-		App.Instance.Zeze.checkpointRun();
-		b.report("flush " + mode, count);
+
 	}
 }
