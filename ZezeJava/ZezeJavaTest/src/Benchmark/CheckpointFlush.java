eee9ae6abe7e5d8b45439b645c7275f271da2d95
lichenghua <e2wugui@163.com>
2023-02-03 16:09:31 +0800

    CheckpointFlush 构建脏数据改成并发，大大减少测试时间。

--- ZezeJava/ZezeJavaTest/src/Benchmark/CheckpointFlush.java
+++ ZezeJava/ZezeJavaTest/src/Benchmark/CheckpointFlush.java
@@ -1,7 +1,10 @@
 package Benchmark;
 
+import java.util.ArrayList;
+import java.util.concurrent.Future;
 import Zeze.Config;
 import Zeze.Transaction.CheckpointFlushMode;
+import Zeze.Util.Task;
 import demo.App;
 import org.junit.Test;
 
@@ -21,13 +24,16 @@ public class CheckpointFlush {
 		App.Instance.Start(cfg);
 		try {
 			var count = 10_0000;
+			var futures = new ArrayList<Future<?>>();
 			for (int i = 0; i < count; ++i) {
 				long key = i;
-				App.Instance.Zeze.newProcedure(() -> {
+				futures.add(Task.runUnsafe(App.Instance.Zeze.newProcedure(() -> {
 					App.Instance.demo_Module1.getTable1().getOrAdd(key).setLong2(key);
 					return 0;
-				}, "modify").call();
+				}, "modify")));
 			}
+			for (var future : futures)
+				future.get();
 			var b = new Zeze.Util.Benchmark();
 			App.Instance.Zeze.checkpointRun();
 			var name = "flush " + mode + " threads=" + cfg.getCheckpointModeTableFlushConcurrent();
