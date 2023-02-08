d3ebdc5f00c2ad2f8cf9e4eaa93589a09afebd7a
lichenghua <e2wugui@163.com>
2023-02-08 10:08:44 +0800

    ReplayAttackMax.java

--- ZezeJava/ZezeJava/src/main/java/Zeze/Util/ReplayAttackMax.java
+++ ZezeJava/ZezeJava/src/main/java/Zeze/Util/ReplayAttackMax.java
@@ -1,9 +1,13 @@
 package Zeze.Util;
 
 public class ReplayAttackMax implements ReplayAttack {
-
+	private long max;
 	@Override
 	public boolean replay(long serialId) {
-		return false;
+		if (serialId > max) {
+			max = serialId;
+			return false;
+		}
+		return true;
 	}
 }
