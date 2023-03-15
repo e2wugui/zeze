940bf85fc01ed63b94b6450b42ec56d4f5058f1e
dwing <dwing4g@gmail.com>
2023-03-15 11:51:59 +0800

    ZezeJava: 更新版本 1.1.20-SNAPSHOT

--- ZezeJava/change_version.lua
+++ ZezeJava/change_version.lua
@@ -1,5 +1,5 @@
-local old_version = "1.1.18-SNAPSHOT"
-local new_version = "1.1.19-SNAPSHOT"
+local old_version = "1.1.19-SNAPSHOT"
+local new_version = "1.1.20-SNAPSHOT"
 
 local files = {
 	{ 1, "build.gradle" },
