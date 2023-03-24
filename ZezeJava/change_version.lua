755946a21297087945709b3f841d4afb684c09d3
dwing <dwing4g@gmail.com>
2023-03-24 11:33:03 +0800

    ZezeJava: 更新版本 1.2.3-SNAPSHOT

--- ZezeJava/change_version.lua
+++ ZezeJava/change_version.lua
@@ -1,5 +1,5 @@
-local old_version = "1.2.1-SNAPSHOT"
-local new_version = "1.2.2-SNAPSHOT"
+local old_version = "1.2.2-SNAPSHOT"
+local new_version = "1.2.3-SNAPSHOT"
 
 local files = {
 	{ 1, "build.gradle" },
--- ZezeJava/test/InfiniteSimulate/simulate - ProcessDaemon.bat
+++ ZezeJava/test/InfiniteSimulate/simulate - ProcessDaemon.bat
@@ -3,7 +3,7 @@ setlocal
 pushd %~dp0
 
 rem -ea -XX:NativeMemoryTracking=detail
-java -Dlogname=SimulateWithDaemon -cp .;..\..\ZezeJavaTest\lib\*;..\..\ZezeJava\build\libs\ZezeJava-1.2.2-SNAPSHOT.jar Zeze.Services.Daemon ^
-     -Dlogname=Simulate           -cp .;..\..\ZezeJavaTest\lib\*;..\..\ZezeJava\build\libs\ZezeJava-1.2.2-SNAPSHOT.jar;..\..\ZezeJavaTest\build\libs\ZezeJavaTest-1.2.2-SNAPSHOT.jar Infinite.Simulate
+java -Dlogname=SimulateWithDaemon -cp .;..\..\ZezeJavaTest\lib\*;..\..\ZezeJava\build\libs\ZezeJava-1.2.3-SNAPSHOT.jar Zeze.Services.Daemon ^
+     -Dlogname=Simulate           -cp .;..\..\ZezeJavaTest\lib\*;..\..\ZezeJava\build\libs\ZezeJava-1.2.3-SNAPSHOT.jar;..\..\ZezeJavaTest\build\libs\ZezeJavaTest-1.2.3-SNAPSHOT.jar Infinite.Simulate
 
 pause
