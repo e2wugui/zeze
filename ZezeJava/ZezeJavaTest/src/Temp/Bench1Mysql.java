b6b287bda989b6f4b13e53f4d5d673467ee1a214
lichenghua <e2wugui@163.com>
2023-03-31 13:30:52 +0800

    Bench1Mysql

---
+++ ZezeJava/ZezeJavaTest/src/Temp/Bench1Mysql.java
@@ -0,0 +1,28 @@
+package Temp;
+
+import com.alibaba.druid.pool.DruidDataSource;
+
+public class Bench1Mysql {
+	public static void main(String [] args) throws Exception {
+		var url = "jdbc:mysql://localhost/devtest?user=dev&password=devtest12345&useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+
+		var dataSource = new DruidDataSource();
+		dataSource.setUrl(url);
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+
+		final var count = 1_0000;
+		var b = new Zeze.Util.Benchmark();
+		try (var conn = dataSource.getConnection()) {
+			conn.setAutoCommit(true);
+			var sql = "replace into bench1 values(?, ?)";
+			for (var key = 0; key < count; ++key) {
+				try (var pre = conn.prepareStatement(sql)) {
+					pre.setLong(1, key);
+					pre.setInt(2, key);
+					pre.executeUpdate();
+				}
+			}
+		}
+		b.report("mysql replace bench", count);
+	}
+}
