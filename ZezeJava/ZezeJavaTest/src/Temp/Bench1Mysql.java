9100acc11a438e02d0a50c0ff3db08b7fff6309b
lichenghua <e2wugui@163.com>
2023-03-31 14:11:57 +0800

    Bench1Mysql

--- ZezeJava/ZezeJavaTest/src/Temp/Bench1Mysql.java
+++ ZezeJava/ZezeJavaTest/src/Temp/Bench1Mysql.java
@@ -1,5 +1,7 @@
 package Temp;
 
+import java.util.ArrayList;
+import java.util.concurrent.Future;
 import com.alibaba.druid.pool.DruidDataSource;
 
 public class Bench1Mysql {
@@ -7,24 +9,37 @@ public class Bench1Mysql {
 		var url = "jdbc:mysql://localhost/devtest?user=dev&password=devtest12345&useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
 
 		var dataSource = new DruidDataSource();
-		try {
-			dataSource.setUrl(url);
-			dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dataSource.setUrl(url);
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
 
+		try {
+			Zeze.Util.Task.tryInitThreadPool(null, null, null);
+			var futures = new ArrayList<Future<?>>();
+			var bTotal = new Zeze.Util.Benchmark();
 			final var count = 1_0000;
-			var b = new Zeze.Util.Benchmark();
-			try (var conn = dataSource.getConnection()) {
-				conn.setAutoCommit(true);
-				var sql = "replace into bench1 values(?, ?)";
-				for (var key = 0; key < count; ++key) {
-					try (var pre = conn.prepareStatement(sql)) {
-						pre.setLong(1, key);
-						pre.setInt(2, key);
-						pre.executeUpdate();
+			final var threads = 3;
+			for (int i = 0; i < threads; ++i) {
+				var startKey = i * count;
+				var endKey = startKey + count;
+				futures.add(Zeze.Util.Task.runUnsafe(() -> {
+					var b = new Zeze.Util.Benchmark();
+					try (var conn = dataSource.getConnection()) {
+						conn.setAutoCommit(true);
+						var sql = "replace into bench1 values(?, ?)";
+						for (var key = startKey; key < endKey; ++key) {
+							try (var pre = conn.prepareStatement(sql)) {
+								pre.setLong(1, key);
+								pre.setInt(2, key);
+								pre.executeUpdate();
+							}
+						}
 					}
-				}
+					b.report("mysql replace bench", count);
+				}, "mysql replace"));
 			}
-			b.report("mysql replace bench", count);
+			for (var future : futures)
+				future.get();
+			bTotal.report("Total mysql replace bench", count * threads);
 		} finally {
 			dataSource.close();
 		}
