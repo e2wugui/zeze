6262749f9ca48213450cb289cd45a2d102bdc155
lichenghua <e2wugui@163.com>
2023-03-31 14:51:18 +0800

    Dbh2FullTest.bench 默认不会执行。

--- ZezeJava/ZezeJavaTest/src/Dbh2/Dbh2FullTest.java
+++ ZezeJava/ZezeJavaTest/src/Dbh2/Dbh2FullTest.java
@@ -1,6 +1,7 @@
 package Dbh2;
 
 import java.util.ArrayList;
+import java.util.concurrent.Future;
 import Zeze.Config;
 import Zeze.Dbh2.Database;
 import Zeze.Dbh2.Dbh2AgentManager;
@@ -9,6 +10,7 @@ import Zeze.Serialize.ByteBuffer;
 import Zeze.Util.Task;
 import org.junit.Assert;
 import org.junit.Test;
+import Zeze.Transaction.Database.AbstractKVTable;
 
 // 测试整体结构(Dbh2Manager,Master,Agent)
 public class Dbh2FullTest {
@@ -19,6 +21,59 @@ public class Dbh2FullTest {
 		databaseConf.setDatabaseUrl("dbh2://127.0.0.1:30000/" + dbName);
 		databaseConf.setName("dbh2");
 		return new Database(null, databaseConf);
+	}
+
+	private Future<?> startBench(int keyStart, int keyEnd, Database database, AbstractKVTable table, ByteBuffer value) {
+		return Task.runUnsafe(() -> {
+			for (int i = keyStart; i < keyEnd; ++i) {
+				try (var trans = database.beginTransaction()) {
+					var key = ByteBuffer.Allocate();
+					key.WriteInt(i);
+					table.replace(trans, key, value);
+					trans.commit();
+				}
+			}
+		}, "");
+	}
+
+	public void testBench() throws Exception {
+		Task.tryInitThreadPool(null, null, null);
+
+		var master = new Zeze.Dbh2.Master.Main();
+		master.start();
+		var managers = new ArrayList<Dbh2Manager>();
+		for (int i = 0; i < 3; ++i)
+			managers.add(new Zeze.Dbh2.Dbh2Manager("manager" + i));
+		for (var manager : managers)
+			manager.start();
+
+		var database = newDatabase("dbh2TestDb");
+		var tables = new ArrayList<AbstractKVTable>();
+		for (int i = 0; i < 4; ++i)
+			tables.add((Database.AbstractKVTable)database.openTable("table" + i));
+
+		var value = ByteBuffer.Wrap(new byte[] { 1, 2, 3, 4 });
+
+		try {
+			var count = 1_0000;
+			var threads = 4;
+			var futures = new ArrayList<Future<?>>();
+			var b = new Zeze.Util.Benchmark();
+			for (var t = 0; t < threads; ++t) {
+				var keyStart = t * count;
+				var keyEnd = keyStart + count;
+				futures.add(startBench(keyStart, keyEnd, database, tables.get(t % tables.size()), value));
+			}
+			for (var future : futures)
+				future.get();
+			b.report("Bench Dbh2 Full Transaction", count * threads);
+		} finally {
+			master.stop();
+			for (var manager : managers)
+				manager.stop();
+			database.close();
+			Dbh2AgentManager.getInstance().clear();
+		}
 	}
 
 	@Test
