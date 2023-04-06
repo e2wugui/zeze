a37ba72bb37b51f3adbd3aee68224f2a9a656be5
lichenghua <e2wugui@163.com>
2023-04-06 13:30:15 +0800

    修改测试服务器Master配置端口为11000.

--- ZezeJava/test/Dbh2/client_10.12.7.155.bat
+++ ZezeJava/test/Dbh2/client_10.12.7.155.bat
@@ -3,4 +3,4 @@ setlocal
 pushd %~dp0
 
 set classes=../../ZezeJava/build/classes/java/main
-java -Dlogname=Client -cp %classes%;../../ZezeJava/lib/*;. Zeze.Dbh2.BenchClient -masterIp 10.12.7.155 -valueSize 100
+java -Dlogname=Client -cp %classes%;../../ZezeJava/lib/*;. Zeze.Dbh2.BenchClient -masterIp 10.12.7.155 -masterPort 11000 -valueSize 100 -tableNumber 32 -threadNumber 8
