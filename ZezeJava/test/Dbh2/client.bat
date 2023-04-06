15cfcf81f94ddb7d2f94c2a5860aad267742ab3a
lichenghua <e2wugui@163.com>
2023-04-06 16:28:23 +0800

    Dbh2 Bench Client 修改端口。

--- ZezeJava/test/Dbh2/client.bat
+++ ZezeJava/test/Dbh2/client.bat
@@ -3,4 +3,4 @@ setlocal
 pushd %~dp0
 
 set classes=../../ZezeJava/build/classes/java/main
-java -Dlogname=Client -cp %classes%;../../ZezeJava/lib/*;. Zeze.Dbh2.BenchClient -valueSize 100 -masterPort 11000
+java -Dlogname=Client -cp %classes%;../../ZezeJava/lib/*;. Zeze.Dbh2.BenchClient -valueSize 100 -masterPort 10999
