59616c42f3995d0dd2695be33e9c96a119df307a
dwing <dwing4g@gmail.com>
2023-04-06 16:52:40 +0800

    ZezeJava: 修改Dbh2的测试脚本

--- ZezeJava/test/Dbh2/manager_10.12.7.51.sh
+++ ZezeJava/test/Dbh2/manager_10.12.7.51.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-classes=../../ZezeJava/build/classes/java/main
-java -Dlogname=Manage_ -cp $classes:../../ZezeJava/lib/*:. Zeze.Dbh2.Dbh2Manager manager_10.12.7.51 zeze_10.12.7.51.xml &
+cd `dirname $0`
+
+IP=10.12.7.51
+
+# nohup java -Dlogname=Master  -cp .:lib/* Zeze.Dbh2.Master.Main zeze_$IP.xml &
+# sleep 2
+nohup java -Dlogname=Manager -cp .:lib/* Zeze.Dbh2.Dbh2Manager manager_$IP zeze_$IP.xml &
