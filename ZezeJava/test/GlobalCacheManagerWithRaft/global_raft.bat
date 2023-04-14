0773e3c19effd535a2c45e90697545427f924676
dwing <dwing4g@gmail.com>
2023-04-14 12:02:52 +0800

    ZezeJava: 更新版本 1.2.6-SNAPSHOT

--- ZezeJava/test/GlobalCacheManagerWithRaft/global_raft.bat
+++ ZezeJava/test/GlobalCacheManagerWithRaft/global_raft.bat
@@ -2,6 +2,6 @@
 setlocal
 pushd %~dp0
 
-java -ea -Dlogname=GlobalRaft -cp .;..\..\ZezeJavaTest\lib\*;..\..\ZezeJava\build\libs\ZezeJava-1.2.5-SNAPSHOT.jar;..\..\ZezeJavaTest\build\libs\ZezeJavaTest-1.2.5-SNAPSHOT.jar GlobalRaft.TestGlobalCacheMgrWithRaft
+java -ea -Dlogname=GlobalRaft -cp .;..\..\ZezeJavaTest\lib\*;..\..\ZezeJava\build\libs\ZezeJava-1.2.6-SNAPSHOT.jar;..\..\ZezeJavaTest\build\libs\ZezeJavaTest-1.2.6-SNAPSHOT.jar GlobalRaft.TestGlobalCacheMgrWithRaft
 
 pause
--- ZezeJava/test/InfiniteSimulate/simulate - ProcessDaemon.bat
+++ ZezeJava/test/InfiniteSimulate/simulate - ProcessDaemon.bat
@@ -3,7 +3,7 @@ setlocal
 pushd %~dp0
 
 rem -ea -XX:NativeMemoryTracking=detail
-java -Dlogname=SimulateWithDaemon -cp .;..\..\ZezeJavaTest\lib\*;..\..\ZezeJava\build\libs\ZezeJava-1.2.5-SNAPSHOT.jar Zeze.Services.Daemon ^
-     -Dlogname=Simulate           -cp .;..\..\ZezeJavaTest\lib\*;..\..\ZezeJava\build\libs\ZezeJava-1.2.5-SNAPSHOT.jar;..\..\ZezeJavaTest\build\libs\ZezeJavaTest-1.2.5-SNAPSHOT.jar Infinite.Simulate
+java -Dlogname=SimulateWithDaemon -cp .;..\..\ZezeJavaTest\lib\*;..\..\ZezeJava\build\libs\ZezeJava-1.2.6-SNAPSHOT.jar Zeze.Services.Daemon ^
+     -Dlogname=Simulate           -cp .;..\..\ZezeJavaTest\lib\*;..\..\ZezeJava\build\libs\ZezeJava-1.2.6-SNAPSHOT.jar;..\..\ZezeJavaTest\build\libs\ZezeJavaTest-1.2.6-SNAPSHOT.jar Infinite.Simulate
 
 pause
