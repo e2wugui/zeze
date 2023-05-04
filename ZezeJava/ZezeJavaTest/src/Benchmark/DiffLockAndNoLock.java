5f92a2569d85c096f21033ac0243f155f6cd5a03
lichenghua <e2wugui@163.com>
2023-05-04 16:58:13 +0800

    测试性能开销，DiffLockAndNoLock，线程越多，差距越明显。

---
+++ ZezeJava/ZezeJavaTest/src/Benchmark/DiffLockAndNoLock.java
@@ -0,0 +1,72 @@
+package Benchmark;
+
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.locks.ReentrantLock;
+import Zeze.Serialize.ByteBuffer;
+import Zeze.Util.RocksDatabase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.rocksdb.RocksDBException;
+
+@Ignore
+public class DiffLockAndNoLock {
+	Zeze.Util.RocksDatabase db;
+	RocksDatabase.Table table;
+	AtomicLong atomicKey = new AtomicLong();
+	ReentrantLock lock = new ReentrantLock();
+
+	@Before
+	public void before() throws RocksDBException {
+		db = new Zeze.Util.RocksDatabase("DiffLockAndNoLock");
+		table = db.getOrAddTable("testDiff");
+	}
+
+	@After
+	public void after() {
+		db.close();
+	}
+
+	@Test
+	public void testDiff() throws RocksDBException, InterruptedException {
+		var threads = new ArrayList<Thread>();
+		var threadCount = 8;
+		for (int i = 0; i < threadCount; ++i)
+			threads.add(new Thread(this::lockPut));
+		var bMulti = new Zeze.Util.Benchmark();
+		for (var thread : threads)
+			thread.start();
+		for (var thread : threads)
+			thread.join();
+		bMulti.report("MultiThreadWithLock", taskPerThreadCount * threadCount);
+		var bSingle = new Zeze.Util.Benchmark();
+		for (int i = 0; i < taskPerThreadCount * threadCount; ++i)
+			put();
+		bSingle.report("SingleThreadWithoutLock", taskPerThreadCount * threadCount);
+	}
+
+	public final static int taskPerThreadCount = 10_000;
+
+	public void put() throws RocksDBException {
+		var key = ByteBuffer.Allocate();
+		key.WriteLong(atomicKey.incrementAndGet());
+		var value = Zeze.Util.Random.nextBytes(100);
+		table.put(key.Bytes, key.ReadIndex, key.size(), value, 0, value.length);
+	}
+
+	public void lockPut() {
+		for (int i = 0; i < taskPerThreadCount; ++i) {
+			lock.lock();
+			try {
+				put();
+			} catch (RocksDBException e) {
+				throw new RuntimeException(e);
+			} finally {
+				lock.unlock();
+			}
+		}
+	}
+}
