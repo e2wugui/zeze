298cbb36ae1002befe4db5dffe90678b0c6b85e1
dwing <dwing4g@gmail.com>
2023-05-17 14:48:12 +0800

    ZezeJava,publish: 更新版本 1.2.11-SNAPSHOT; 更新Gen

--- ZezeJava/change_version.lua
+++ ZezeJava/change_version.lua
@@ -1,5 +1,5 @@
-local old_version = "1.2.9-SNAPSHOT"
-local new_version = "1.2.10-SNAPSHOT"
+local old_version = "1.2.10-SNAPSHOT"
+local new_version = "1.2.11-SNAPSHOT"
 
 local files = {
 	{ 1, "build.gradle" },
--- ZezeJava/test/InfiniteSimulate/simulate - Bug.bat
+++ ZezeJava/test/InfiniteSimulate/simulate - Bug.bat
@@ -3,6 +3,6 @@ setlocal
 pushd %~dp0
 
 rem -ea -XX:NativeMemoryTracking=detail
-java -Dlogname=TwoTestBug -cp .;..\..\ZezeJavaTest\lib\*;..\..\ZezeJava\build\libs\ZezeJava-1.2.10-SNAPSHOT.jar;..\..\ZezeJavaTest\build\libs\ZezeJavaTest-1.2.10-SNAPSHOT.jar Infinite.TwoTestBug
+java -Dlogname=TwoTestBug -cp .;..\..\ZezeJavaTest\lib\*;..\..\ZezeJava\build\libs\ZezeJava-1.2.11-SNAPSHOT.jar;..\..\ZezeJavaTest\build\libs\ZezeJavaTest-1.2.11-SNAPSHOT.jar Infinite.TwoTestBug
 
 pause
