ce70746dca381d8ed7ea782886b6d85fd232a04c
lichenghua <e2wugui@163.com>
2023-05-18 16:50:27 +0800

    Gen java ModuleFormatter _reflect variable

--- Zeze/Gen/java/ModuleFormatter.cs
+++ Zeze/Gen/java/ModuleFormatter.cs
@@ -604,19 +604,6 @@ namespace Zeze.Gen.java
 
             foreach (var s in module.ServletStreams.Values)
             {
-                if (!writtenHeader)
-                {
-                    writtenHeader = true;
-                    sw.WriteLine();
-                    sw.WriteLine("    public void RegisterHttpServlet(Zeze.Netty.HttpServer httpServer) {");
-                    sw.WriteLine("        var _reflect = new Zeze.Util.Reflect(getClass());");
-                    defReflect = true;
-                }
-                else if (!defReflect)
-                {
-                    defReflect = true;
-                    sw.WriteLine("        var _reflect = new Zeze.Util.Reflect(getClass());");
-                }
                 var path = module.WebPathBase.Length > 0 ? module.WebPathBase + s.Name : "/" + module.Path("/", s.Name);
                 sw.WriteLine($"        {httpVar}.addHandler(\"{path}\",");
                 sw.WriteLine($"                _reflect.getTransactionLevel(\"OnServletBeginStream{s.Name}\", Zeze.Transaction.TransactionLevel.{s.TransactionLevel}),");
