5020c20db9a108061afa68f0c04fd6e5a45a92af
lichenghua <e2wugui@163.com>
2023-07-19 10:15:57 +0800

    World.LinkSender 优化！多目标发送时，Command不用每次编码。

--- ZezeJava/ZezeJava/src/main/java/Zeze/World/LinkSender.java
+++ ZezeJava/ZezeJava/src/main/java/Zeze/World/LinkSender.java
@@ -38,7 +38,8 @@ public class LinkSender implements ILinkSender {
 	@Override
 	public boolean sendCommand(String linkName, long linkSid, int commandId, Data data) {
 		logger.info("SendCommand {}:{} {}", linkName, linkSid, commandId);
-		return sendLink(linkName, encodeSend(java.util.List.of(linkSid), commandId, data));
+		var send = encodeSend(java.util.List.of(linkSid), Command.TypeId_, encodeCommand(commandId, data));
+		return sendLink(linkName, send);
 	}
 
 	@Override
@@ -49,24 +50,29 @@ public class LinkSender implements ILinkSender {
 			link.add(target.getBean().getLinkSid());
 		}
 		var result = true;
+		var command = encodeCommand(commandId, data);
 		for (var e : group.entrySet()) {
-			result &= sendLink(e.getKey(), encodeSend(e.getValue(), commandId, data));
+			result &= sendLink(e.getKey(), encodeSend(e.getValue(), Command.TypeId_, command));
 		}
 		return result;
 	}
 
-	public static ByteBuffer encodeSend(Collection<Long> linkSids, int commandId, Data data) {
+	public static ByteBuffer encodeSend(Collection<Long> linkSids, long typeId, ByteBuffer wholeProtocol) {
+		var send = new Send();
+		send.Argument.getLinkSids().addAll(linkSids);
+		send.Argument.setProtocolType(typeId);
+		send.Argument.setProtocolWholeData(new Binary(wholeProtocol));
+
+		return send.encode();
+	}
+
+	public static ByteBuffer encodeCommand(int commandId, Data data) {
 		var cmd = new Command();
 		cmd.Argument.setCommandId(commandId);
 		var bb = ByteBuffer.Allocate();
 		data.encode(bb);
 		cmd.Argument.setParam(new Binary(bb));
 
-		var send = new Send();
-		send.Argument.getLinkSids().addAll(linkSids);
-		send.Argument.setProtocolType(cmd.getTypeId());
-		send.Argument.setProtocolWholeData(new Binary(cmd.encode()));
-
-		return send.encode();
+		return cmd.encode();
 	}
 }
