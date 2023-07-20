aa059cb5cea6fe2a4757e6252e587e63065e8c0c
lichenghua <e2wugui@163.com>
2023-07-20 17:28:52 +0800

    none

--- Components/WorldDemo/Game/Fight/ModuleFight.cs
+++ Components/WorldDemo/Game/Fight/ModuleFight.cs
@@ -13,6 +13,9 @@ namespace Game.Fight
 
         protected override System.Threading.Tasks.Task<long> ProcessAreYouFightRequest(Zeze.Net.Protocol _p)
         {
+            // 这是Zezex测试服务器里面的一条服务器向客户端发起的rpc请求。
+            // 为了不影响Zezex服务器以及关联的单元测试，在这里支持一下。
+            // 【WorldDemo并不需要这个模块】
             var p = _p as AreYouFight;
             p.SendResult();
             return Task.FromResult<long>(0);
