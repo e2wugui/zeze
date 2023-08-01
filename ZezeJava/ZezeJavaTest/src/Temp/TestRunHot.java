c7b2c5fbb37d96bf5a8eafeed7d144f106351373
lichenghua <e2wugui@163.com>
2023-08-01 16:07:54 +0800

    啊

--- ZezeJava/ZezeJavaTest/src/Temp/TestRunHot.java
+++ ZezeJava/ZezeJavaTest/src/Temp/TestRunHot.java
@@ -11,7 +11,7 @@ public class TestRunHot {
 		// 由于测试的module.class和interface.class也存在与本AppClassLoader的路径中。
 		// 按ClassLoader.Parent优先规则，实际装载这些类的ClassLoader是AppClassLoader，而不是HotManager。
 		// 这样测试能运行，但没有完全模拟出实际热更模式。
-		var module = manager.put("Temp", Path.of(".\\ZezeJavaTest\\\\hot\\modules\\m.jar").toFile());
+		var module = manager.put("Temp", Path.of(".\\ZezeJavaTest\\hot\\modules\\m.jar").toFile());
 		var service = module.getService();
 		service.test(manager);
 		System.out.println(manager.getClass().getClassLoader());
