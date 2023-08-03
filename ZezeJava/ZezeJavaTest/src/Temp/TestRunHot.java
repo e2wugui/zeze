bf80f16fbd4343874491bf715e3dae8948399c55
lichenghua <e2wugui@163.com>
2023-08-03 12:04:34 +0800

    TestRunHot 更准确提示。

--- ZezeJava/ZezeJavaTest/src/Temp/TestRunHot.java
+++ ZezeJava/ZezeJavaTest/src/Temp/TestRunHot.java
@@ -9,8 +9,13 @@ public class TestRunHot {
 		// 按ClassLoader.Parent优先规则，实际装载这些类的ClassLoader是AppClassLoader，而不是HotManager。
 		// 这样测试能运行，但没有完全模拟出实际热更模式。
 		System.out.println("hot install ...");
-		var module = tempApp.manager.install("Temp");
-		System.out.println(tempApp.manager.getClass().getClassLoader());
-		tempApp.manager.stopWatchAndJoin();
+		try {
+			var module = tempApp.manager.install("Temp");
+			System.out.println(tempApp.manager.getClass().getClassLoader());
+		} catch (Exception ex) {
+			throw new RuntimeException("maybe run this 'zeze\\ZezeJava\\ZezeJavaTest\\hot\\distribute.bat'", ex);
+		} finally {
+			tempApp.manager.stopWatchAndJoin();
+		}
 	}
 }
