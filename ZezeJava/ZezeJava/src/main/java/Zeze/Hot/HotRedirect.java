17838e39e208617590b05717665d442a6b45d3f2
lichenghua <e2wugui@163.com>
2023-08-09 10:01:08 +0800

    HotRedirect

--- ZezeJava/ZezeJava/src/main/java/Zeze/Hot/HotRedirect.java
+++ ZezeJava/ZezeJava/src/main/java/Zeze/Hot/HotRedirect.java
@@ -1,7 +1,7 @@
 package Zeze.Hot;
 
 public class HotRedirect extends ClassLoader {
-	private HotManager manager;
+	private final HotManager manager;
 
 	public HotRedirect(HotManager manager) {
 		this.manager = manager;
@@ -12,7 +12,7 @@ public class HotRedirect extends ClassLoader {
 		// 1. Redirect编译Module子类的时候，需要装载Module的类。
 		// 2. 如果使用Cl树，那么编译每个HotModule都需要设置一个子Cl。
 		// 3. 参考osgi机制。横向把请求转给HotModule来装载类。
-		// *. 重载这个方法实现。
+		// *. 重载这个方法实现；把GenModule.compiler.useParentClassLoader(this);
 		// todo parent? findLoadedClass?
 		var cl = manager.findHotModule(name);
 		if (null != cl)
