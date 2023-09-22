5dff26f9974bb00dc092f7584e108654b4d7da2c
dwing <dwing4g@gmail.com>
2023-09-22 12:12:22 +0800

    ZezeJava: Bench1Mysql增加测试存储过程

--- ZezeJava/ZezeJavaTest/src/Temp/Bench1Mysql.java
+++ ZezeJava/ZezeJavaTest/src/Temp/Bench1Mysql.java
@@ -1,5 +1,8 @@
 package Temp;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
 import java.util.ArrayList;
 import java.util.concurrent.Future;
 import com.alibaba.druid.pool.DruidDataSource;
@@ -45,5 +48,122 @@ public class Bench1Mysql {
 				future.get();
 			bTotal.report("Total mysql replace bench", count * threads);
 		}
+	}
+
+	public static void mainTestPolarDb1(String[] args) throws Exception {
+		try (var dataSource = new DruidDataSource()) {
+			dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+			dataSource.setUrl("jdbc:mysql://localhost/devtest?user=dev&password=devtest12345&useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true");
+
+			try (var connection = dataSource.getConnection()) {
+				connection.setAutoCommit(true);
+
+				var dropSql = "drop procedure if exists TestProc1;";
+				try (var cmd = connection.prepareStatement(dropSql)) {
+					cmd.executeUpdate();
+				}
+
+				var proc = "create procedure TestProc1(in in_data binary, out out_data binary, out out_hex varchar)\n" +
+						"    begin\n" +
+						"        set out_data = in_data;\n" +
+						"        set out_hex = hex(in_data);\n" +
+						"    end;";
+				try (var cmd = connection.prepareStatement(proc)) {
+					cmd.executeUpdate();
+				}
+
+				try (var cmd = connection.prepareCall("call TestProc1(?, ?, ?)")) {
+					cmd.setBytes(1, new byte[]{(byte)0xa4}); // data
+					cmd.registerOutParameter(2, Types.BINARY);
+					cmd.registerOutParameter(3, Types.VARCHAR);
+					cmd.executeUpdate();
+					var data = cmd.getBytes(2);
+					//var data = blob.getBytes(1, (int)blob.length());
+					System.out.print("out_data =");
+					for (byte b : data)
+						System.out.format(" %02X", b & 0xff);
+					System.out.println();
+					System.out.println("out_hex = " + cmd.getString(3));
+				}
+			}
+		}
+		System.out.println("OK!");
+	}
+
+	public static void mainTestPolarDb2(String[] args) throws Exception {
+		try (var dataSource = new DruidDataSource()) {
+			dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+			dataSource.setUrl("jdbc:mysql://localhost/devtest?user=dev&password=devtest12345&useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true");
+
+			try (var connection = dataSource.getConnection()) {
+				connection.setAutoCommit(true);
+
+				try (var cmd = connection.prepareStatement("drop table if exists TestTable;")) {
+					cmd.executeUpdate();
+				}
+
+				var createSql = "create table if not exists TestTable(id varbinary(767) not null primary key, data blob not null)";
+				try (var cmd = connection.prepareStatement(createSql)) {
+					cmd.executeUpdate();
+				}
+
+//				try (var cmd = connection.prepareStatement("select hex(data) from TestTable where id='key1'")) {
+//					// cmd.setBytes(1, "key1".getBytes(StandardCharsets.UTF_8)); // key
+//					try (ResultSet rs = cmd.executeQuery()) {
+//						if (rs.next()) {
+//							byte[] value = rs.getBytes(1);
+//							System.out.println("size = " + value.length);
+//							for (byte b : value)
+//								System.out.format("%02X %c\n", b & 0xff, (char)b);
+//						}
+//					}
+//				}
+
+				var dropSql = "drop procedure if exists TestProc;";
+				try (var cmd = connection.prepareStatement(dropSql)) {
+					cmd.executeUpdate();
+				}
+
+				var createProc = "create procedure TestProc(in in_data blob)\n" +
+						"    begin\n" +
+						"        replace into TestTable values('key3', in_data);\n" +
+						"    end;";
+
+				try (var cmd = connection.prepareStatement(createProc)) {
+					cmd.executeUpdate();
+				} catch (SQLException ex) {
+					if (!ex.getMessage().contains("already exist"))
+						throw ex;
+				}
+
+				var bs = new byte[256];
+				for (int i = 0; i < 256; i++)
+					bs[i] = (byte)i;
+
+				try (var cmd = connection.prepareCall("call TestProc(?)")) {
+					cmd.setBytes(1, bs); // data
+					cmd.executeUpdate();
+				}
+
+				try (var cmd = connection.prepareStatement("replace into TestTable values('key4', ?)")) {
+					cmd.setBytes(1, bs); // data
+					cmd.executeUpdate();
+				}
+
+				try (var cmd = connection.prepareStatement("select id, data from TestTable")) {
+					try (ResultSet rs = cmd.executeQuery()) {
+						while (rs.next()) {
+							var id = rs.getString(1);
+							var data = rs.getBytes(2);
+							System.out.print("id=" + id + " data.size=" + data.length);
+							for (byte b : data)
+								System.out.format(" %02X", b & 0xff);
+							System.out.println();
+						}
+					}
+				}
+			}
+		}
+		System.out.println("OK!");
 	}
 }
