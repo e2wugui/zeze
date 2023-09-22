870a66bf409a73f5772edd861a29c81dd48ba16d
dwing <dwing4g@gmail.com>
2023-09-22 18:41:29 +0800

    ZezeJava: 更新版本号: 1.3.4-SNAPSHOT

--- ZezeJava/change_version.lua
+++ ZezeJava/change_version.lua
@@ -1,5 +1,5 @@
-local old_version = "1.3.2-SNAPSHOT"
-local new_version = "1.3.3-SNAPSHOT"
+local old_version = "1.3.3-SNAPSHOT"
+local new_version = "1.3.4-SNAPSHOT"
 
 local files = {
 	{ 1, "build.gradle" },
--- ZezeJava/test/InfiniteSimulate/simulate.bat
+++ ZezeJava/test/InfiniteSimulate/simulate.bat
@@ -3,6 +3,6 @@ setlocal
 pushd %~dp0
 
 rem -ea -XX:NativeMemoryTracking=detail
-java -Dlogname=Simulate -cp .;..\..\ZezeJavaTest\lib\*;..\..\ZezeJavaTest\build\libs\ZezeJavaTest-1.3.3-SNAPSHOT.jar Infinite.Simulate
+java -Dlogname=Simulate -cp .;..\..\ZezeJavaTest\lib\*;..\..\ZezeJavaTest\build\libs\ZezeJavaTest-1.3.4-SNAPSHOT.jar Infinite.Simulate
 
 pause
