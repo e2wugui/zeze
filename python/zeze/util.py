f6013816c8399ceec683f3ad51b5afc93bba86e8
dwing <dwing4g@gmail.com>
2023-09-22 16:03:09 +0800

    python: util增加几个方法

--- python/zeze/util.py
+++ python/zeze/util.py
@@ -75,8 +75,8 @@ def hash_dict(a):
 
 INDENT_MAX = 64
 INDENTS = []
-for i in range(0, INDENT_MAX):
-    INDENTS.append(" " * i)
+for indent in range(0, INDENT_MAX):
+    INDENTS.append(" " * indent)
 
 
 def indent(n):
@@ -85,3 +85,52 @@ def indent(n):
     if n >= INDENT_MAX:
         n = INDENT_MAX - 1
     return INDENTS[n]
+
+
+def num_to_hex(n):
+    return n + 0x30 + (((9 - n) >> 31) & (0x41 - 0x39 - 1))  # 'A'=0x41 无分支,比查表快
+
+
+def to_string(buf, offset=0, length=-1):
+    if length < 0:
+        length = len(buf) - offset
+    if length <= 0:
+        return ""
+    s = bytearray(length * 3 - 1)
+    j = 0
+    for i in range(0, length):
+        if i > 0:
+            s[j] = 0x2d  # '-'
+            j += 1
+        b = buf[offset + i]
+        s[j] = num_to_hex((b >> 4) & 0xf)
+        s[j + 1] = num_to_hex(b & 0xf)
+        j += 2
+    return str(s, 'latin_1')
+
+
+def to_string_with_limit(limit, buf, offset=0, length=-1):
+    if limit < 0:
+        raise Exception("limit=" + limit)
+    if length < 0:
+        length = len(buf) - offset
+    if length <= limit:
+        return to_string(buf, offset, length)
+    s = bytearray(limit * 3 + 15)
+    j = 0
+    for i in range(0, limit):
+        b = buf[offset + i]
+        if i > 0:
+            s[j] = 0x2d  # '-'
+            j = j + 1
+        s[j] = num_to_hex((b >> 4) & 0xf)
+        s[j + 1] = num_to_hex(b & 0xf)
+        j += 2
+    s[j:j + 5] = b"...[+"
+    j += 5
+    t = str(length - limit).encode('latin_1')
+    n = len(t)
+    s[j:j + n] = t
+    j += n
+    s[j] = 0x5d  # ']'
+    return str(s[0:j + 1], 'latin_1')
