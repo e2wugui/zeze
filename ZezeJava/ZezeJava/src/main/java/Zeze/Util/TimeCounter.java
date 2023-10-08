620314d823b8e234c6842e0c4db2c6aadbed87d0
lichenghua <e2wugui@163.com>
2023-10-08 11:08:19 +0800

    TimeCounter 加强注释

--- ZezeJava/ZezeJava/src/main/java/Zeze/Util/TimeCounter.java
+++ ZezeJava/ZezeJava/src/main/java/Zeze/Util/TimeCounter.java
@@ -1,9 +1,5 @@
 package Zeze.Util;
 
-import java.util.ArrayDeque;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.atomic.AtomicLong;
-
 /**
  * 用于几秒内的计数，
  * 每秒删除队头的计数，
@@ -13,7 +9,7 @@ public class TimeCounter {
 	private int lastIndex;
 
 	public static class CounterSecond {
-		private long timestamp;
+		private long seconds;
 		private int counter;
 	}
 
@@ -23,13 +19,14 @@ public class TimeCounter {
 
 	public synchronized void increment(long now) {
 		var last = counters[lastIndex];
-		if (now / 1000 == last.timestamp / 1000) {
+		var nowSeconds = now / 1000;
+		if (nowSeconds == last.seconds) {
 			// 同一秒，简单计数
 			last.counter++;
 		} else {
 			lastIndex = (lastIndex + 1) % counters.length;
 			last = counters[lastIndex];
-			last.timestamp = now;
+			last.seconds = nowSeconds;
 			last.counter = 1;
 		}
 	}
@@ -68,11 +65,12 @@ public class TimeCounter {
 
 		for (var i = 0; i < counters.length; ++i) {
 			var head = counters[(headIndex + i) % counters.length];
-			if (nowSeconds - head.timestamp / 1000 <= counters.length)
+			if (nowSeconds - head.seconds <= counters.length)
 				break;
 
 			// reset
-			head.timestamp = 0;
+			head.seconds = 0; // 这个结合上面的if，会导致不必要的reset；这里用Long.MAX_VALUE，可以避免多余的reset，
+							  // 但要求记住headIndex，不能从lastIndex+1开始判断。
 			head.counter = 0;
 		}
 	}
