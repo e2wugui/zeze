c763d279ded016e05f546af415419edd3f386956
lichenghua <e2wugui@163.com>
2023-10-08 10:28:23 +0800

    TimeCounter 零动态内存分配版

--- ZezeJava/ZezeJava/src/main/java/Zeze/Util/TimeCounter.java
+++ ZezeJava/ZezeJava/src/main/java/Zeze/Util/TimeCounter.java
@@ -1,6 +1,7 @@
 package Zeze.Util;
 
 import java.util.ArrayDeque;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
@@ -8,39 +9,58 @@ import java.util.concurrent.atomic.AtomicLong;
  * 每秒删除队头的计数，
  */
 public class TimeCounter {
-	private final ArrayDeque<CounterSecond> counters = new ArrayDeque<>(); // 5-10个。
+	private final CounterSecond[] counters; // 5-10个。
+	private int lastIndex;
+
 	public static class CounterSecond {
-		private final long timestamp;
-		private final AtomicLong counter = new AtomicLong();
-		public CounterSecond(long now) {
-			timestamp = now;
-			counter.getAndSet(1);
-		}
+		private long timestamp;
+		private int counter;
 	}
 
-	public void increace(long now) {
-		if (counters.isEmpty()) {
-			counters.add(new CounterSecond(now));
-		} else if (now / 1000 == counters.getLast().timestamp / 1000) {
-			counters.getLast().counter.incrementAndGet();
+	public synchronized void increment() {
+		var now = System.currentTimeMillis();
+		var last = counters[lastIndex];
+		if (now / 1000 == last.timestamp / 1000) {
+			// 同一秒，简单计数
+			last.counter++;
 		} else {
-			counters.add(new CounterSecond(now));
+			lastIndex = (lastIndex + 1) % counters.length;
+			last = counters[lastIndex];
+			last.timestamp = now;
+			last.counter = 1;
 		}
 	}
 
-	public long count() {
+	public synchronized long count() {
 		var count = 0L;
 		for (var c : counters)
-			count += c.counter.get();
+			count += c.counter;
 		return count;
 	}
 
-	public TimeCounter() {
+	public TimeCounter(int seconds) {
+		counters = new CounterSecond[seconds];
+		for (var i = 0; i < counters.length; ++i)
+			counters[i] = new CounterSecond();
+
 		// 目前这个用于provider，数量不会很多，简单起见，每个计数启用一个定时任务。
-		Task.scheduleUnsafe(Random.getInstance().nextLong(1000), 1000, this::discardHead);
+		Task.scheduleUnsafe(Random.getInstance().nextLong(1000), 1000, this::discard);
 	}
 
-	public void discardHead() {
-		counters.poll();
+	public synchronized void discard() {
+		var nowSeconds = System.currentTimeMillis() / 1000;
+		var headIndex = lastIndex + 1;
+		if (headIndex >= counters.length)
+			headIndex = 0;
+
+		for (var i = 0; i < counters.length; ++i) {
+			var head = counters[headIndex];
+			if (nowSeconds - head.timestamp / 1000 < counters.length)
+				break;
+
+			// reset
+			head.timestamp = 0;
+			head.counter = 0;
+		}
 	}
 }
