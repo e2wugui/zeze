3bcbc5cc08fdb6e8ff32a06d0f87bf59dc9b95c5
dwing <dwing4g@gmail.com>
2023-10-08 15:30:13 +0800

    ZezeJava: GlobalTimer读写curSec使用opaque方式,以避免激进优化不实际读写内存

--- ZezeJava/ZezeJava/src/main/java/Zeze/Util/GlobalTimer.java
+++ ZezeJava/ZezeJava/src/main/java/Zeze/Util/GlobalTimer.java
@@ -1,15 +1,28 @@
 package Zeze.Util;
 
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.VarHandle;
+
 public final class GlobalTimer {
-	// 当前的秒级时间戳,由定时器更新,为了性能不用volatile,多数CPU都能让所有线程及时看到最新值
-	private static long curSec = System.nanoTime() / 1_000_000_000;
+	// 当前的秒级时间戳,由定时器更新,为了性能用opaque方式读写而不用volatile,多数CPU都能让所有线程及时看到最新值
+	@SuppressWarnings("FieldMayBeFinal")
+	private static long curSec;
+	private static final VarHandle vhCurSec;
 
 	static {
-		Task.scheduleUnsafe(1000, 1000, () -> curSec = System.nanoTime() / 1_000_000_000);
+		try {
+			vhCurSec = MethodHandles.lookup().findStaticVarHandle(GlobalTimer.class, "curSec", long.class);
+		} catch (ReflectiveOperationException e) {
+			Task.forceThrow(e);
+			throw new AssertionError(); // never run here
+		}
+
+		curSec = System.nanoTime() / 1_000_000_000;
+		Task.scheduleUnsafe(1000, 1000, () -> vhCurSec.setOpaque(System.nanoTime() / 1_000_000_000));
 	}
 
 	public static long getCurrentSeconds() {
-		return curSec;
+		return (long)vhCurSec.getOpaque();
 	}
 
 	private GlobalTimer() {
