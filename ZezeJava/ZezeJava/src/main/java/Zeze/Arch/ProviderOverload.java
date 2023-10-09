b0fa260fb4d0c74dc95d7cb0ae09b1273d46c72c
lichenghua <e2wugui@163.com>
2023-10-09 10:08:04 +0800

    过载检测新实现。

--- ZezeJava/ZezeJava/src/main/java/Zeze/Arch/ProviderOverload.java
+++ ZezeJava/ZezeJava/src/main/java/Zeze/Arch/ProviderOverload.java
@@ -2,12 +2,23 @@ package Zeze.Arch;
 
 import java.util.HashSet;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import Zeze.Builtin.Provider.BLoad;
 import Zeze.Config;
+import Zeze.Util.Random;
+import Zeze.Util.ThreadDiagnosable;
 
 public class ProviderOverload {
 	private final HashSet<ThreadPoolMonitor> threadPools = new HashSet<>();
+	private final ScheduledExecutorService scheduledExecutorService;
+
+	public ProviderOverload() {
+		scheduledExecutorService = Executors.newScheduledThreadPool(
+				1, ThreadDiagnosable.newFactory("ZezeScheduledPool"));
+	}
 
 	public void register(ExecutorService threadPool, Config config) {
 		threadPools.add(new ThreadPoolMonitor(threadPool, config));
@@ -26,27 +37,43 @@ public class ProviderOverload {
 		return result;
 	}
 
-	public static class ThreadPoolMonitor {
+	public class ThreadPoolMonitor {
 		final ExecutorService threadPool;
 		final Config config;
+		volatile int overload = BLoad.eWorkFine;
 
 		public ThreadPoolMonitor(ExecutorService threadPool, Config config) {
 			this.threadPool = threadPool;
 			this.config = config;
+			startDetectDelay();
+		}
+
+		private synchronized void startDetectDelay() {
+			scheduledExecutorService.schedule(
+					this::detecting, Random.getInstance().nextLong(1000) + 1000, TimeUnit.MILLISECONDS);
+		}
+
+		private void detecting() {
+			var time = System.currentTimeMillis();
+
+			// todo 虚拟线程需要想其他办法检测。比如还是回到任务数量上：同时执行的任务超过多少。
+			threadPool.execute(() -> {
+				synchronized (this) {
+					var detectElapse = System.currentTimeMillis() - time;
+					if (detectElapse > config.getProviderOverload())
+						overload = BLoad.eOverload;
+					else if (detectElapse > config.getProviderThreshold())
+						overload = BLoad.eThreshold;
+					else
+						overload = BLoad.eWorkFine;
+
+					startDetectDelay();
+				}
+			});
 		}
 
 		public int overload() {
-			if (threadPool instanceof ThreadPoolExecutor) {
-				var pool = (ThreadPoolExecutor)threadPool;
-				var elementSize = pool.getQueue().size();
-				if (elementSize > config.getProviderOverload())
-					return BLoad.eOverload;
-				if (elementSize > config.getProviderThreshold())
-					return BLoad.eThreshold;
-				return BLoad.eWorkFine;
-			}
-			// todo 虚拟线程需要使用TaskCount/n，再来比较。
-			return BLoad.eWorkFine;
+			return overload;
 		}
 
 		@Override
@@ -56,12 +83,7 @@ public class ProviderOverload {
 
 		@Override
 		public boolean equals(Object other) {
-			if (other == this)
-				return true;
-			if (other instanceof ThreadPoolMonitor) {
-				return threadPool.equals(((ThreadPoolMonitor)other).threadPool);
-			}
-			return false;
+			return other == this; // 直接比较引用即可。
 		}
 	}
 }
