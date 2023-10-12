836aae0731c255d2198bbb0ae8b9a389b8c1273c
lichenghua <e2wugui@163.com>
2023-10-12 14:29:23 +0800

    给dbh2.manager加配置端口。

--- ZezeJava/test/Dbh2/allServer.bat
+++ ZezeJava/test/Dbh2/allServer.bat
@@ -6,6 +6,6 @@ set classes=.;../../ZezeJava/lib/*;../../ZezeJava/build/classes/java/main;../../
 
 start "Master"   java -Dlogname=master   -Xmx4g -DZeze.Dbh2.Master.stat=100 -cp %classes% Zeze.Dbh2.Master.Main zeze.xml
 ping -n 3 127.1 > nul
-start "Manager0" java -Dlogname=manager0 -Xmx4g -DZeze.Dbh2.Server.stat=100 -DrocksdbCache=1g -cp %classes% Zeze.Dbh2.Dbh2Manager manager0 zeze.xml
-start "Manager1" java -Dlogname=manager1 -Xmx4g -DZeze.Dbh2.Server.stat=100 -DrocksdbCache=1g -cp %classes% Zeze.Dbh2.Dbh2Manager manager1 zeze.xml
-start "Manager2" java -Dlogname=manager2 -Xmx4g -DZeze.Dbh2.Server.stat=100 -DrocksdbCache=1g -cp %classes% Zeze.Dbh2.Dbh2Manager manager2 zeze.xml
+start "Manager0" java -Dlogname=manager0 -Xmx4g -DZeze.Dbh2.Server.stat=100 -DrocksdbCache=1g -cp %classes% Zeze.Dbh2.Dbh2Manager manager0 zeze0.xml
+start "Manager1" java -Dlogname=manager1 -Xmx4g -DZeze.Dbh2.Server.stat=100 -DrocksdbCache=1g -cp %classes% Zeze.Dbh2.Dbh2Manager manager1 zeze1.xml
+start "Manager2" java -Dlogname=manager2 -Xmx4g -DZeze.Dbh2.Server.stat=100 -DrocksdbCache=1g -cp %classes% Zeze.Dbh2.Dbh2Manager manager2 zeze2.xml
--- ZezeJava/test/Dbh2/allServer.sh
+++ ZezeJava/test/Dbh2/allServer.sh
@@ -8,6 +8,6 @@ mkdir -p log 2> /dev/null
 
 nohup java -Dlogname=master   -Xmx4g -DZeze.Dbh2.Master.stat=10 -Dlogconsole=Null -cp .:lib/* Zeze.Dbh2.Master.Main zeze.xml 1> log/master_stdout.log 2> log/master_stderr.log &
 sleep 2
-nohup java -Dlogname=manager0 -Xmx4g -DZeze.Dbh2.Server.stat=10 -DrocksdbCache=1g -Dlogconsole=Null -cp .:lib/* Zeze.Dbh2.Dbh2Manager manager0 zeze.xml 1> log/manager0_stdout.log 2> log/manager0_stderr.log &
-nohup java -Dlogname=manager1 -Xmx4g -DZeze.Dbh2.Server.stat=10 -DrocksdbCache=1g -Dlogconsole=Null -cp .:lib/* Zeze.Dbh2.Dbh2Manager manager1 zeze.xml 1> log/manager1_stdout.log 2> log/manager1_stderr.log &
-nohup java -Dlogname=manager2 -Xmx4g -DZeze.Dbh2.Server.stat=10 -DrocksdbCache=1g -Dlogconsole=Null -cp .:lib/* Zeze.Dbh2.Dbh2Manager manager2 zeze.xml 1> log/manager2_stdout.log 2> log/manager2_stderr.log &
+nohup java -Dlogname=manager0 -Xmx4g -DZeze.Dbh2.Server.stat=10 -DrocksdbCache=1g -Dlogconsole=Null -cp .:lib/* Zeze.Dbh2.Dbh2Manager manager0 zeze0.xml 1> log/manager0_stdout.log 2> log/manager0_stderr.log &
+nohup java -Dlogname=manager1 -Xmx4g -DZeze.Dbh2.Server.stat=10 -DrocksdbCache=1g -Dlogconsole=Null -cp .:lib/* Zeze.Dbh2.Dbh2Manager manager1 zeze1.xml 1> log/manager1_stdout.log 2> log/manager1_stderr.log &
+nohup java -Dlogname=manager2 -Xmx4g -DZeze.Dbh2.Server.stat=10 -DrocksdbCache=1g -Dlogconsole=Null -cp .:lib/* Zeze.Dbh2.Dbh2Manager manager2 zeze2.xml 1> log/manager2_stdout.log 2> log/manager2_stderr.log &
