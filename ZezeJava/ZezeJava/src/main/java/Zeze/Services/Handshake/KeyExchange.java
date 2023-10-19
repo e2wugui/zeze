6a595b42137693b436b11a87966f662d5ed2c22e
dwing <dwing4g@gmail.com>
2023-10-19 17:16:55 +0800

    ZezeJava: 实现一部分 KeyExchange

--- ZezeJava/ZezeJava/src/main/java/Zeze/Services/Handshake/KeyExchange.java
+++ ZezeJava/ZezeJava/src/main/java/Zeze/Services/Handshake/KeyExchange.java
@@ -1,12 +1,25 @@
 package Zeze.Services.Handshake;
 
+import java.math.BigInteger;
+import java.security.GeneralSecurityException;
+import java.security.MessageDigest;
+import java.security.SecureRandom;
+import Zeze.Net.AsyncSocket;
 import Zeze.Net.Binary;
 import Zeze.Net.Protocol;
 import Zeze.Net.Rpc;
+import Zeze.Net.Service;
 import Zeze.Serialize.ByteBuffer;
 import Zeze.Serialize.Serializable;
 import Zeze.Transaction.Bean;
+import Zeze.Transaction.DispatchMode;
+import Zeze.Transaction.TransactionLevel;
+import Zeze.Util.Cert;
+import Zeze.Util.Task;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import sun.security.rsa.RSAPublicKeyImpl;
+import sun.security.rsa.RSAUtil;
 
 // TCP连接成功后,主动连接方(客户端)先发密钥交换请求(KeyExchange)给被动连接方(服务器). 收到回复后,后续通信用双方各自随机生成的key做双向对称加密(己方生成的key用于对方加密发送,己方接收解密)
 // 客户端 === KeyExchange(明文请求) ==> 服务器 (客户端用服务器公钥加密客户端生成的key给服务器,可选提供客户端公钥)
@@ -17,6 +30,7 @@ public final class KeyExchange extends Rpc<KeyExchange.Arg, KeyExchange.Res> {
 	public static final int ModuleId = 0;
 	public static final int ProtocolId = Bean.hash32(KeyExchange.class.getName()); // 571437512 0x220F71C8
 	public static final long TypeId = Protocol.makeTypeId(ModuleId, ProtocolId); // 571437512 [00 00 00 00 C8 71 0F 22]
+	public static final BigInteger rsaE = BigInteger.valueOf(65537);
 
 	static {
 		register(TypeId, KeyExchange.class);
@@ -26,7 +40,7 @@ public final class KeyExchange extends Rpc<KeyExchange.Arg, KeyExchange.Res> {
 		public int version; // 版本. 目前只定义初始版本:0, 即固定使用RSA-2048(exponent固定为65537)作为非对称加密,AES-128(CFB模式)作为对称加密
 		public Binary clientPubKey; // 本地客户端公钥. RSA公钥中的modulus以大端序列化成byte数组(最高位的字节不能为0). 用于对方验证自己的身份,可以为空(不验证,仅单向非对称加密)
 		public Binary serverPubKeyMd5; // 对方服务器公钥的MD5. 如上方法序列化后再做MD5, 用于对方选取所用的私钥
-		public Binary encIvKey; // 如果pubKeyMd5不为空,则表示随机生成对称加密的iv和key各16字节拼接成32字节,以大端序使用对方公钥加密后再以大端序列化的结果; 否则表示双方随机生成的对称加密的iv和key的异或结果
+		public Binary encIvKey; // 随机生成对称加密的iv和key各16字节拼接成32字节,以大端序使用对方公钥加密后再以大端序列化的结果
 
 		@Override
 		public int preAllocSize() {
@@ -59,8 +73,8 @@ public final class KeyExchange extends Rpc<KeyExchange.Arg, KeyExchange.Res> {
 		public static final int ErrorUnknownServerPubKey = 2; // 未知或非法的服务器公钥
 		public static final int ErrorDecryptFailed = 3; // 解密encIvKey失败
 
-		public int version; // 版本. 目前只定义初始版本:0, 即固定使用RSA-2048(exponent固定为65537)作为非对称加密,AES-128(CFB模式)作为对称加密
-		public Binary encIvKey; // 如果clientPubKey为空,表示双方随机生成的对称加密的iv和key的异或结果; 否则类似请求中的encIvKey,给客户端发送服务器随机生成的iv和key
+		public int version; // 版本. 同Arg的version
+		public Binary encIvKey; // 如果clientPubKey为空,表示双方随机生成的对称加密的iv和key的异或结果; 否则类似Arg中的encIvKey,给客户端发送服务器随机生成的iv和key
 
 		@Override
 		public int preAllocSize() {
@@ -86,6 +100,51 @@ public final class KeyExchange extends Rpc<KeyExchange.Arg, KeyExchange.Res> {
 	public KeyExchange() {
 		Argument = new Arg();
 		Result = new Res();
+	}
+
+	public KeyExchange(byte @NotNull [] serverPubKey) {
+		this(serverPubKey, null);
+	}
+
+	public KeyExchange(byte @NotNull [] serverPubKey, byte @Nullable [] clientPubKey) {
+		this();
+		if (clientPubKey != null)
+			Argument.clientPubKey = new Binary(clientPubKey);
+		int i = 0;
+		for (; i < serverPubKey.length; i++) {
+			if (serverPubKey[i] != 0) // 跳过前面的字节0
+				break;
+		}
+		try {
+			var md5 = MessageDigest.getInstance("MD5");
+			md5.update(serverPubKey, i, serverPubKey.length - i);
+			Argument.serverPubKeyMd5 = new Binary(md5.digest());
+			var ivKey = new byte[32];
+			new SecureRandom().nextBytes(ivKey);
+			var pubKey = RSAPublicKeyImpl.newKey(RSAUtil.KeyType.RSA, null, new BigInteger(1, serverPubKey), rsaE);
+			Argument.encIvKey = new Binary(Cert.encryptRsa(pubKey, ivKey));
+		} catch (GeneralSecurityException e) {
+			Task.forceThrow(e);
+		}
+	}
+
+	public boolean send(Service service, AsyncSocket so) {
+		return Send(so, r -> {
+			//TODO
+			return 0;
+		});
+	}
+
+	public static long processKeyExchangeRequest(@NotNull KeyExchange p) {
+		//TODO
+		return 0;
+	}
+
+	public static void addHandler(Service service) {
+		if (!service.getFactorys().containsKey(KeyExchange.TypeId)) {
+			service.AddFactoryHandle(KeyExchange.TypeId, new Service.ProtocolFactoryHandle<>(KeyExchange::new,
+					KeyExchange::processKeyExchangeRequest, TransactionLevel.None, DispatchMode.Direct));
+		}
 	}
 
 	@Override
