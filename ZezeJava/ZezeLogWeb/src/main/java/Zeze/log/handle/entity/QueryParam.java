c579c6ceb1bc3b633b4d3d528c1b1a271b44a561
56798405@qq.com <56798405@qq.com>
2023-10-25 18:53:16 +0800

    ZezeJava\ZezeLogWeb\src\main\java\Zeze\log\handle\entity\QueryParam.java

---
+++ ZezeJava/ZezeLogWeb/src/main/java/Zeze/log/handle/entity/QueryParam.java
@@ -0,0 +1,22 @@
+package Zeze.log.handle.entity;
+
+public class QueryParam {
+	private String serverName;
+	private String json;
+
+	public String getServerName() {
+		return serverName;
+	}
+
+	public void setServerName(String serverName) {
+		this.serverName = serverName;
+	}
+
+	public String getJson() {
+		return json;
+	}
+
+	public void setJson(String json) {
+		this.json = json;
+	}
+}
