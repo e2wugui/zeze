75b448cd6c587d13d3fa8bcac173db6aa30c0c0e
dwing <dwing4g@gmail.com>
2023-12-13 11:32:35 +0800

    ZezeJava: 另一组service&global端口号从5003/5004改为5011/5012

--- ZezeJava/test/service & global.another.bat
+++ ZezeJava/test/service & global.another.bat
@@ -4,5 +4,5 @@ pushd %~dp0
 
 set classes=../ZezeJava/build/classes/java/main;../ZezeJava/build/resources/main
 
-start "ServiceManagerServer2" java -Dlogname=ServiceManagerServer2 -cp %classes%;../ZezeJava/lib/*;. Zeze.Services.ServiceManagerServer -port 5003 -autokeys autokeys2
-start "GlobalCacheManagerAsyncServer2" java -Dlogname=GlobalCacheManagerAsyncServer2 -cp %classes%;../ZezeJava/lib/*;. Zeze.Services.GlobalCacheManagerAsyncServer -port 5004
+start "ServiceManagerServer2" java -Dlogname=ServiceManagerServer2 -cp %classes%;../ZezeJava/lib/*;. Zeze.Services.ServiceManagerServer -port 5011 -autokeys autokeys2
+start "GlobalCacheManagerAsyncServer2" java -Dlogname=GlobalCacheManagerAsyncServer2 -cp %classes%;../ZezeJava/lib/*;. Zeze.Services.GlobalCacheManagerAsyncServer -port 5012
