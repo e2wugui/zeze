1a2dc6416f9e8872e4f9055efd59a3ff505567fa
dwing <dwing4g@gmail.com>
2024-04-11 19:31:11 +0800

    ZezeJava: 修正 ConcurrentHashMapOrdered.putIfAbsent

--- ZezeJava/ZezeJava/src/main/java/Zeze/Util/ConcurrentHashMapOrdered.java
+++ ZezeJava/ZezeJava/src/main/java/Zeze/Util/ConcurrentHashMapOrdered.java
@@ -121,11 +121,10 @@ public class ConcurrentHashMapOrdered<K, V> implements Iterable<V> {
 	public @Nullable V putIfAbsent(@NotNull K key, @NotNull V value) {
 		var oldValue = new OutObject<V>();
 		map.compute(key, (k, v) -> {
-			if (v == null || v == deleted) {
+			if (v == null || v == deleted)
 				return value;
-			}
 			oldValue.value = v;
-			return value;
+			return v;
 		});
 		return oldValue.value;
 	}
