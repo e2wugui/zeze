4541f1d2b8cc47bb32610473ba0fee1c86756fd0
dwing <dwing4g@gmail.com>
2024-05-09 19:15:10 +0800

    ZezeJava: 调整 InfiniteSimulate 测试配置

--- ZezeJava/test/InfiniteSimulate/simulate.bat
+++ ZezeJava/test/InfiniteSimulate/simulate.bat
@@ -3,6 +3,6 @@ setlocal
 pushd %~dp0
 
 rem -ea -XX:NativeMemoryTracking=detail
-java -Dlogname=Simulate -cp .;..\..\ZezeJavaTest\lib\*;..\..\ZezeJavaTest\build\libs\ZezeJavaTest-1.4.5-SNAPSHOT.jar Infinite.Simulate
+java -Dlogname=Simulate -DuseUnlimitedVirtualThread=false -cp .;..\..\ZezeJavaTest\lib\*;..\..\ZezeJavaTest\build\libs\ZezeJavaTest-1.4.5-SNAPSHOT.jar Infinite.Simulate
 
 pause
