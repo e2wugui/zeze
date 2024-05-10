f8bb15db0e88d906cd5b89ccc27c9a5b8ef8b840
lichenghua <e2wugui@163.com>
2024-05-10 11:05:16 +0800

    ApplyHelper add lock

--- ZezeJava/ZezeJava/src/main/java/Zeze/History/ApplyHelper.java
+++ ZezeJava/ZezeJava/src/main/java/Zeze/History/ApplyHelper.java
@@ -8,8 +8,9 @@ import java.util.concurrent.ConcurrentHashMap;
 import Zeze.Application;
 import Zeze.Builtin.HistoryModule.ZezeHistoryTable_m_a_g_i_c;
 import Zeze.Transaction.TableKey;
+import Zeze.Util.FastLock;
 
-public class ApplyHelper {
+public class ApplyHelper extends FastLock {
 	private final Application zeze;
 	private final ZezeHistoryTable_m_a_g_i_c historyTable;
 	private final IApplyDatabase dbApplied;
@@ -25,6 +26,10 @@ public class ApplyHelper {
 		this.beforeTimeMs = beforeTimeMs;
 	}
 
+	public ConcurrentHashMap<Integer, ApplyTable<?, ?>> getApplyTables() {
+		return applyTables;
+	}
+
 	/**
 	 * 应用一批历史数据。
 	 * @param count 指定这次应用的历史记录数量。
@@ -32,27 +37,32 @@ public class ApplyHelper {
 	 * @throws Exception exception。
 	 */
 	public Map<ApplyTable<?, ?>, Set<Object>> apply(int count) throws Exception {
-		var endTime = System.currentTimeMillis() - beforeTimeMs;
-		var result = new HashMap<ApplyTable<?, ?>, Set<Object>>();
-		exclusiveStartKey = historyTable.walkDatabase(exclusiveStartKey, count, (key, value) -> {
-			if (value.getTimestamp() >= endTime)
-				return false;
+		lock();
+		try {
+			var endTime = System.currentTimeMillis() - beforeTimeMs;
+			var result = new HashMap<ApplyTable<?, ?>, Set<Object>>();
+			exclusiveStartKey = historyTable.walkDatabase(exclusiveStartKey, count, (key, value) -> {
+				if (value.getTimestamp() >= endTime)
+					return false;
 
-			for (var r : value.getChanges().entrySet()) {
-				var applyTable = applyTables.computeIfAbsent(r.getKey().getTableId(), __ -> {
-					var tableName = TableKey.tables.get(r.getKey().getTableId());
-					if (null == tableName)
-						throw new RuntimeException("table id not found. id=" + r.getKey().getTableId());
-					var originTable = zeze.getTable(tableName);
-					if (null == originTable)
-						throw new RuntimeException("table not found. name=" + tableName);
-					return originTable.createApplyTable(dbApplied);
-				});
-				var affectKeys = result.computeIfAbsent(applyTable, __ -> new HashSet<>());
-				affectKeys.add(applyTable.apply(r.getKey(), r.getValue()));
-			}
-			return true;
-		});
-		return result;
+				for (var r : value.getChanges().entrySet()) {
+					var applyTable = applyTables.computeIfAbsent(r.getKey().getTableId(), __ -> {
+						var tableName = TableKey.tables.get(r.getKey().getTableId());
+						if (null == tableName)
+							throw new RuntimeException("table id not found. id=" + r.getKey().getTableId());
+						var originTable = zeze.getTable(tableName);
+						if (null == originTable)
+							throw new RuntimeException("table not found. name=" + tableName);
+						return originTable.createApplyTable(dbApplied);
+					});
+					var affectKeys = result.computeIfAbsent(applyTable, __ -> new HashSet<>());
+					affectKeys.add(applyTable.apply(r.getKey(), r.getValue()));
+				}
+				return true;
+			});
+			return result;
+		} finally {
+			unlock();
+		}
 	}
 }
