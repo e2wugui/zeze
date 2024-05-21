decac69606d5605780bdf4b25247016054dc793c
lichenghua <e2wugui@163.com>
2024-05-21 17:15:22 +0800

    before sync freemarker gradle

---
+++ ZezeJava/ZezeJava/src/main/java/Zeze/Netty/FreeMarker.java
@@ -0,0 +1,18 @@
+package Zeze.Netty;
+
+public class FreeMarker {
+	/*
+	private final Configuration freeMarker;
+
+	public FreeMarker() {
+		freeMarker = new Con
+	}
+
+	public void sendResponse(HttpExchange x, Object modalBean) {
+		var tmpl = freeMarker.getTemplate(url_path + Name);
+		tmpl.write(out, modalBean); // MemoryOutputStreamWriter();
+		x.send(memroy.content(););
+		x.sendStream(memroy.context(););
+	}
+	*/
+}
