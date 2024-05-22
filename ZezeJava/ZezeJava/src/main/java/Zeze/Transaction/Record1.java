b2cf9159b5c51bd6539468b87bb5b18c755c0319
dwing <dwing4g@gmail.com>
2024-05-22 16:42:49 +0800

    Revert "本机删除记录的时候马上table.rocksCacheRemove(key);"
    This reverts commit 081cfdbea4549ad7f7ea767d2e21b32a8d0d58ef.

--- ZezeJava/ZezeJava/src/main/java/Zeze/Transaction/Record1.java
+++ ZezeJava/ZezeJava/src/main/java/Zeze/Transaction/Record1.java
@@ -163,13 +163,10 @@ public final class Record1<K extends Comparable<K>, V extends Bean> extends Reco
 	public void commit(@NotNull RecordAccessed accessed) {
 		if (null != accessed.committedPutLog) {
 			setSoftValue(accessed.committedPutLog.getValue());
-			if (accessed.committedPutLog.getValue() == null) {
-				if (table.isMemory()) {
-					// 记录删除并且是内存表，马上删除。
-					table.getCache().remove(key, this);
-					return; // 内存表已经删除，done
-				}
-				table.rocksCacheRemove(key);
+			if (table.isMemory() && accessed.committedPutLog.getValue() == null) {
+				// 记录删除并且是内存表，马上删除。
+				table.getCache().remove(key, this);
+				return; // 内存表已经删除，done
 			}
 		}
 		setTimestamp(getNextTimestamp()); // 必须在 Value = 之后设置。防止出现新的事务得到新的Timestamp，但是数据时旧的。
