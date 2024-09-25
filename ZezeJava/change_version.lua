4167e175667aa62904f3b401e27111fc31e4e7a5
dwing <dwing4g@gmail.com>
2024-09-25 12:03:50 +0800

    ZezeJava: 更新版本号: 1.4.12-SNAPSHOT

--- ZezeJava/change_version.lua
+++ ZezeJava/change_version.lua
@@ -1,5 +1,5 @@
-local old_version = "1.4.10-SNAPSHOT"
-local new_version = "1.4.11-SNAPSHOT"
+local old_version = "1.4.11-SNAPSHOT"
+local new_version = "1.4.12-SNAPSHOT"
 
 local files = {
 	{ 1, "build.gradle" },
--- ZezeJava/test/GlobalCacheManagerWithRaft/global_raft.bat
+++ ZezeJava/test/GlobalCacheManagerWithRaft/global_raft.bat
@@ -2,6 +2,6 @@
 setlocal
 pushd %~dp0
 
-java -ea -Dlogname=GlobalRaft -cp .;..\..\ZezeJavaTest\lib\*;..\..\ZezeJavaTest\build\libs\ZezeJavaTest-1.4.11-SNAPSHOT.jar GlobalRaft.TestGlobalCacheMgrWithRaft
+java -ea -Dlogname=GlobalRaft -cp .;..\..\ZezeJavaTest\lib\*;..\..\ZezeJavaTest\build\libs\ZezeJavaTest-1.4.12-SNAPSHOT.jar GlobalRaft.TestGlobalCacheMgrWithRaft
 
 pause
