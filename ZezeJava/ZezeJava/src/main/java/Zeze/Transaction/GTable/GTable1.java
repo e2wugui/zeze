a3c30400eb33e446241f699c82735177d1fc24aa
lichenghua <e2wugui@163.com>
2024-12-24 18:32:19 +0800

    GTable1 GTable2 正式

--- ZezeJava/ZezeJava/src/main/java/Zeze/Transaction/GTable/GTable1.java
+++ ZezeJava/ZezeJava/src/main/java/Zeze/Transaction/GTable/GTable1.java
@@ -1,16 +1,13 @@
 package Zeze.Transaction.GTable;
 
 import java.io.Serializable;
-import java.util.LinkedHashMap;
 import java.util.Map;
 import Zeze.Transaction.Collections.PMap2;
 import com.google.common.base.Supplier;
 
 public class GTable1<R extends Comparable<R>, C extends Comparable<C>, V> extends StandardTable<R, C, V> {
 	public GTable1(Class<R> rowClass, Class<C> colClass, Class<V> valueClass) {
-		// TODO 这里怎么传递参数啊。
-		//super(new PMap2<R, BeanMap1<C, V>>(rowClass, BeanMap1.class), new Factory<>(colClass, valueClass)));
-		super(new LinkedHashMap<>(), new Factory<>(colClass, valueClass)); // TODO 先编译通过。
+		super(new PMap2(rowClass, BeanMap1.class), new Factory<>(colClass, valueClass));
 	}
 
 	private static class Factory<C extends Comparable<C>, V> implements Supplier<Map<C, V>>, Serializable {
--- ZezeJava/ZezeJava/src/main/java/Zeze/Transaction/GTable/GTable2.java
+++ ZezeJava/ZezeJava/src/main/java/Zeze/Transaction/GTable/GTable2.java
@@ -1,19 +1,15 @@
 package Zeze.Transaction.GTable;
 
 import java.io.Serializable;
-import java.util.LinkedHashMap;
 import java.util.Map;
 import Zeze.Transaction.Bean;
 import Zeze.Transaction.Collections.PMap2;
-import Zeze.Util.Factory;
 import com.google.common.base.Supplier;
 
 public class GTable2<R extends Comparable<R>, C extends Comparable<C>, V extends Bean, VReadOnly>
 		extends StandardTable<R, C, V> {
 	public GTable2(Class<R> rowClass, Class<C> colClass, Class<V> valClass) {
-		// TODO 这里怎么传递参数啊。
-		//super(new PMap2<R, BeanMap2<C, V, VReadOnly>>(rowClass, BeanMap2.class), new Factory<>(colClass, valClass));
-		super(new LinkedHashMap<>(), new Factory<>(colClass, valClass)); // TODO 先编译通过。
+		super(new PMap2(rowClass, BeanMap2.class), new Factory<>(colClass, valClass));
 	}
 
 	private static class Factory<C extends Comparable<C>, V extends Bean> implements Supplier<Map<C, V>>, Serializable {
