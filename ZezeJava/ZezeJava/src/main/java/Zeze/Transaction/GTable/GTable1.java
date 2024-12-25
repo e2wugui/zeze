40f5b1629cc7672f28ae02883a84ec5d08ad14d9
lichenghua <e2wugui@163.com>
2024-12-25 17:51:19 +0800

    遗漏代码编译不过，修复。

--- ZezeJava/ZezeJava/src/main/java/Zeze/Transaction/GTable/GTable1.java
+++ ZezeJava/ZezeJava/src/main/java/Zeze/Transaction/GTable/GTable1.java
@@ -6,17 +6,17 @@ import Zeze.Transaction.Collections.PMap2;
 import com.google.common.base.Supplier;
 
 public class GTable1<R extends Comparable<R>, C extends Comparable<C>, V> extends StandardTable<R, C, V> {
-	private final PMap2<R, BeanMap1IntegerInteger> pMap2;
+	private final PMap2<R, BeanMap1<C, V>> pMap2;
 
 	@SuppressWarnings({"unchecked", "rawtypes"})
 	public GTable1(Class<R> rowClass, Class<C> colClass, Class<V> valClass) {
-		this.pMap2 = new PMap2(rowClass, BeanMap1IntegerInteger.class);
+		this.pMap2 = new PMap2(rowClass, BeanMap1.class);
 		PMap2 raw = pMap2;
 		super.backingMap = raw;
 		super.factory = new Factory(colClass, valClass);
 	}
 
-	public PMap2<R, BeanMap1IntegerInteger> getPMap2() {
+	public PMap2<R, BeanMap1<C, V>> getPMap2() {
 		return pMap2;
 	}
 
