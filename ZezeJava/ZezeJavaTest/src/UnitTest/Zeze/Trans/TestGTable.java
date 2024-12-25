8d60a2b5b45f40b73b9ce947c5e7e2df93a75849
lichenghua <e2wugui@163.com>
2024-12-25 20:33:18 +0800

    测试，不能跑的。

--- ZezeJava/ZezeJavaTest/src/UnitTest/Zeze/Trans/TestGTable.java
+++ ZezeJava/ZezeJavaTest/src/UnitTest/Zeze/Trans/TestGTable.java
@@ -3,8 +3,12 @@ package UnitTest.Zeze.Trans;
 import Zeze.Transaction.Collections.PMap2;
 import Zeze.Transaction.GTable.GTable1;
 import Zeze.Transaction.Locks;
+import Zeze.Transaction.Record;
+import Zeze.Transaction.Record1;
+import Zeze.Transaction.TableKey;
 import Zeze.Transaction.Transaction;
 import demo.Bean1;
+import demo.web.tMap2Bean1;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -15,10 +19,17 @@ public class TestGTable {
 	public void testGTableBegin() {
 		Transaction.create(locks);
 		try {
-			/*
 			Transaction.getCurrent().begin();
 			// process
-			var table = new GTable1<>(Integer.class);
+			// 设置假的rootInfo，不能commit，用来测试。
+			var tTable = new tMap2Bean1();
+			var record = new Record1(tTable, 1, null);
+			var bean = new Bean1();
+			var root = new Record.RootInfo(record, new TableKey(1, 1));
+
+			var table = new GTable1<>(Integer.class, Integer.class, Integer.class);
+			table.getPMap2().initRootInfo(root, bean); // 设置假的rootInfo，不能commit，用来测试。
+
 			Assert.assertTrue(table.isEmpty());
 
 			table.put(1, 1, 1);
@@ -27,9 +38,7 @@ public class TestGTable {
 			Assert.assertEquals(1, value.intValue());
 
 			Transaction.getCurrent().rollback();
-			// TODO table 必须managed才会开启事务模式，所以这里暂时通不过。需要gen实现了才能测试了。
-			//Assert.assertTrue(table.isEmpty());
-			*/
+			Assert.assertTrue(table.isEmpty());
 		} finally {
 			Transaction.destroy();
 		}
