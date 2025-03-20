ad862d7c8b7b4e98221db21cd5fbbd4c76ae434a
lichenghua <e2wugui@163.com>
2025-03-20 11:22:08 +0800

    更加严格的MathEx.unsignedMod

--- ZezeJava/ZezeJava/src/main/java/Zeze/Util/MathEx.java
+++ ZezeJava/ZezeJava/src/main/java/Zeze/Util/MathEx.java
@@ -2,6 +2,8 @@ package Zeze.Util;
 
 public class MathEx {
 	public static int unsignedMod(int hash, int div) {
-		return (int)(Integer.toUnsignedLong(hash) % Integer.toUnsignedLong(div));
+		if (div < 0)
+			throw new RuntimeException("div < 0");
+		return (int)(Integer.toUnsignedLong(hash) % div);
 	}
 }
