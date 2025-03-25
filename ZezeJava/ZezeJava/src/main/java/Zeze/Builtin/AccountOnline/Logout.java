abb250cbc1222270461644c2370d7afff1646296
lichenghua <e2wugui@163.com>
2025-03-25 12:50:03 +0800

    AccountOnline server 完成。

--- ZezeJava/ZezeJava/src/main/java/Zeze/Builtin/AccountOnline/Logout.java
+++ ZezeJava/ZezeJava/src/main/java/Zeze/Builtin/AccountOnline/Logout.java
@@ -2,7 +2,7 @@
 package Zeze.Builtin.AccountOnline;
 
 // 断线时自动登出。
-public class Logout extends Zeze.Net.Rpc<Zeze.Transaction.EmptyBean.Data, Zeze.Transaction.EmptyBean.Data> {
+public class Logout extends Zeze.Net.Rpc<Zeze.Builtin.AccountOnline.BAccountLink.Data, Zeze.Transaction.EmptyBean.Data> {
     public static final int ModuleId_ = 11041;
     public static final int ProtocolId_ = 1723066623;
     public static final long TypeId_ = Zeze.Net.Protocol.makeTypeId(ModuleId_, ProtocolId_); // 47422456981759
@@ -24,7 +24,12 @@ public class Logout extends Zeze.Net.Rpc<Zeze.Transaction.EmptyBean.Data, Zeze.T
     }
 
     public Logout() {
-        Argument = Zeze.Transaction.EmptyBean.Data.instance;
+        Argument = new Zeze.Builtin.AccountOnline.BAccountLink.Data();
+        Result = Zeze.Transaction.EmptyBean.Data.instance;
+    }
+
+    public Logout(Zeze.Builtin.AccountOnline.BAccountLink.Data arg) {
+        Argument = arg;
         Result = Zeze.Transaction.EmptyBean.Data.instance;
     }
 }
--- ZezeJava/ZezeJava/src/main/java/Zeze/Services/AccountOnline.java
+++ ZezeJava/ZezeJava/src/main/java/Zeze/Services/AccountOnline.java
@@ -1,18 +1,110 @@
 package Zeze.Services;
 
+import java.util.concurrent.ConcurrentHashMap;
+import Zeze.Builtin.AccountOnline.BAccountLink;
+import Zeze.Builtin.AccountOnline.Kick;
+import Zeze.Config;
+import Zeze.Net.AsyncSocket;
+import Zeze.Net.Service;
+import Zeze.Transaction.Procedure;
+import Zeze.Util.FastLock;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 public class AccountOnline extends AbstractAccountOnline {
+    private final AccountOnlineService service;
+    private final ConcurrentHashMap<String, AsyncSocket> links = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, AccountInfo> accounts = new ConcurrentHashMap<>();
+
+    public AccountOnline(Config config) {
+        service = new AccountOnlineService(config);
+        RegisterProtocols(service);
+    }
+
+    public void start() throws Exception {
+        service.start();
+    }
+
+    public void stop() throws Exception {
+        service.stop();
+    }
+
+    private void kick(BAccountLink.Data link) {
+        var r = new Kick();
+        r.Argument = link;
+        r.Send(links.get(link.getLinkName())); // 不等待结果。
+    }
+
     @Override
     protected long ProcessLoginRequest(Zeze.Builtin.AccountOnline.Login r) {
-        return Zeze.Transaction.Procedure.NotImplement;
+        while (true) {
+            var info = accounts.computeIfAbsent(r.Argument.getAccountLink().getAccount(), (key) -> new AccountInfo());
+            info.lock();
+            try {
+                if (info.removed)
+                    continue; // 拿到了被删除的记录，重新获取一个有效的。
+
+                if (info.link == null || info.link.equals(r.Argument.getAccountLink())) {
+                    // 新鲜的Login或者是自己.
+                    info.link = r.Argument.getAccountLink();
+                    r.SendResult();
+                    return 0;
+                }
+
+                if (r.Argument.isKickOld()) {
+                    // 存在旧的登录。踢掉。
+                    kick(info.link);
+                    info.link = r.Argument.getAccountLink();
+                    r.SendResult();
+                    return 0;
+                }
+                // 存在旧的登录，这次登录失败。
+                r.SendResultCode(Procedure.DuplicateRequest);
+                return 0;
+            } finally {
+                info.unlock();
+            }
+        }
     }
 
     @Override
     protected long ProcessLogoutRequest(Zeze.Builtin.AccountOnline.Logout r) {
-        return Zeze.Transaction.Procedure.NotImplement;
+        accounts.computeIfPresent(r.Argument.getAccount(), (key, exist) -> {
+            if (exist.link.equals(r.Argument)) {
+                exist.removed = true;
+                return null; // 是自己，删除掉。
+            }
+            return exist;
+        });
+        r.SendResult();
+        return 0;
     }
 
     @Override
     protected long ProcessRegisterRequest(Zeze.Builtin.AccountOnline.Register r) {
-        return Zeze.Transaction.Procedure.NotImplement;
+        r.getSender().setUserState(r.Argument.getLinkName());
+        links.put(r.Argument.getLinkName(), r.getSender());
+        r.SendResult();
+        return 0;
+    }
+
+    public static class AccountInfo extends FastLock {
+        private boolean removed = false;
+        private BAccountLink.Data link;
+    }
+
+    public class AccountOnlineService extends Service {
+        public AccountOnlineService(Config config) {
+            super("Zeze.Services.AccountOnline", config);
+            setNoProcedure(true);
+        }
+
+        @Override
+        public void OnSocketClose(@NotNull AsyncSocket so, @Nullable Throwable e) throws Exception {
+            if (so.getUserState() != null) {
+                links.remove((String)so.getUserState(), so);
+            }
+            super.OnSocketClose(so, e);
+        }
     }
 }
