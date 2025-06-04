2235ee895e95c3a29ae2d571201f9a54e63b9d42
dwing <dwing4g@gmail.com>
2025-06-04 11:41:15 +0800

    ZezeJava: 更新版本号: 1.5.7

--- ZezeJava/test/InfiniteSimulate/simulate - ProcessDaemon.bat
+++ ZezeJava/test/InfiniteSimulate/simulate - ProcessDaemon.bat
@@ -4,6 +4,6 @@ pushd %~dp0
 
 rem -ea -XX:NativeMemoryTracking=detail
 java -Dlogname=SimulateWithDaemon -cp .;..\..\ZezeJavaTest\lib\* Zeze.Services.Daemon ^
-     -Dlogname=Simulate           -cp .;..\..\ZezeJavaTest\lib\*;..\..\ZezeJavaTest\build\libs\ZezeJavaTest-1.5.7-SNAPSHOT.jar Infinite.Simulate
+     -Dlogname=Simulate           -cp .;..\..\ZezeJavaTest\lib\*;..\..\ZezeJavaTest\build\libs\ZezeJavaTest-1.5.7.jar Infinite.Simulate
 
 pause
