e361712bbef8ecd85f265a0ffb66c2bef13e96d1
lichenghua <e2wugui@163.com>
2025-06-18 12:37:19 +0800

    HttpMultipartHandle.java 增加 fileWrite ，递归判断compositeByteBuf。

--- ZezeJava/ZezeJava/src/main/java/Zeze/Netty/HttpMultipartHandle.java
+++ ZezeJava/ZezeJava/src/main/java/Zeze/Netty/HttpMultipartHandle.java
@@ -3,6 +3,9 @@ package Zeze.Netty;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import com.google.protobuf.ByteString;
+import io.netty.buffer.ByteBuf;
 import io.netty.buffer.CompositeByteBuf;
 import io.netty.handler.codec.http.HttpContent;
 import io.netty.handler.codec.http.HttpResponseStatus;
@@ -29,19 +32,22 @@ public interface HttpMultipartHandle extends HttpBeginStreamHandle, HttpStreamCo
 //		System.out.println("onAttribute: " + attr.getName() + " = " + attr.getValue());
 	}
 
+	default void fileWrite(OutputStream os, ByteBuf buf) throws IOException {
+		if (buf instanceof CompositeByteBuf) {
+			var composite = (CompositeByteBuf)buf;
+			for (var i = 0; i < composite.numComponents(); ++i) {
+				fileWrite(os, composite.component(i));
+			}
+		} else {
+			os.write(buf.array(), buf.arrayOffset(), buf.readableBytes());
+		}
+	}
+
 	default void fileComplete(@NotNull FileUpload fileUpload, File target) throws IOException {
 		if (fileUpload.isInMemory()) {
 			try (var os = new FileOutputStream(target)) {
 				var buf = fileUpload.getByteBuf();
-				if (buf instanceof CompositeByteBuf) {
-					var composite = (CompositeByteBuf)buf;
-					for (var i = 0; i < composite.numComponents(); ++i) {
-						var buf2 = composite.component(i);
-						os.write(buf2.array(), buf2.arrayOffset(), buf2.readableBytes());
-					}
-				} else {
-					os.write(buf.array(), buf.arrayOffset(), buf.readableBytes());
-				}
+				fileWrite(os, buf);
 			}
 		} else {
 			var r = fileUpload.getFile().renameTo(target); // 可把临时文件移动到指定位置
