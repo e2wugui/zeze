13a21dcbd021e64397da64ccac08941482800f46
dwing <dwing4g@gmail.com>
2025-06-28 16:19:36 +0800

    ZezeJava: 内存表软引用被清理的测试

---
+++ ZezeJava/ZezeJavaTest/src/Temp/TestMemoryTable.java
@@ -0,0 +1,33 @@
+package Temp;
+
+import java.lang.ref.SoftReference;
+import Zeze.Util.OutInt;
+import Zeze.Util.Task;
+import demo.App;
+import demo.Bean1;
+
+public class TestMemoryTable {
+	public static void main(String[] args) throws Exception {
+		App.Instance.Start();
+
+		var ref = new SoftReference<>(new OutInt(1234));
+
+		Task.call(App.Instance.Zeze.newProcedure(() -> {
+			App.Instance.demo_Module1.tMemorySize().put(123L, new Bean1(1));
+			return 0L;
+		}, "put"));
+
+		try {
+			var obj = new long[2_000_000_000];
+		} catch (OutOfMemoryError err) {
+			System.out.println("OutOfMemoryError");
+		}
+
+		System.out.println(ref.get());
+
+		Task.call(App.Instance.Zeze.newProcedure(() -> {
+			System.out.println(App.Instance.demo_Module1.tMemorySize().get(123L));
+			return 0L;
+		}, "get"));
+	}
+}
