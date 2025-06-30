62e7b3bbbb776ae77517a50fef7e54a8ac9bd56b
lichenghua <e2wugui@163.com>
2025-06-30 13:07:38 +0800

    打开memory.cache.clean。

--- ZezeJava/ZezeJava/src/main/java/Zeze/Transaction/TableCache.java
+++ ZezeJava/ZezeJava/src/main/java/Zeze/Transaction/TableCache.java
@@ -165,7 +165,7 @@ public class TableCache<K extends Comparable<K>, V extends Bean> {
 		// 每次执行完重新调度。
 		var capacity = table.getTableConf().getRealCacheCapacity();
 		// 内存表关闭clean。
-		if (capacity > 0 && !table.isMemory()) {
+		if (capacity > 0) {
 			var timeBegin = System.nanoTime();
 			int recordCount = 0, nodeCount = 0;
 			while (dataMap.size() > capacity && table.getZeze().isStart()) { // 超出容量，循环尝试
