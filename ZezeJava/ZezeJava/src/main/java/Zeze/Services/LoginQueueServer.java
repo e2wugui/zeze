abdfa937008b00c82dceea79a7a531f05a02b5e7
lichenghua <e2wugui@163.com>
2025-07-16 11:39:03 +0800

    LoginQueueServer.java rpc改成protocol

--- ZezeJava/ZezeJava/src/main/java/Zeze/Services/LoginQueueServer.java
+++ ZezeJava/ZezeJava/src/main/java/Zeze/Services/LoginQueueServer.java
@@ -2,12 +2,12 @@ package Zeze.Services;
 
 public class LoginQueueServer extends AbstractLoginQueueServer {
     @Override
-    protected long ProcessReportProviderLoadRequest(Zeze.Builtin.LoginQueueServer.ReportProviderLoad r) {
+    protected long ProcessReportProviderLoad(Zeze.Builtin.LoginQueueServer.ReportProviderLoad r) {
         return Zeze.Transaction.Procedure.NotImplement;
     }
 
     @Override
-    protected long ProcessReportLinkLoadRequest(Zeze.Builtin.LoginQueueServer.ReportLinkLoad r) {
+    protected long ProcessReportLinkLoad(Zeze.Builtin.LoginQueueServer.ReportLinkLoad r) {
         return Zeze.Transaction.Procedure.NotImplement;
     }
 }
