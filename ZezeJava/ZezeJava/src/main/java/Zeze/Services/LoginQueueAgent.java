2628247bab635b53e6f403a72a8d2b38b8357b2a
lichenghua <e2wugui@163.com>
2025-07-17 10:14:53 +0800

    LoginQueueAgent.java rpc改成protocol

--- ZezeJava/ZezeJava/src/main/java/Zeze/Services/LoginQueueAgent.java
+++ ZezeJava/ZezeJava/src/main/java/Zeze/Services/LoginQueueAgent.java
@@ -4,7 +4,7 @@ import Zeze.Builtin.LoginQueueServer.AnnounceSecret;
 
 public class LoginQueueAgent extends AbstractLoginQueueAgent {
 	@Override
-	protected long ProcessAnnounceSecretRequest(AnnounceSecret r) throws Exception {
+	protected long ProcessAnnounceSecret(AnnounceSecret r) throws Exception {
 		return 0;
 	}
 }
