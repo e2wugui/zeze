a825e007e9a592a5dae0c1d8441e0e5293d31be7
lichenghua <e2wugui@163.com>
2025-07-24 17:49:16 +0800

    LoginQueueAgent

--- ZezeJava/ZezeJava/src/main/java/Zeze/Services/LoginQueueAgent.java
+++ ZezeJava/ZezeJava/src/main/java/Zeze/Services/LoginQueueAgent.java
@@ -1,10 +1,65 @@
 package Zeze.Services;
 
 import Zeze.Builtin.LoginQueueServer.AnnounceSecret;
+import Zeze.Builtin.LoginQueueServer.ReportLinkLoad;
+import Zeze.Builtin.LoginQueueServer.ReportProviderLoad;
+import Zeze.Builtin.Provider.BLoad;
+import Zeze.Net.Binary;
+import Zeze.Net.Service;
 
 public class LoginQueueAgent extends AbstractLoginQueueAgent {
+
+	/**
+	 * Connector service. 连接LoginQueueServer.
+	 */
+	public static class LoginQueueAgentService extends Service {
+		public LoginQueueAgentService() {
+			super("LoginQueueAgentService");
+		}
+	}
+
+	private final LoginQueueAgentService service;
+	private Binary secretKey;
+	private final int serverId;
+	private final String serviceIp;
+	private final int servicePort;
+
+	public LoginQueueAgent(int serverId, String serviceIp, int servicePort) {
+		this.serverId = serverId;
+		this.serviceIp = serviceIp;
+		this.servicePort = servicePort;
+
+		this.service = new LoginQueueAgentService();
+		RegisterProtocols(this.service);
+	}
+
+	public Binary getSecretKey() {
+		return secretKey;
+	}
+
 	@Override
-	protected long ProcessAnnounceSecret(AnnounceSecret r) throws Exception {
+	protected long ProcessAnnounceSecret(AnnounceSecret r) {
+		secretKey = r.Argument.getSecretKey();
 		return 0;
+	}
+
+	// 下面两个报告对于一个服务只能二选一。要嘛是link，要嘛是provider。
+
+	public void reportLinkLoad(BLoad.Data load) {
+		var p = new ReportLinkLoad();
+		p.Argument.setServerId(serverId);
+		p.Argument.setServiceIp(serviceIp);
+		p.Argument.setServicePort(servicePort);
+		p.Argument.setLoad(load);
+		p.Send(service.GetSocket());
+	}
+
+	public void reportProviderLoad(BLoad.Data load) {
+		var p = new ReportProviderLoad();
+		p.Argument.setServerId(serverId);
+		p.Argument.setServiceIp(serviceIp);
+		p.Argument.setServicePort(servicePort);
+		p.Argument.setLoad(load);
+		p.Send(service.GetSocket());
 	}
 }
--- doc/index.md
+++ doc/index.md
@@ -54,6 +54,8 @@
 
 [运维和配置](Writerside/topics/DevOps-and-configuration.md)
 
+[Log](Writerside/topics/Log.md)
+
 [Inside Zeze](Writerside/topics/Inside-Zeze.md)
 
 [Onz](Writerside/topics/Onz.md)
