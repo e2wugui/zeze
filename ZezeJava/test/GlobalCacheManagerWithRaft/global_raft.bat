56d0858ddb9c6ca6e47f06e228376d0e8e6f1955
dwing <dwing4g@gmail.com>
2025-09-04 19:34:49 +0800

    ZezeJava: 更新版本号: 1.5.11-SNAPSHOT

--- ZezeJava/test/GlobalCacheManagerWithRaft/global_raft.bat
+++ ZezeJava/test/GlobalCacheManagerWithRaft/global_raft.bat
@@ -2,6 +2,6 @@
 setlocal
 pushd %~dp0
 
-java -ea -Dlogname=GlobalRaft -cp .;..\..\ZezeJavaTest\lib\*;..\..\ZezeJavaTest\build\libs\ZezeJavaTest-1.5.10.jar GlobalRaft.TestGlobalCacheMgrWithRaft
+java -ea -Dlogname=GlobalRaft -cp .;..\..\ZezeJavaTest\lib\*;..\..\ZezeJavaTest\build\libs\ZezeJavaTest-1.5.11-SNAPSHOT.jar GlobalRaft.TestGlobalCacheMgrWithRaft
 
 pause
