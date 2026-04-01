c67ed112af009eb690a02994897a339cb616f51c
stallboy <stallboy@qq.com>
2026-04-01 13:43:20 +0800

    fix index.mdx

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,8 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://e2wugui.github.io',
-	base: '/zeze/',
+	base: '/zeze',
 	integrations: [
 		starlight({
 			title: 'Zeze 文档',
